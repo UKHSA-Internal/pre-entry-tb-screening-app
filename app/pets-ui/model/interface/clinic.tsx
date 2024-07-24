b6b0ce22723d6e02aac3bd62c4a8b3eee5d0bf73
Will Tipper <will.tipper@hotmail.co.uk>
2024-07-24 17:21:59 +0100

    Create model folder: - contains clinic component

---
+++ app/pets-ui/model/interface/clinic.tsx
@@ -0,0 +1,17 @@
+export default interface ClinicInterface {
+    testStationId: string
+    testStationPNumber: string
+    testStationStatus: string
+    testStationName: string
+    testStationContactNumber: string
+    testStationAccessNotes: string
+    testStationGeneralNotes: string
+    testStationTown: string
+    testStationAddress: string
+    testStationPostcode: string
+    testStationCountry: string
+    testStationLongitude: string
+    testStationLatitude: string
+    testStationType: string
+    testStationEmails: string[]
+}
