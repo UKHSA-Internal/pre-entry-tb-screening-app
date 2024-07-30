54865ab2a54e7696e4724bba71d4c3405c29d26f
Will Tipper <will.tipper@hotmail.co.uk>
2024-07-30 13:22:27 +0100

    Fix whitespace in page.tsx

--- app/pets-ui/app/page.tsx
+++ app/pets-ui/app/page.tsx
@@ -3,7 +3,7 @@ import ClinicsDashboard from '@components/dashboard/clinicDashboard';
 export default function Home() {
 	return (
     <div className="govuk-width-container">
-      <main className=" govuk-main-wrapper">
+      <main className="govuk-main-wrapper">
         <ClinicsDashboard/>
       </main>
     </div>
