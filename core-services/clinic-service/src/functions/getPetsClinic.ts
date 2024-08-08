750194981fc544fd2ae2dace77df1db91cc911ed
tomide1 <cleriocuzio15@yahoo.com>
2024-08-08 14:15:15 +0100

    adding ts-jest

---
+++ core-services/clinic-service/src/functions/getPetsClinic.ts
@@ -0,0 +1,38 @@
+import { PetsClinicService } from "../services/PetsClinicService";
+import { PetsClinicDAO } from "../models/PetsClinicDAO";
+import { HTTPResponse } from "../models/HTTPResponse";
+import { HTTPError } from "../models/HTTPError";
+import { Validator } from "../utils/Validator";
+import { Handler } from "aws-lambda";
+import { ERRORS, HTTPRESPONSE } from "../utils/Enum";
+
+export const getPetsClinic: Handler = async (event) => {
+  const petsClinicDAO = new PetsClinicDAO();
+  const service = new PetsClinicService(petsClinicDAO);
+
+  const check: Validator = new Validator();
+
+  if (event.pathParameters) {
+    if (!check.parametersAreValid(event.pathParameters)) {
+      return new HTTPResponse(400, HTTPRESPONSE.MISSING_PARAMETERS);
+    }
+  } else {
+    return new HTTPResponse(400, HTTPRESPONSE.MISSING_PARAMETERS);
+  }
+
+  const testStationPNumber = event.pathParameters
+    ? event.pathParameters.testStationPNumber
+    : undefined;
+
+  try {
+    const testStation = await service.getTestStation(testStationPNumber);
+    return new HTTPResponse(200, testStation);
+  } catch (error: any) {
+    console.error(error);
+    if (!(error instanceof HTTPError)) {
+      error.statusCode = 500;
+      error.body = ERRORS.INTERNAL_SERVER_ERROR;
+    }
+    return new HTTPError(error.statusCode, error.body);
+  }
+};
---
+++ core-services/clinic-service/src/services/PetsClinicService.ts
@@ -0,0 +1,45 @@
+import { HTTPError } from "../models/HTTPError";
+import { IPetsClinic } from "../models/IPetsClinic";
+import { ERRORS } from "../utils/Enum";
+
+export class PetsClinicService {
+  public readonly petsClinicDAO: PetsClinicDAO;
+
+  constructor(petsClinicDAO: PetsClinicDAO) {
+    this.petsClinicDAO = petsClinicDAO;
+  }
+
+  /**
+   * Fetch a list of all pets clinics from DynamoDB
+   */
+  public getAllPetsClinic() {
+    return this.petsClinicDAO
+      .getAll()
+      .then((data: any) => {
+        if (data.Count === 0) {
+          throw new HTTPError(404, ERRORS.RESOURCE_NOT_FOUND);
+        }
+        return data.Items;
+      })
+      .catch((error: any) => {
+        if (!(error instanceof HTTPError)) {
+          console.log(error);
+          error.statusCode = 500;
+          error.body = ERRORS.INTERNAL_SERVER_ERROR;
+        }
+        throw new HTTPError(error.statusCode, error.body);
+      });
+  }
+
+  /**
+   * Update or insert the provided Pets Clinic details to the DB.
+   * @param petsClinicItem (PetsClinic)
+   */
+     public async putPetsClinic(petsClinicItem: IPetsClinic) {
+      const data: any = await this.petsClinicDAO.putItem(petsClinicItem);
+      if (data?.UnprocessedItems) {
+        console.error(`Item not added: ${JSON.stringify(data.UnprocessedItems)}`);
+        throw new Error(ERRORS.FAILED_TO_ADD_ITEM);
+      }
+    }
+}
