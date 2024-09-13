13b2285f3ca10d49ed5242a4dcb902569b3be37d
Will Tipper <will.tipper@hotmail.co.uk>
2024-09-13 15:32:28 +0100

    Implement correct lambda layer folder structure into pipeline

--- core-services/clinic-service/zip-node-modules.js
+++ core-services/clinic-service/zip-node-modules.js
@@ -1,10 +1,10 @@
 fs = require('fs')
-fs.mkdir('./clinic-service-node-modules', (err) => {
+fs.mkdir('./nodejs', (err) => {
     if (err) throw err;
 })
 let inputPackageDotJson = fs.readFileSync('./core-services/clinic-service/package.json', 'utf-8')
 let outputPackageDotJson = '{"dependencies":' + JSON.stringify(JSON.parse(inputPackageDotJson)['dependencies']) + '}'
-fs.writeFile('./clinic-service-node-modules/package.json', outputPackageDotJson, (err) => {
+fs.writeFile('./nodejs/package.json', outputPackageDotJson, (err) => {
     if (err) throw err;
 });
 
