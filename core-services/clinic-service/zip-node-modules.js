12b3e8a1e70fc212bb5cb2f74a9febcdab00041e
Will Tipper <will.tipper@hotmail.co.uk>
2024-09-13 15:00:03 +0100

    Fix zip-node-modules script

--- core-services/clinic-service/zip-node-modules.js
+++ core-services/clinic-service/zip-node-modules.js
@@ -2,8 +2,14 @@ fs = require('fs')
 fs.mkdir('./clinic-service-node-modules', (err) => {
     if (err) throw err;
 })
-inputPackageDotJson = fs.readFileSync('./core-services/clinic-service/package.json', 'utf-8')
-outputPackageDotJson = '{"dependencies":' + JSON.stringify(JSON.parse(inputPackageDotJson)['dependencies']) + '}'
+let inputPackageDotJson = fs.readFileSync('./core-services/clinic-service/package.json', 'utf-8')
+let outputPackageDotJson = '{"dependencies":' + JSON.stringify(JSON.parse(inputPackageDotJson)['dependencies']) + '}'
 fs.writeFile('./clinic-service-node-modules/package.json', outputPackageDotJson, (err) => {
     if (err) throw err;
 });
+
+module.exports = () => {
+    return {
+        outputPackageDotJson
+    }
+}
