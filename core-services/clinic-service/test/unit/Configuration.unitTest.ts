16f5ac5d4b9ec955934c3bd39ef107a4cd5ab077
tomide1 <tomide.fasugba@ukhsa.gov.uk>
2024-09-16 09:53:12 +0100

    updated configutation unit test

--- core-services/clinic-service/test/unit/Configuration.unitTest.ts
+++ core-services/clinic-service/test/unit/Configuration.unitTest.ts
@@ -4,7 +4,7 @@ import { ERRORS } from "@utils/Enum";
 
 let dbConfig: IDBConfig;
 
-function setUpConfigs (env: string) {
+function setUpDatabaseConfigs (env: string) {
   process.env.BRANCH = env;
 
   if (env == "develop") {
@@ -37,7 +37,6 @@ describe("Configuration", () => {
   });
 
   describe("the config is empty", () => {
-    setUpConfigs("local");
     const emptyConfig: Configuration = new Configuration(
       "../../test/resources/EmptyConfig.yml"
     );
@@ -53,7 +52,7 @@ describe("Configuration", () => {
   });
 
   describe("the BRANCH environment variable is local", () => {
-    setUpConfigs("local");
+    setUpDatabaseConfigs("local");
     it("should return the local invoke config", () => {
       expect(Object.keys(dbConfig.params)).toEqual(
         expect.arrayContaining(["region", "endpoint"])
@@ -63,26 +62,24 @@ describe("Configuration", () => {
   });
 
   describe("the BRANCH environment variable is local-global", () => {
-    process.env.BRANCH = "local-global";
-    const databaseConfig:IDBConfig = Configuration.getInstance().getDynamoDBConfig();
     it("should return the local-global invoke config", () => {
-      expect(Object.keys(databaseConfig)).toEqual(
+      setUpDatabaseConfigs("local-global");
+      expect(Object.keys(dbConfig)).toEqual(
         expect.arrayContaining(["params", "table"])
       );
-      expect(Object.keys(databaseConfig.params)).toEqual(
+      expect(Object.keys(dbConfig.params)).toEqual(
         expect.arrayContaining(["region", "endpoint"])
       );
-      expect(Object.keys(databaseConfig)).not.toEqual(
+      expect(Object.keys(dbConfig)).not.toEqual(
         expect.arrayContaining(["keys"])
       );
-      expect(databaseConfig.table).toEqual("pets-local-global-clinics");
+      expect(dbConfig.table).toEqual("pets-local-global-clinics");
     });
   });
 
   describe("the BRANCH environment variable is empty", () => {
-    process.env.BRANCH = "";
-    let dbConfig = Configuration.getInstance().getDynamoDBConfig();
     it("should return the remote invoke config", () => {
+      setUpDatabaseConfigs("");
       expect(Object.keys(dbConfig)).not.toEqual(
         expect.arrayContaining(["keys"])
       );
@@ -91,35 +88,18 @@ describe("Configuration", () => {
     });
   });
 
-  describe("the config is empty", () => {
-    setUpConfigs("local");
-    const emptyConfig: Configuration = new Configuration(
-      "../../test/resources/EmptyConfig.yml"
-    );
-    it("should throw error", () => {
-      try {
-        emptyConfig.getFunctions();
-      } catch (e) {
-        expect((e as Error).message).toEqual(
-          ERRORS.FUNCTION_CONFIG_NOT_DEFINED
-        );
-      }
-    });
-
-    describe("the BRANCH environment variable is 'develop'", () => {
-      it("should return the remote invoke config", () => {
-        setUpConfigs("develop");
-        expect(Object.keys(dbConfig)).not.toEqual(
-          expect.arrayContaining(["keys"])
-        );
-        expect(dbConfig.table).toEqual("pets-clinics");
-        expect(dbConfig.params).toStrictEqual({});
-      });
+  describe("the BRANCH environment variable is 'develop'", () => {
+    it("should return the remote invoke config", () => {
+      setUpDatabaseConfigs("develop");
+      expect(Object.keys(dbConfig)).not.toEqual(
+        expect.arrayContaining(["keys"])
+      );
+      expect(dbConfig.table).toEqual("pets-clinics");
+      expect(dbConfig.params).toStrictEqual({});
     });
   });
 
   describe("the config is present", () => {
-    setUpConfigs("local");
     const funcConfig: IFunctionConfig[] =
       Configuration.getInstance().getFunctions();
     it("should return the list of specified functions with names and matching paths", () => {
