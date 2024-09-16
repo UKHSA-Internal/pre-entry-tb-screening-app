4f1ba573d31d6b51fda4bf65336f99b143310eed
Will Tipper <will.tipper@hotmail.co.uk>
2024-09-16 17:14:20 +0100

    Wrap zip-node-modules.js contents in module.export

--- .github/scripts/zip-node-modules.js
+++ .github/scripts/zip-node-modules.js
@@ -1,12 +1,14 @@
-// Create folder structure required for lambda layer
-const fs = require('fs')
-fs.mkdir('./nodejs', (err) => {
-    if (err) throw err;
-})
+module.exports = () => {
+    // Create folder structure required for lambda layer
+    const fs = require('fs')
+    fs.mkdir('./nodejs', (err) => {
+        if (err) throw err;
+    })
 
-// Create package.json containing only non-dev dependencies
-let inputPackageDotJson = fs.readFileSync('./core-services/clinic-service/package.json', 'utf-8')
-let outputPackageDotJson = '{"dependencies":' + JSON.stringify(JSON.parse(inputPackageDotJson)['dependencies']) + '}'
-fs.writeFile('./nodejs/package.json', outputPackageDotJson, (err) => {
-    if (err) throw err;
-});
+    // Create package.json containing only non-dev dependencies
+    let inputPackageDotJson = fs.readFileSync('./core-services/clinic-service/package.json', 'utf-8')
+    let outputPackageDotJson = '{"dependencies":' + JSON.stringify(JSON.parse(inputPackageDotJson)['dependencies']) + '}'
+    fs.writeFile('./nodejs/package.json', outputPackageDotJson, (err) => {
+        if (err) throw err;
+    });
+}
