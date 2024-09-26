84bf1c0568d5c347562fa43a0815ab6a15380fd0
Will Tipper <will.tipper@hotmail.co.uk>
2024-09-26 18:14:02 +0100

    Update zip-node-modules to work across multiple services

--- .github/scripts/zip-node-modules.js
+++ .github/scripts/zip-node-modules.js
@@ -1,14 +1,15 @@
-module.exports = () => {
+module.exports = (serviceName) => {
     // Create folder structure required for lambda layer
     const fs = require('fs')
-    fs.mkdir('./nodejs', (err) => {
+    serviceName = serviceName.toString()
+    fs.mkdir(`./node-zip/${serviceName}/nodejs`, (err) => {
         if (err) throw err;
     })
 
     // Create package.json containing only non-dev dependencies
-    let inputPackageDotJson = fs.readFileSync('./core-services/clinic-service/package.json', 'utf-8')
+    let inputPackageDotJson = fs.readFileSync(`./core-services/${serviceName}/package.json`, 'utf-8')
     let outputPackageDotJson = '{"dependencies":' + JSON.stringify(JSON.parse(inputPackageDotJson)['dependencies']) + '}'
-    fs.writeFile('./nodejs/package.json', outputPackageDotJson, (err) => {
+    fs.writeFile(`./node-zip/${serviceName}/nodejs/package.json`, outputPackageDotJson, (err) => {
         if (err) throw err;
     });
 }
