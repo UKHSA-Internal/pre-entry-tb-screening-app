61b4864cb5069433ec3d67a4726e9729063d39a8
Will Tipper <will.tipper@hotmail.co.uk>
2024-09-26 19:26:36 +0100

    Fix script to use fs Sync methods

--- .github/scripts/create-package-dot-json.js
+++ .github/scripts/create-package-dot-json.js
@@ -2,19 +2,12 @@ module.exports = (serviceName) => {
     // Create folder structure required for lambda layer
     const fs = require('fs')
     serviceName = serviceName.toString()
-    fs.mkdir(`./node-zip/${serviceName}/nodejs`, { recursive: true }, (err) => {
-        if (err) throw err;
-    })
-
-    fs.readdir(`./`, (err, files) => { if (err) throw err; else console.log(files)})
-    fs.readdir(`./node-zip/`, (err, files) => { if (err) throw err; else console.log(files)})
-    fs.readdir(`./node-zip/${serviceName}/`, (err, files) => { if (err) throw err; else console.log(files)})
-    fs.readdir(`./node-zip/${serviceName}/nodejs/`, (err, files) => { if (err) throw err; else console.log(files)})
+    fs.mkdirSync(`./node-zip/${serviceName}/nodejs/`, {recursive: true}, (err) => { if (err) throw err })
 
     // Create package.json containing only non-dev dependencies
     let inputPackageDotJson = fs.readFileSync(`./core-services/${serviceName}/package.json`, 'utf-8')
     let outputPackageDotJson = '{"dependencies":' + JSON.stringify(JSON.parse(inputPackageDotJson)['dependencies']) + '}'
-    fs.writeFile(`./node-zip/${serviceName}/nodejs/package.json`, outputPackageDotJson, (err) => {
+    fs.writeFileSync(`./node-zip/${serviceName}/nodejs/package.json`, outputPackageDotJson, (err) => {
         if (err) throw err;
     });
 }
