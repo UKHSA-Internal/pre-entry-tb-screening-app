027f798c9a57ab9eeb79786c162b57b29e152a80
Will Tipper <will.tipper@hotmail.co.uk>
2024-10-03 11:35:46 +0100

    Replace globals.css with sass: - delete globals.css - remove globals.css import from layout.tsx - add sass as dev dependency

--- app/pets-ui/app/layout.tsx
+++ app/pets-ui/app/layout.tsx
@@ -4,8 +4,6 @@ import { Inter } from "next/font/google";
 import Footer from "@components/footer/footer";
 import Header from "@components/header/header";
 
-import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
