164a9b466d3f71e2f3a96fdbee493637377a37b0
Will Tipper <will.tipper@hotmail.co.uk>
2024-10-03 17:25:11 +0100

    Add sass to homepage & layout files

--- app/pets-ui/app/layout.tsx
+++ app/pets-ui/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import "./layout.scss";
+
 import Footer from "@components/footer/footer";
 import Header from "@components/header/header";
 
