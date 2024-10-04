b304e2554dd7c86a6973141263d0806360cb267b
Will Tipper <will.tipper@hotmail.co.uk>
2024-10-04 13:50:01 +0100

    Add types to button component

--- app/pets-ui/components/button/button.tsx
+++ app/pets-ui/components/button/button.tsx
@@ -3,15 +3,23 @@ import Link from 'next/link'
 
 export interface ButtonProps {
     id: string;
+    type: "default" | "secondary" | "warning";
     text: string;
     href: string;
+}
+
+const buttonClassName = (type: string) => {
+    if (type == "default") return "govuk-button"
+    else if (type == "secondary") return "govuk-button govuk-button--secondary"
+    else if (type == "warning") return "govuk-button govuk-button--warning"
+    else return "govuk-button"
 }
 
 export default function Button(props: ButtonProps) {
     return (
         <div id={props.id} className="govuk-button-group">
             <Link href={props.href}>
-                <button type="submit" className="govuk-button" data-module="govuk-button">
+                <button type="submit" className={buttonClassName(props.type)} data-module="govuk-button">
                     {props.text}
                 </button>
             </Link>
