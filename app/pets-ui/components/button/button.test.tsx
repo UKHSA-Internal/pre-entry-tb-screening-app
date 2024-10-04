75755fad31beaa2539532c5681d14adee3875e51
Will Tipper <will.tipper@hotmail.co.uk>
2024-10-04 16:28:43 +0100

    Add unit tests for button components

---
+++ app/pets-ui/components/button/button.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Button, { ButtonType } from './button'
+
+describe('Button component', () => {
+    it('renders correctly when button type is DEFAULT', () => {
+        render(<Button id='test-id' text='test-text-default' href='/test-href' type={ButtonType.DEFAULT}/>)
+        expect(screen.getByText('test-text-default')).toBeTruthy()
+
+        const {container} = render(<Button id='test-id' text='test-text-default' href='/test-href' type={ButtonType.DEFAULT}/>)
+        expect(container.getElementsByClassName('govuk-button')).toHaveLength(1)
+    })
+
+    it('renders correctly when button type is SECONDARY', () => {
+        render(<Button id='test-id' text='test-text-secondary' href='/test-href' type={ButtonType.SECONDARY}/>)
+        expect(screen.getByText('test-text-secondary')).toBeTruthy()
+
+        const {container} = render(<Button id='test-id' text='test-text-secondary' href='/test-href' type={ButtonType.SECONDARY}/>)
+        expect(container.getElementsByClassName('govuk-button--secondary')).toHaveLength(1)
+    })
+
+    it('renders correctly when button type is WARNING', () => {
+        render(<Button id='test-id' text='test-text-warning' href='/test-href' type={ButtonType.WARNING}/>)
+        expect(screen.getByText('test-text-warning')).toBeTruthy()
+
+        const {container} = render(<Button id='test-id' text='test-text-warning' href='/test-href' type={ButtonType.WARNING}/>)
+        expect(container.getElementsByClassName('govuk-button--warning')).toHaveLength(1)
+    })
+})
