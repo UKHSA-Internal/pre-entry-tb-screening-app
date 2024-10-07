64b05947e42722d2e63ec177a460fdef928b4a13
Will Tipper <will.tipper@hotmail.co.uk>
2024-10-07 14:32:33 +0100

    Add enter-details page with name & passport number fields and submit button

---
+++ app/pets-ui/app/applicant/enter-details/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+import FreeText from '@/components/input/freeText';
+import Button, { ButtonType } from '@/components/button/button';
+import './page.scss'
+
+export default function Page() {
+	return (
+    <div className="govuk-width-container">
+      <main className="govuk-main-wrapper">
+        <FreeText
+            id="name"
+            title="Applicant's Name"
+            label="Full Name"
+        />
+        <FreeText
+            id="passport-number"
+            title="Applicant's Passport Information"
+            label="Passport Number"
+            hint="For example, 1208297A"
+        />
+        <Button
+            id="save-and-continue"
+            type={ButtonType.DEFAULT}
+            text="Save and continue"
+            href="/applicant/check-answers"
+        />
+        <br/>
+      </main>
+    </div>
+  );
+}
