6e012c434fa9171c2f2432ad189b9ed9651342ac
Will Tipper <will.tipper@hotmail.co.uk>
2024-10-21 10:55:44 +0100

    Remove identitySource from securityDefinitions

--- .github/scripts/add-integrations-to-swagger.js
+++ .github/scripts/add-integrations-to-swagger.js
@@ -21,7 +21,6 @@ module.exports = (serviceName) => {
             'x-amazon-apigateway-authtype' : 'custom',
             'x-amazon-apigateway-authorizer' : {
                 'type' : 'token',
-                'identitySource' : 'method.request.header.Authorization',
                 'authorizerUri' : 'arn:aws:apigateway:eu-west-2:lambda:path/2015-03-31/functions/arn:aws:lambda:eu-west-2:AWS_ACCOUNT_ID:function:api-gateway-authoriser/invocations',
                 'authorizerCredentials' : 'arn:aws:iam::AWS_ACCOUNT_ID:role/api_gateway_auth_invocation',
                 'identityValidationExpression' : '^x-[a-z]+',
