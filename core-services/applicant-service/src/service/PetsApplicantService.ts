e6f1080e2bcaf9947e5fd95aa650169a223bbe68
Will Tipper <will.tipper@hotmail.co.uk>
2024-10-23 10:46:14 +0100

    Add catch block to putPetsApplicant method in service

--- core-services/applicant-service/src/service/PetsApplicantService.ts
+++ core-services/applicant-service/src/service/PetsApplicantService.ts
@@ -1,5 +1,6 @@
 import { IPetsApplicant } from "@/models/IPetsApplicant";
 import { ERRORS } from "@utils/Enum";
+import { HTTPError } from "@/models/HTTPError";
 import PetsApplicantDAO from "@/models/dao/PetsApplicantDAO";
 
 export class PetsApplicantService {
@@ -14,10 +15,20 @@ export class PetsApplicantService {
    * @param petsApplicantItem (PetsApplicant)
    */
      public async putPetsApplicant(petsApplicantItem: IPetsApplicant) {
-      const data: any = await this.PetsApplicantDAO.putItem(petsApplicantItem);
+      const data: any = await this.PetsApplicantDAO
+        .putItem(petsApplicantItem)
+        .catch((error: any) => {
+          if (!(error instanceof HTTPError)) {
+            console.log(error);
+            error.statusCode = 500;
+            error.body = ERRORS.INTERNAL_SERVER_ERROR;
+          }
+          throw new HTTPError(error.statusCode, error.body);
+        });
       if (data?.UnprocessedItems) {
         console.error(`Item not added: ${JSON.stringify(data.UnprocessedItems)}`);
         throw new Error(ERRORS.FAILED_TO_ADD_ITEM);
       }
+      return "Data successfully posted"
     }
 }
