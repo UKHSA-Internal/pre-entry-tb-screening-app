dd9dd6566a061eed0792c8511fc05f8a31e37761
Will Tipper <will.tipper@hotmail.co.uk>
2024-10-24 15:20:18 +0100

    Add get method to service

--- core-services/applicant-service/src/service/PetsApplicantService.ts
+++ core-services/applicant-service/src/service/PetsApplicantService.ts
@@ -1,4 +1,5 @@
 import { IPetsApplicant } from "@/models/IPetsApplicant";
+import { IPetsApplicantPassport } from "@/models/IPetsApplicantPassport";
 import { ERRORS } from "@utils/Enum";
 import { HTTPError } from "@/models/HTTPError";
 import PetsApplicantDAO from "@/models/dao/PetsApplicantDAO";
@@ -8,6 +9,31 @@ export class PetsApplicantService {
 
   constructor(PetsApplicantDAO: PetsApplicantDAO) {
     this.PetsApplicantDAO = PetsApplicantDAO;
+  }
+
+  /**
+   * Fetch applicant details using passport number & country of issue.
+   * @param petsApplicantPassport (PetsApplicantPassport)
+   */
+  public async getPetsApplicant(petsApplicantPassport: IPetsApplicantPassport) {
+    const data: any = await this.PetsApplicantDAO
+      .getItem(petsApplicantPassport)
+      .then((data: any) => {
+        if (data.Count === 0) {
+          throw new HTTPError(404, ERRORS.RESOURCE_NOT_FOUND);
+        }
+        console.log(data.Items)
+        return data.Items;
+      })
+      .catch((error: any) => {
+        if (!(error instanceof HTTPError)) {
+          console.log(error);
+          error.statusCode = 500;
+          error.body = ERRORS.INTERNAL_SERVER_ERROR;
+        }
+        throw new HTTPError(error.statusCode, error.body);
+      });
+    return data
   }
 
   /**
