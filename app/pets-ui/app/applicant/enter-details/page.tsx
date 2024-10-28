94b34163e37e7facd5b6d46ccc266a3810198919
Will Tipper <will.tipper@hotmail.co.uk>
2024-10-28 11:24:08 +0000

    Update existing components on enter-details page, add fields for sex & address

--- app/pets-ui/app/applicant/enter-details/page.tsx
+++ app/pets-ui/app/applicant/enter-details/page.tsx
@@ -2,6 +2,7 @@
 import FreeText from '@/components/freeText/freeText';
 import Button, { ButtonType } from '@/components/button/button';
 import DateTextInput from '@/components/dateTextInput/dateTextInput';
+import Radio, { RadioIsInline } from '@/components/radio/radio';
 
 import './page.scss'
 
@@ -20,23 +21,59 @@ export default function Page() {
             label="Passport Number"
             hint="For example, 1208297A"
         />
+        {/* Country of nationality dropdown */}
+        {/* Country of issue dropdown */}
         <DateTextInput
             id="passport-issue-date"
             autocomplete={false}
-            title="Issue Date"
+            legend="Issue Date"
             hint="For example, 31 3 2019"
         />
         <DateTextInput
             id="passport-expiry-date"
             autocomplete={false}
-            title="Expiry Date"
+            legend="Expiry Date"
             hint="For example, 31 3 2019"
         />
         <DateTextInput
             id="birth-date"
             autocomplete={true}
-            title="Date of Birth"
+            legend="Date of Birth"
             hint="For example, 31 3 2019"
+        />
+        <Radio
+            id="sex"
+            title="Applicant's Sex"
+            isInline={RadioIsInline.TRUE}
+            answerOptions={["Male", "Female"]}
+            sortAnswersAlphabetically={false}
+        />
+        {/* Type of visa dropdown */}
+        <FreeText
+            id="address-1"
+            title="Applicant's Home Address"
+            label="Address line 1"
+        />
+        <FreeText
+            id="address-2"
+            label="Address line 2"
+        />
+        <FreeText
+            id="address-3"
+            label="Address line 3"
+        />
+        <FreeText
+            id="town-or-city"
+            label="Town/City"
+        />
+        <FreeText
+            id="province-or-state"
+            label="Province/State"
+        />
+        {/* Country dropdown */}
+        <FreeText
+            id="postcode"
+            label="Postcode"
         />
         <Button
             id="save-and-continue"
