d5e2b8d41749ab691e0000e7114c99ac933439c8
Will Tipper <will.tipper@hotmail.co.uk>
2024-11-01 10:51:59 +0000

    Fix freetext component tests

--- app/pets-ui/components/freeText/freeText.test.tsx
+++ app/pets-ui/components/freeText/freeText.test.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import FreeText from './freeText'
 
+const handleChange = () => {}
+
 describe('FreeText component', () => {
     it('renders correctly when all attributes are specified', () => {
-        render(<FreeText id='test-id' title='test-title' label='test-label' hint='test-hint'/>)
+        render(<FreeText id='test-id' title='test-title' label='test-label' hint='test-hint' handleChange={handleChange}/>)
         expect(screen.getByRole('textbox')).toBeTruthy()
         expect(screen.getByText('test-title')).toBeTruthy()
         expect(screen.getByText('test-label')).toBeTruthy()
@@ -12,7 +14,7 @@ describe('FreeText component', () => {
     })
 
     it('renders correctly when all optional attributes are omitted', () => {
-        render(<FreeText id='test-id'/>)
+        render(<FreeText id='test-id' handleChange={handleChange}/>)
         expect(screen.getByRole('textbox')).toBeTruthy()
         expect(screen.queryByText('test-title')).toBeNull()
         expect(screen.queryByText('test-label')).toBeNull()
