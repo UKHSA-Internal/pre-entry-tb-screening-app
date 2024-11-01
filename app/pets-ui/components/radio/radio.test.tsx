53cce55b288167d2fbea02537490e2a81bd37164
Will Tipper <will.tipper@hotmail.co.uk>
2024-11-01 10:54:52 +0000

    Fix radio tests

--- app/pets-ui/components/radio/radio.test.tsx
+++ app/pets-ui/components/radio/radio.test.tsx
@@ -3,10 +3,11 @@ import { fireEvent, render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Radio, { RadioIsInline } from './radio'
 
+const handleChange = () => {}
 
 describe('Radio component', () => {
     it('renders correctly when all optional props are specified', () => {
-        render(<Radio id='test-id' title='test-title' legend='test-legend' hint='test-hint' isInline={RadioIsInline.FALSE} answerOptions={['zzz Answer One', 'aaa Answer Two']} sortAnswersAlphabetically={false}/>)
+        render(<Radio id='test-id' title='test-title' legend='test-legend' hint='test-hint' isInline={RadioIsInline.FALSE} answerOptions={['zzz Answer One', 'aaa Answer Two']} sortAnswersAlphabetically={false} handleChange={handleChange}/>)
         expect(screen.getAllByRole('radio')).toBeTruthy()
         expect(screen.getByText('test-title')).toBeTruthy()
         expect(screen.getByText('test-legend')).toBeTruthy()
@@ -14,7 +15,7 @@ describe('Radio component', () => {
     })
 
     it('renders correctly when all optional props are omitted', () => {
-        render(<Radio id='test-id' isInline={RadioIsInline.FALSE} answerOptions={['zzz Answer One', 'aaa Answer Two']} sortAnswersAlphabetically={false}/>)
+        render(<Radio id='test-id' isInline={RadioIsInline.FALSE} answerOptions={['zzz Answer One', 'aaa Answer Two']} sortAnswersAlphabetically={false} handleChange={handleChange}/>)
         expect(screen.getAllByRole('radio')).toBeTruthy()
         expect(screen.queryByText('test-title')).toBeNull()
         expect(screen.queryByText('test-legend')).toBeNull()
@@ -22,33 +23,33 @@ describe('Radio component', () => {
     })
 
     it('is not inline when props.isInline is FALSE', () => {
-        const {container} = render(<Radio id='test-id' isInline={RadioIsInline.FALSE} answerOptions={['zzz Answer One', 'aaa Answer Two']} sortAnswersAlphabetically={false}/>)
+        const {container} = render(<Radio id='test-id' isInline={RadioIsInline.FALSE} answerOptions={['zzz Answer One', 'aaa Answer Two']} sortAnswersAlphabetically={false} handleChange={handleChange}/>)
         expect(container.getElementsByClassName('govuk-radios')).toHaveLength(1)
         expect(container.getElementsByClassName('govuk-radios--inline')).toHaveLength(0)
     })
     
     it('is inline when props.isInline is TRUE', () => {
-        const {container} = render(<Radio id='test-id' isInline={RadioIsInline.TRUE} answerOptions={['zzz Answer One', 'aaa Answer Two']} sortAnswersAlphabetically={false}/>)
+        const {container} = render(<Radio id='test-id' isInline={RadioIsInline.TRUE} answerOptions={['zzz Answer One', 'aaa Answer Two']} sortAnswersAlphabetically={false} handleChange={handleChange}/>)
         expect(container.getElementsByClassName('govuk-radios')).toHaveLength(1)
         expect(container.getElementsByClassName('govuk-radios--inline')).toHaveLength(1)
     })
 
     it('orders answers in the order specified when props.sortAnswersAlphabetically is false', () => {
-        const {container} = render(<Radio id='test-id' isInline={RadioIsInline.FALSE} answerOptions={['zzz Answer One', 'aaa Answer Two']} sortAnswersAlphabetically={false}/>)
+        const {container} = render(<Radio id='test-id' isInline={RadioIsInline.FALSE} answerOptions={['zzz Answer One', 'aaa Answer Two']} sortAnswersAlphabetically={false} handleChange={handleChange}/>)
         const labels = container.getElementsByClassName('govuk-label govuk-radios__label')
         expect(labels[0].innerHTML).toEqual(expect.stringContaining('zzz Answer One'))
         expect(labels[1].innerHTML).toEqual(expect.stringContaining('aaa Answer Two'))
     })
 
     it('orders answers alphabetically when props.sortAnswersAlphabetically is true', () => {
-        const {container} = render(<Radio id='test-id' isInline={RadioIsInline.FALSE} answerOptions={['zzz Answer One', 'aaa Answer Two']} sortAnswersAlphabetically={true}/>)
+        const {container} = render(<Radio id='test-id' isInline={RadioIsInline.FALSE} answerOptions={['zzz Answer One', 'aaa Answer Two']} sortAnswersAlphabetically={true} handleChange={handleChange}/>)
         const labels = container.getElementsByClassName('govuk-label govuk-radios__label')
         expect(labels[0].innerHTML).toEqual(expect.stringContaining('aaa Answer Two'))
         expect(labels[1].innerHTML).toEqual(expect.stringContaining('zzz Answer One'))
     })
 
     it('renders with no answer selected and only selects a single answer at a time', () => {
-        render(<Radio id='test-id' isInline={RadioIsInline.FALSE} answerOptions={['zzz Answer One', 'aaa Answer Two']} sortAnswersAlphabetically={false}/>)
+        render(<Radio id='test-id' isInline={RadioIsInline.FALSE} answerOptions={['zzz Answer One', 'aaa Answer Two']} sortAnswersAlphabetically={false} handleChange={handleChange}/>)
         const radioOne = screen.getAllByRole('radio')[0]
         const radioTwo = screen.getAllByRole('radio')[1]
         
