2efa6cdf312dd984806e9adeaad5a038933a9a6f
Will Tipper <will.tipper@hotmail.co.uk>
2024-11-04 11:08:03 +0000

    Move visa options list, id-db map, db-id map to utils folder

---
+++ app/pets-ui/utils/component-id-to-db-attribute.ts
@@ -0,0 +1,15 @@
+export const idToDbAttribute: {[key:string]:string} = {
+    "name": "fullName",
+    "passport-number": "passportNumber",
+    "country-of-nationality": "countryOfNationality",
+    "country-of-issue": "countryOfIssue",
+    "visa-type": "typesOfVisa",
+    "applicants-sex": "sex",
+    "address-1": "applicantHomeAddress1",
+    "address-2": "applicantHomeAddress2",
+    "address-3": "applicantHomeAddress3",
+    "town-or-city": "townOrCity",
+    "province-or-state": "provinceOrState",
+    "address-country": "country",
+    "postcode": "postcode"
+}
---
+++ app/pets-ui/utils/db-attribute-to-component-id.ts
@@ -0,0 +1,15 @@
+export const attributeToComponentId: {[key:string]:string} = {
+    "fullName": "name",
+    "passportNumber": "passport-number",
+    "countryOfNationality": "country-of-nationality",
+    "countryOfIssue": "country-of-issue",
+    "typesOfVisa": "visa-type",
+    "sex": "applicants-sex",
+    "applicantHomeAddress1": "address-1",
+    "applicantHomeAddress2": "address-2",
+    "applicantHomeAddress3": "address-3",
+    "townOrCity": "town-or-city",
+    "provinceOrState": "province-or-state",
+    "country": "address-country",
+    "postcode": "postcode"
+}
