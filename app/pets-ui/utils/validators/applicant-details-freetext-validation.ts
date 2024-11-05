56f84338fe96fd07a3d79b55c2de428caa73e44f
Will Tipper <will.tipper@hotmail.co.uk>
2024-11-05 17:00:44 +0000

    Refactor free text validation function

--- app/pets-ui/utils/validators/applicant-details-freetext-validation.ts
+++ app/pets-ui/utils/validators/applicant-details-freetext-validation.ts
@@ -5,49 +5,49 @@ const fieldValidationDetails = {
         "regex": /[^A-Za-z\s]/,
         "regexError": "Full name must contain only letters and spaces.",
         "isMandatory": true,
-        "mandatoryError": "Enter the applicant's full name."
+        "emptyFieldError": "Enter the applicant's full name."
     },
     "passportNumber": {
         "regex": /[^A-Za-z0-9]/,
         "regexError": "Passport number must contain only letters and numbers.",
         "isMandatory": true,
-        "mandatoryError": "Enter the applicant's passport number."
+        "emptyFieldError": "Enter the applicant's passport number."
     },
     "applicantHomeAddress1": {
         "regex": /[^A-Za-z0-9\s,-/()]/,
         "regexError": "Home address must contain only letters, numbers, spaces and punctuation.",
         "isMandatory": true,
-        "mandatoryError": "Enter the first line of the applicant's home address."
+        "emptyFieldError": "Enter the first line of the applicant's home address."
     },
     "applicantHomeAddress2": {
         "regex": /[^A-Za-z0-9\s,-/()]/,
         "regexError": "Home address must contain only letters, numbers, spaces and punctuation.",
         "isMandatory": false,
-        "mandatoryError": ""
+        "emptyFieldError": ""
     },
     "applicantHomeAddress3": {
         "regex": /[^A-Za-z0-9\s,-/()]/,
         "regexError": "Home address must contain only letters, numbers, spaces and punctuation.",
         "isMandatory": false,
-        "mandatoryError": ""
+        "emptyFieldError": ""
     },
     "townOrCity": {
         "regex": /[^A-Za-z\s,-/()]/,
         "regexError": "Town name must contain only letters, spaces and punctuation.",
         "isMandatory": true,
-        "mandatoryError": "Enter the town or city of the applicant's home address."
+        "emptyFieldError": "Enter the town or city of the applicant's home address."
     },
     "provinceOrState": {
         "regex": /[^A-Za-z\s,-/()]/,
         "regexError": "Province/state name must contain only letters, spaces and punctuation.",
         "isMandatory": true,
-        "mandatoryError": "Enter the province or state of the applicant's home address."
+        "emptyFieldError": "Enter the province or state of the applicant's home address."
     },
     "postcode": {
         "regex": /[^A-Za-z0-9\s]/,
         "regexError": "Postcode must contain only letters, numbers and spaces.",
         "isMandatory": false,
-        "mandatoryError": ""
+        "emptyFieldError": ""
     }
 }
 
@@ -70,7 +70,7 @@ export function validateFreeTextFields(formData: IFormData) {
             const validationDetails = fieldValidationDetails[field as keyof typeof fieldValidationDetails]
             if (validationDetails.isMandatory && textFieldValue.length < 1) {
                 errorsExist = true
-                errorMessages[field as keyof typeof errorMessages] = validationDetails.mandatoryError
+                errorMessages[field as keyof typeof errorMessages] = validationDetails.emptyFieldError
             } else if (textFieldValue.search(new RegExp(validationDetails.regex)) > -1 ) {
                 errorsExist = true
                 errorMessages[field as keyof typeof errorMessages] = validationDetails.regexError
