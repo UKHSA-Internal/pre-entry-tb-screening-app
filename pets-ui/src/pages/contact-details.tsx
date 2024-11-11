959d53f31f56a9556d70abe33c45a3997e71051b
Will Tipper <will.tipper@hotmail.co.uk>
2024-11-11 17:01:04 +0000

    Add contact details page & supporting sections/utils

---
+++ pets-ui/src/pages/contact-details.tsx
@@ -0,0 +1,36 @@
+import {default as ContactForm} from '@sections/contact-form';
+import { GridCol, GridRow, Heading, Main, Paragraph, Page } from 'govuk-react';
+
+// ----------------------------------------------------------------------
+
+
+export default function ContactDetailsPage() {
+  return (
+    <Page>
+      <Main>
+        {/* Page Title */}
+        <Heading>
+            Service Name
+        </Heading>
+        <Paragraph>Welcome to the service description or page introduction text.</Paragraph>
+
+        {/* Content Section */}
+        <GridRow>
+            <GridCol setWidth="one-half">
+                <Heading size="M">Section Title</Heading>
+                <Paragraph>Content for this section goes here.</Paragraph>
+            </GridCol>
+
+            <GridCol setWidth="one-half">
+                <Heading size="M">Another Section</Heading>
+                <Paragraph>Additional content or instructions go here.</Paragraph>
+            </GridCol>
+        </GridRow>
+
+        <ContactForm />
+
+    </Main>
+
+    </Page>
+  );
+}
