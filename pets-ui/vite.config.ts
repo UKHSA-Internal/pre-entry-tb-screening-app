5f70d9469b77f5aab36df77bb9f0fd12f0321045
Will Tipper <will.tipper@hotmail.co.uk>
2024-11-12 12:05:04 +0000

    Fix path aliasing

--- pets-ui/vite.config.ts
+++ pets-ui/vite.config.ts
@@ -1,16 +1,8 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-      alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@sections": path.resolve(__dirname, "./src/sections"),
-    },
-  },
-})
+  plugins: [react(), tsconfigPaths()],
+});
