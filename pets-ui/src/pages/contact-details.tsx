72590ed935cf87cd9b5cc5cbe19643cb825be50d
Will Tipper <will.tipper@hotmail.co.uk>
2024-11-20 17:50:55 +0000

    Refactor contact-details page to use applicant-form and new components

--- pets-ui/src/pages/contact-details.tsx
+++ pets-ui/src/pages/contact-details.tsx
@@ -1,26 +1,23 @@
 import { Helmet } from 'react-helmet-async';
-import ContactForm from '@sections/contact-form';
-import { Heading, Main, Paragraph, Page, GlobalStyle } from 'govuk-react';
-
-// ----------------------------------------------------------------------
-
+import Header from "@/components/header/header";
+import Footer from "@/components/footer/footer";
+import ApplicantForm from '@/sections/applicant-form';
+import "./contact-details.scss"
 
 export default function ContactDetailsPage() {
   return (
-    <Page>
-      <GlobalStyle/>
-      <Helmet>
-        <title> Create Applicant Form</title>
-      </Helmet>
-
-      <Main>
-        <Heading>
-            Enter Applicant Information
-        </Heading>
-        <Paragraph>Enter the applicants profile information below. Select &apos;Save and Review&apos; to save any information added</Paragraph>
-        <ContactForm />
-      </Main>
-
-    </Page>
+    <body className="govuk-template__body">
+    <Helmet>
+      <title> Applicant Details Form</title>
+    </Helmet>
+    <Header/>
+      <div className="govuk-width-container">
+        <main className="govuk-main-wrapper">
+          <h1 className="govuk-heading-xl">Enter Applicant Details</h1>
+          <ApplicantForm/>
+        </main>
+      </div>
+      <Footer/>
+    </body>
   );
 }
