81cd097f0b532fd970c07d6267aace094b75ab83
Will Tipper <will.tipper@hotmail.co.uk>
2024-11-22 10:39:03 +0000

    Update config files for vitest

--- pets-ui/vite.config.ts
+++ pets-ui/vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     port: 3000,
   },
   test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
     coverage: {
       exclude: [
         ...coverageConfigDefaults.exclude,
