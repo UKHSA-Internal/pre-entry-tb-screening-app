b9f60cb882f70963c212d7aacd499d87135a3224
Will Tipper <will.tipper@hotmail.co.uk>
2024-11-29 17:31:07 +0000

    Add breadcrumbs to applicant seach page

--- pets-ui/src/pages/applicant-search.tsx
+++ pets-ui/src/pages/applicant-search.tsx
@@ -1,19 +1,32 @@
 import { Helmet } from 'react-helmet-async';
 import Header from "@/components/header/header";
 import Footer from "@/components/footer/footer";
+import Breadcrumb, { IBreadcrumbItem } from "@/components/breadcrumb/breadcrumb";
 import ApplicantSearchForm from '@/sections/applicant-search-form';
 import "./applicant-search.scss"
 
 export default function ApplicantSearchPage() {
+  const breadcrumbItems: IBreadcrumbItem[] = [
+    {
+      text: "Home",
+      href: "/"
+    },
+    {
+      text: "Applicant Search",
+      href: "#"
+    },
+  ]
+  
   return (
     <body className="govuk-template__body">
-    <Helmet>
-      <title>Applicant Search</title>
-    </Helmet>
-    <Header/>
+      <Helmet>
+        <title>Applicant Search</title>
+      </Helmet>
+      <Header/>
       <div className="govuk-width-container">
+        <Breadcrumb items={breadcrumbItems}/>
         <main className="govuk-main-wrapper">
-          <h1 className="govuk-heading-l">Search for a visa applicant</h1>
+        <h1 className="govuk-heading-l">Search for a visa applicant</h1>
           <p className="govuk-heading-s">Enter the applicant&apos;s passport number and the passport&apos;s country of issue.</p>
           <ApplicantSearchForm/>
         </main>
