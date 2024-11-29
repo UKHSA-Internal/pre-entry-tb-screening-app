735730a09220d9b50e4550619f6ee0ba55b0b6c5
edem.atoklo@ukhsa.gov.uk <Edem.Atoklo@ukhsa.gov.uk>
2024-11-29 16:32:40 +0000

    TBBETA-324
    Updates to Test TBBETA-324

--- pets-ui/cypress.config.mjs
+++ pets-ui/cypress.config.mjs
@@ -4,6 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     supportFile: false,
+    experimentalStudio: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ pets-ui/cypress/e2e/spec.cy.ts
@@ -0,0 +1,76 @@
+import { countryList } from '../../src/utils/helpers';
+
+describe ('Fill out Applicant Details form', () => {
+
+ 
+
+  beforeEach(() => {
+  
+  cy.visit('http://localhost:3000');
+  cy.intercept('POST', 'http://localhost:3004/dev/register-applicant', {
+    statusCode: 200,
+    body: {success: true, message: 'Data successfully posted'}
+}).as('formSubmit');
+});
+
+it('Fill out the application form with valid data', () => { 
+
+  //Enter valid data for 'Full name'
+  cy.get('input[name="fullName"]').type('John Doe');
+  
+
+  //Select a 'Sex'
+  cy.get('input[name="sex"]').check('male');
+
+  //Randomly select a 'Country of Nationality'
+  cy.get('#country-of-nationality.govuk-select').select('ATA');
+
+  //Enter valid data for 'date of birth'
+  cy.get('input#birth-date-day').type('4');
+  cy.get('input#birth-date-month').type('JAN');
+  cy.get('input#birth-date-year').type('1998');
+
+
+  //Enter valid data for 'Applicant's Passport number'
+  cy.get('input[name="passportNumber"]').type('AA1235467');
+
+
+  //Select a 'Country of Issue'
+  cy.get('#country-of-issue.govuk-select').select('ATA');
+
+  //Enter valid data for 'Issue Date'
+  cy.get('input#passport-issue-date-day').type('20');
+  cy.get('input#passport-issue-date-month').type('11');
+  cy.get('input#passport-issue-date-year').type('2011');
+
+
+  //Enter valid data for 'Expiry Date'
+  cy.get('input#passport-expiry-date-day').type('19');
+  cy.get('input#passport-expiry-date-month').type('11');
+  cy.get('input#passport-expiry-date-year').type('2031');
+
+
+  //Enter valid address information
+  cy.get('#address-1').type('1322');
+  cy.get('#address-2').type('100th St');
+  cy.get('#address-3').type('Apt 16');
+  cy.get('#town-or-city').type('North Battleford');
+  cy.get('#province-or-state').type('Saskatchewan');
+  cy.get('#address-country.govuk-select').select('CAN');
+  cy.get('#postcode').type('S4R 0M6');
+
+
+  // Click the submit button 
+  cy.get('button[type="submit"]').click();
+
+  
+
+
+
+
+
+
+
+    
+  })
+})
