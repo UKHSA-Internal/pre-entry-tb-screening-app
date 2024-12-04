9b6ef37fc4f7cc69ed2562593679372a86742a8b
Will Tipper <will.tipper@hotmail.co.uk>
2024-12-04 16:59:32 +0000

    Update vite config to run intTest tests

--- pets-ui/vite.config.ts
+++ pets-ui/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, coverageConfigDefaults } from 'vitest/config'
+import { defineConfig, coverageConfigDefaults, defaultInclude } from 'vitest/config'
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -13,6 +13,10 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.ts',
+    include: [
+      ...defaultInclude,
+      "**/*.intTest.?(c|m)[jt]s?(x)"
+    ],
     coverage: {
       include: [
         '**/src/components/**',
