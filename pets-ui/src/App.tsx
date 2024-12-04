369e5fe4be3f43de15cc388df9458e0bf95d7d4e
Will Tipper <will.tipper@hotmail.co.uk>
2024-12-04 11:47:43 +0000

    Create landing page

--- pets-ui/src/App.tsx
+++ pets-ui/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import HomePage from './pages/home-page';
 import ContactDetailsPage from './pages/contact-details';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ContactDetailsPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contact" element={<ContactDetailsPage />} />
       </Routes>
     </Router>
   )
---
+++ pets-ui/src/pages/home-page.tsx
@@ -0,0 +1,41 @@
+import { Helmet } from 'react-helmet-async';
+import Header from "@/components/header/header";
+import Footer from "@/components/footer/footer";
+import StartButton from '@/components/startButton/startButton';
+import "./home-page.scss"
+
+export default function HomePage() {
+  return (
+    <body className="govuk-template__body">
+    <Helmet>
+      <title> Applicant Details Form</title>
+    </Helmet>
+    <Header/>
+      <div className="govuk-width-container">
+        <main className="govuk-main-wrapper">
+          <h1 className="govuk-heading-l">Complete a UK visa applicant&apos;s TB screening</h1>
+          <p className="govuk-body">This service is for authorised staff to enter TB screening information for UK visa applicants. For advice and guidance on the process, visa applicants should visit <a href='https://www.gov.uk/tb-test-visa'>Tuberculosis tests for visa applicants: Check if you need a TB test for your visa application</a>.</p>
+          <p className="govuk-body">Use this service to:</p>
+          <ul className="govuk-body">
+            <li>enter the applicant details</li>
+            <li>enter their travel information</li>
+            <li>Complete the TB medical screening</li>
+            <li>upload and view X-ray images</li>
+            <li>search for screening information for an existing UK visa applicant</li>
+            <li>check on a TB screening applicant&apos;s progress</li>
+          </ul>
+          <p className="govuk-body">Applicants need a:</p>
+          <ul className="govuk-body">
+            <li>valid passport</li>
+            <li>UK address</li>
+            <li>UK visa type</li>
+          </ul>
+          <p className="govuk-body">If you&apos;re a clinician you can also sign in to resume a medical screening case.</p>
+          <p className="govuk-body">Use the username and password you were assigned to sign in.</p>
+          <StartButton id="sign-in" text="Sign In" href='' handleClick={() => {}}/>
+        </main>
+      </div>
+      <Footer/>
+    </body>
+  );
+}
