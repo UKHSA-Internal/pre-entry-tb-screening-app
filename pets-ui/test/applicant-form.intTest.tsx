853a1071b393a1eb13f626c95f298f87c8661f9f
Will Tipper <will.tipper@hotmail.co.uk>
2024-12-05 14:46:28 +0000

    Create redux integration test for applicant form

--- pets-ui/test/applicant-form.intTest.tsx
+++ pets-ui/test/applicant-form.intTest.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import { http, HttpResponse, delay } from 'msw'
+import { http, HttpResponse } from 'msw'
 import { setupServer } from 'msw/node'
 import { fireEvent, screen } from '@testing-library/react'
-
+import userEvent from '@testing-library/user-event'
+import { Mock } from 'vitest'
 import { renderWithProviders } from '@/utils/test-utils'
 import ApplicantForm from '@/sections/applicant-form'
 import ApplicantReview from '@sections/applicant-confirmation'
 
+const useNavigateMock: Mock = vi.fn();
+vi.mock(`react-router-dom`, async (): Promise<unknown> => {
+  const actual: Record<string, unknown> = await vi.importActual(`react-router-dom`);
+  return {
+    ...actual,
+    useNavigate: (): Mock => useNavigateMock,
+  };
+});
 
 export const handlers = [
-  http.post("http://localhost:3005/dev/register-applicant", async ({ request }) => {
+  http.post('http://localhost:3005/dev/register-applicant', async ({ request }) => {
     const newPost = await request.json()
     console.log(newPost);
     return HttpResponse.json({}, { status: 201 })
@@ -27,28 +35,64 @@ afterEach(() => server.resetHandlers())
 // Disable API mocking after the tests are done.
 afterAll(() => server.close())
 
-
-test('fetches & receives a user after clicking the fetch user button', async () => {
+test('state is updated from ApplicantForm and then read by ApplicantReview', async () => {
   renderWithProviders(
-    <>
+    <div>
       <ApplicantForm /> 
       <ApplicantReview /> 
-    </>
+    </div>
   )
-
-  // expect(screen.getByText(/no user/i)).toBeInTheDocument()
-  // expect(screen.queryByText(/Fetching user\.\.\./i)).not.toBeInTheDocument()
-
-  // after clicking the 'Fetch user' button, it should now show that it is fetching the user
-  fireEvent.click(screen.getByRole('button', { name: /Fetch user/i }))
-  expect(screen.getByText(/no user/i)).toBeInTheDocument()
-
-  // after some time, the user should be received
-  expect(await screen.findByText(/John Smith/i)).toBeInTheDocument()
-  expect(screen.queryByText(/no user/i)).not.toBeInTheDocument()
-  expect(screen.queryByText(/Fetching user\.\.\./i)).not.toBeInTheDocument()
-
-
-
   
+  const user = userEvent.setup()
+
+  await user.type(screen.getByTestId('name'), 'Sigmund Sigmundson')
+  await user.click(screen.getAllByTestId('sex')[1])
+  fireEvent.change(screen.getAllByRole('combobox')[0], {target: { value: 'NOR'}})
+  await user.type(screen.getByTestId('birth-date-day'), '1')
+  await user.type(screen.getByTestId('birth-date-month'), '1')
+  await user.type(screen.getByTestId('birth-date-year'), '1901')
+  await user.type(screen.getByTestId('passportNumber'), '1234')
+  fireEvent.change(screen.getAllByRole('combobox')[1], {target: { value: 'FIN'}})
+  await user.type(screen.getByTestId('passport-issue-date-day'), '2')
+  await user.type(screen.getByTestId('passport-issue-date-month'), 'feb')
+  await user.type(screen.getByTestId('passport-issue-date-year'), '1902')
+  await user.type(screen.getByTestId('passport-expiry-date-day'), '3')
+  await user.type(screen.getByTestId('passport-expiry-date-month'), 'march')
+  await user.type(screen.getByTestId('passport-expiry-date-year'), '2103')
+  await user.type(screen.getByTestId('address-1'), 'The Bell Tower')
+  await user.type(screen.getByTestId('address-2'), 'Hallgrimskirkja')
+  await user.type(screen.getByTestId('address-3'), 'Hallgrimstorg 1')
+  await user.type(screen.getByTestId('town-or-city'), 'Reykjavik')
+  await user.type(screen.getByTestId('province-or-state'), 'Reykjavik')
+  fireEvent.change(screen.getAllByRole('combobox')[2], {target: { value: 'ISL'}})
+  await user.type(screen.getByTestId('postcode'), '101')
+
+  expect(screen.getByTestId('name')).toHaveValue('Sigmund Sigmundson')
+  expect(screen.getAllByTestId('sex')[0]).not.toBeChecked()
+  expect(screen.getAllByTestId('sex')[1]).toBeChecked()
+  expect(screen.getAllByRole('combobox')[0]).toHaveValue('NOR')
+  expect(screen.getByTestId('birth-date-day')).toHaveValue('1')
+  expect(screen.getByTestId('birth-date-month')).toHaveValue('1')
+  expect(screen.getByTestId('birth-date-year')).toHaveValue('1901')
+  expect(screen.getByTestId('passportNumber')).toHaveValue('1234')
+  expect(screen.getAllByRole('combobox')[1]).toHaveValue('FIN')
+  expect(screen.getByTestId('passport-issue-date-day')).toHaveValue('2')
+  expect(screen.getByTestId('passport-issue-date-month')).toHaveValue('feb')
+  expect(screen.getByTestId('passport-issue-date-year')).toHaveValue('1902')
+  expect(screen.getByTestId('passport-expiry-date-day')).toHaveValue('3')
+  expect(screen.getByTestId('passport-expiry-date-month')).toHaveValue('march')
+  expect(screen.getByTestId('passport-expiry-date-year')).toHaveValue('2103')
+  expect(screen.getByTestId('address-1')).toHaveValue('The Bell Tower')
+  expect(screen.getByTestId('address-2')).toHaveValue('Hallgrimskirkja')
+  expect(screen.getByTestId('address-3')).toHaveValue('Hallgrimstorg 1')
+  expect(screen.getByTestId('town-or-city')).toHaveValue('Reykjavik')
+  expect(screen.getByTestId('province-or-state')).toHaveValue('Reykjavik')
+  expect(screen.getAllByRole('combobox')[2]).toHaveValue('ISL')
+  expect(screen.getByTestId('postcode')).toHaveValue('101')
+
+  await user.click(screen.getByRole('button'))
+
+  expect(screen.getAllByRole("paragraph")[0]).toHaveTextContent("Name: Sigmund Sigmundson")
+  expect(screen.getAllByRole("paragraph")[1]).toHaveTextContent("Country: ISL")
+  expect(screen.getAllByRole("paragraph")[2]).toHaveTextContent("DOB: 1-1-1901")
 })
