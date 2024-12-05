96e157e18e243986ce1e19b52698047df1af7e2f
Will Tipper <will.tipper@hotmail.co.uk>
2024-12-05 14:49:22 +0000

    Fix build error

--- pets-ui/src/main.tsx
+++ pets-ui/src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.ts'
+import { setupStore } from './redux/store.ts'
 
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
+  <Provider store={setupStore()}>
     <HelmetProvider>
       <StrictMode>
         <App />
