5c9055b306c400c63a7efe7c7a48a1bd73e414e0
Will <100358610+WJTipper@users.noreply.github.com>
2024-12-05 11:09:29 +0000

    TBBETA-376: Applicant Search page (#80)
    * Fix country values in db seed data, should be 3-letter values
    
    * Create applicant search page
    
    * Add route for applicant search page
    
    * Create breadcrumb component
    
    * Add breadcrumbs to applicant seach page
    
    * Remove console log
    
    * Fix date validation bug:
    - parseInt accepts non-numeric characters if they appear after numeric characters
    - updated validateDate fn to check for non-digit chars using regex class \D
    
    * Move validateDate from applicant-form to helpers
    
    * Rename FormValues type to ApplicantSearchFormType
    
    * Fix build errors

--- pets-ui/src/App.tsx
+++ pets-ui/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import ContactDetailsPage from './pages/contact-details';
+import ApplicantSearchPage from './pages/applicant-search';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ContactDetailsPage />} />
+        <Route path="/applicant-search" element={<ApplicantSearchPage />} />
       </Routes>
     </Router>
   )
