c81ac2517943c940a1bdeb0e544fa8baaa6ca481
Will Tipper <will.tipper@hotmail.co.uk>
2024-12-06 11:05:21 +0000

    Remove ApplicantDetailsType definition from applicant-form

--- pets-ui/src/sections/applicant-form.tsx
+++ pets-ui/src/sections/applicant-form.tsx
@@ -34,24 +34,6 @@ import {
   setTownOrCity
 } from "@/redux/applicantSlice";
 
-export type ApplicantDetailsType = {
-  fullName: string
-  sex: string
-  dateOfBirth: DateType
-  countryOfNationality: string
-  passportNumber: string
-  countryOfIssue: string
-  passportIssueDate: DateType
-  passportExpiryDate: DateType 
-  applicantHomeAddress1: string
-  applicantHomeAddress2?: string
-  applicantHomeAddress3?: string
-  townOrCity: string
-  provinceOrState: string
-  country: string
-  postcode?: string
-}
-
 const ApplicantForm = () => {
   const navigate = useNavigate();
 
