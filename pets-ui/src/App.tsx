e9b899a0f21696a1af43469252ffd2f75560bda0
Will Tipper <will.tipper@hotmail.co.uk>
2024-12-11 15:02:43 +0000

    Add route for travel details form to app.tsx

--- pets-ui/src/App.tsx
+++ pets-ui/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import ContactDetailsPage from './pages/contact-details';
 import ApplicantSearchPage from './pages/applicant-search';
+import TravelDetailsPage from './pages/travel-details';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ContactDetailsPage />} />
         <Route path="/applicant-search" element={<ApplicantSearchPage />} />
+        <Route path="/travel-details" element={<TravelDetailsPage />} />
       </Routes>
     </Router>
   )
