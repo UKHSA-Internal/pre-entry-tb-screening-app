94d3031878d337a160cc9b0075315ef041a7247f
Will Tipper <will.tipper@hotmail.co.uk>
2024-12-12 17:47:18 +0000

    Add placeholder breadcrumbs

--- pets-ui/src/pages/travel-details.tsx
+++ pets-ui/src/pages/travel-details.tsx
@@ -1,10 +1,22 @@
 import { Helmet } from 'react-helmet-async';
 import Header from "@/components/header/header";
 import Footer from "@/components/footer/footer";
+import Breadcrumb, { IBreadcrumbItem } from "@/components/breadcrumb/breadcrumb";
 import ApplicantTravelForm from '@/sections/applicant-travel-form';
 import "./travel-details.scss"
 
 export default function TravelDetailsPage() {
+  const breadcrumbItems: IBreadcrumbItem[] = [
+    {
+      text: "Home",
+      href: "#"
+    },
+    {
+      text: "New Applicant",
+      href: "#"
+    },
+  ]
+  
   return (
     <body className="govuk-template__body">
     <Helmet>
@@ -12,6 +24,7 @@ export default function TravelDetailsPage() {
     </Helmet>
     <Header/>
       <div className="govuk-width-container">
+        <Breadcrumb items={breadcrumbItems}/>
         <main className="govuk-main-wrapper">
           <h1 className="govuk-heading-l">Travel Information</h1>
           <p className="govuk-body">Enter the applicant&apos;s travel information below.</p>
