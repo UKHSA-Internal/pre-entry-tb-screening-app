c68c2b1d3ba28d26cbd1c9a8fee0e7e709c2b2b6
Will Tipper <will.tipper@hotmail.co.uk>
2024-12-13 11:25:35 +0000

    Create applicant confirmation page

--- pets-ui/src/App.tsx
+++ pets-ui/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import ContactDetailsPage from './pages/contact-details';
 import ApplicantSearchPage from './pages/applicant-search';
+import ApplicantConfirmation from './pages/applicant-confirmation';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ContactDetailsPage />} />
         <Route path="/applicant-search" element={<ApplicantSearchPage />} />
+        <Route path="/applicant-confirmation" element={<ApplicantConfirmation />} />
       </Routes>
     </Router>
   )
