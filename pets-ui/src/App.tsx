18a3508a599c43614efb7051ad4ee437449c0111
Will <100358610+WJTipper@users.noreply.github.com>
2024-12-13 13:44:31 +0000

    TBBETA-553: Create landing page (#107)
    * Create landing page
    
    * Fix home page text
    
    * Increase UI unit test timeout in ci workflow
    
    * Fix landing page title

--- pets-ui/src/App.tsx
+++ pets-ui/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import HomePage from './pages/home-page';
 import ContactDetailsPage from './pages/contact-details';
 import ApplicantSearchPage from './pages/applicant-search';
 
@@ -7,7 +8,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ContactDetailsPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contact" element={<ContactDetailsPage />} />
         <Route path="/applicant-search" element={<ApplicantSearchPage />} />
       </Routes>
     </Router>
