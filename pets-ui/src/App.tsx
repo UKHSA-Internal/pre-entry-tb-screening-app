e51cf21539de9e49825144df167a5505153dce01
edatoklo <167878066+edatoklo@users.noreply.github.com>
2025-01-02 16:18:53 +0000

    Feature/TBBETA-547: Build Travel Information Page (#125)
    * Add route for travel details form to app.tsx
    
    * Add type for travel info to applicant.d.ts
    
    * Create travel-details page
    
    * Add regexes for numbers only and email address
    
    * Create travel info slice in redux
    
    * Fix travel details type
    
    * Remove validateFullName from helpers
    
    * Add visaOptions & fix numbersOnly regex in helpers
    
    * Add travel info attributes to attribute-component map
    
    * Fix double quotes in applicant-form intTest
    
    * Create travel info form page
    
    * Create intTest for travel info form page
    
    * Add placeholder breadcrumbs
    
    * Cypress Tests for TBBETA-547
    
    Tests to cover feature TBBETA-547 Travel Information Details.
    
    * Update enterPostCodeTest
    
    updating the test for basic postcode validation.
    
    ---------
    
    Co-authored-by: Will Tipper <will.tipper@hotmail.co.uk>

--- pets-ui/src/App.tsx
+++ pets-ui/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home-page';
 import ContactDetailsPage from './pages/contact-details';
 import ApplicantSearchPage from './pages/applicant-search';
+import TravelDetailsPage from './pages/travel-details';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/contact" element={<ContactDetailsPage />} />
         <Route path="/applicant-search" element={<ApplicantSearchPage />} />
+        <Route path="/travel-details" element={<TravelDetailsPage />} />
       </Routes>
     </Router>
   )
