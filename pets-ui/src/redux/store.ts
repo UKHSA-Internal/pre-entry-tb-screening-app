db027e980eea45efd68723fd1ba2c51ffa6ca5bc
Will Tipper <will.tipper@hotmail.co.uk>
2025-01-02 16:06:19 +0000

    Create medical screening slice & add to store

--- pets-ui/src/redux/store.ts
+++ pets-ui/src/redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import { applicantReducer } from './applicantSlice'
+import { medicalScreeningReducer } from './medicalScreeningSlice'
 
 const rootReducer = combineReducers({
   applicant: applicantReducer,
+  medicalScreening: medicalScreeningReducer,
 })
 
 export const setupStore = (preloadedState?: Partial<RootState>) => {
