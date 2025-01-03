49954e4af5d4ee012820deb2e3fd11fb6809ba29
Will Tipper <will.tipper@hotmail.co.uk>
2025-01-03 16:52:10 +0000

    Add integration test for medical screening form

---
+++ pets-ui/src/sections/medical-screening-confirmation.tsx
@@ -0,0 +1,15 @@
+import { useAppSelector } from '@/redux/hooks'
+import { selectMedicalScreening } from '@/redux/medicalScreeningSlice';
+
+const MedicalScreeningReview = () => {
+  const medicalScreening = useAppSelector(selectMedicalScreening);
+  return(
+    <>
+      <p>Age: {medicalScreening.age}</p>
+      <p>Previous TB: {medicalScreening.previousTb}</p>
+      <p>Exam Notes: {medicalScreening.physicalExamNotes}</p>
+    </>
+  )
+}
+
+export default MedicalScreeningReview;
