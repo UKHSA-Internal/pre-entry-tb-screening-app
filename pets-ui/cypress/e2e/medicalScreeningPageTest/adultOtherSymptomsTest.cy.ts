7f45efa738dd4e8d594d277712b9029323bdd8bb
edem.atoklo@ukhsa.gov.uk <Edem.Atoklo@ukhsa.gov.uk>
2025-01-08 08:25:03 +0000

    Cypress Tests for feature/TBBETA-461
    Cypress Tests for Medical Screening Page. Changes to this PR include Test Runner config file changes to cypress config file in order to generate HTML reports.

---
+++ pets-ui/cypress/e2e/medicalScreeningPageTest/adultOtherSymptomsTest.cy.ts
@@ -0,0 +1,70 @@
+import { countryList } from "../../../src/utils/helpers";
+
+// Random number generator
+const randomElement = <T>(arr: T[]): T =>
+  arr[Math.floor(Math.random() * arr.length)];
+const randomCountry = randomElement(countryList);
+const countryName = randomCountry?.value;
+const visaType = [
+  "Family Reunion",
+  "Settlement and Dependents",
+  "Students",
+  "Work",
+  "Working Holiday Maker",
+  "Government Sponsored",
+];
+const urlFragment = [
+    "#age",
+    
+];
+// Validate the error messages above each text box are correct
+const errorMessages = ["Enter applicant's age in years."];
+
+describe("Validate error message is displayed when age field is empty", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/medical-screening");
+    cy.intercept("POST", "http://localhost:3004/dev/register-applicant", {
+      statusCode: 200,
+      body: { success: true, message: "Data successfully posted" },
+    }).as("formSubmit");
+  });
+  it("Should display error message above the age test field", () => {
+
+     //Enter applicant's age
+     cy.get('input[name="age"]').type("66");
+
+     //Select applicants TB Status'
+     cy.get('input[name="tbSymptoms"]').check("yes");
+   
+     //Select applicant's symptons
+     cy.get('input[name="tbSymptomsList"]').check("other-symptoms");
+
+     //Enter 'Other symptons'
+     cy.get('#other-symptoms-detail').type("chest pains, headache and fatigue");
+
+     //Select an option if applicant is a child under 11
+     cy.get('input[name="underElevenConditions"]').check("not-applicable---applicant-is-aged-11-or-over");
+
+     //Select Applicant's previous TB status
+     cy.get('input[name="previousTb"]').check("no");
+
+     //Select if applicant has had close contact with a person with Pulmonary TB in the past year
+     cy.get('input[name="closeContactWithTb"]').check("no");
+
+     //Indicate if applicant is pregnant
+     cy.get('input[name="pregnant"]').check("dont-know");
+
+     //Select if applicant have menstrual periods
+     cy.get('input[name="menstrualPeriods"]').check("yes");
+
+     //Enter applicant physical examination notes
+     cy.get('#physical-exam-notes').type("Applicant appears fit");
+
+
+    // Click the submit button
+    cy.get('button[type="submit"]').click();
+
+    // Validate that the page navigates to the confirmation page 
+    cy.url().should('include', 'http://localhost:3000/medical-confirmation');
+    });
+    });
---
+++ pets-ui/cypress/e2e/medicalScreeningPageTest/applicantMissingAgeTest.cy.ts
@@ -0,0 +1,86 @@
+import { countryList } from "../../../src/utils/helpers";
+
+// Random number generator
+const randomElement = <T>(arr: T[]): T =>
+  arr[Math.floor(Math.random() * arr.length)];
+const randomCountry = randomElement(countryList);
+const countryName = randomCountry?.value;
+const visaType = [
+  "Family Reunion",
+  "Settlement and Dependents",
+  "Students",
+  "Work",
+  "Working Holiday Maker",
+  "Government Sponsored",
+];
+const urlFragment = [
+    "#age",
+    
+];
+// Validate the error messages above each text box are correct
+const errorMessages = ["Enter applicant's age in years."];
+
+describe("Validate error message is displayed when age field is empty", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/medical-screening");
+    cy.intercept("POST", "http://localhost:3004/dev/register-applicant", {
+      statusCode: 200,
+      body: { success: true, message: "Data successfully posted" },
+    }).as("formSubmit");
+  });
+  it("Should display error message above the age test field", () => {
+
+     //Select applicants TB Status'
+     cy.get('input[name="tbSymptoms"]').check("yes");
+   
+     //Select applicant's symptons
+     cy.get('input[name="tbSymptomsList"]').check("cough");
+     cy.get('input[name="tbSymptomsList"]').check("other-symptoms");
+
+     //Enter 'Other symptons'
+     cy.get('#other-symptoms-detail').type("chest pains and fatigue");
+
+     //Select an option if applicant is a child under 11
+     cy.get('input[name="underElevenConditions"]').check("not-applicable---applicant-is-aged-11-or-over");
+
+     //Enter details of procedure or condition
+     cy.get('#under-eleven-conditions-detail').type("thoracic surgery");
+
+     //Select Applicant's previous TB status
+     cy.get('input[name="previousTb"]').check("yes");
+
+     //Enter details of previous TB infection
+     cy.get('#previous-tb-detail').type("chest infection");
+
+     //Select if applicant has had close contact with a person with Pulmonary TB in the past year
+     cy.get('input[name="closeContactWithTb"]').check("yes");
+
+     //Enter details of close contact with a person with Pulmonary TB in the past year
+     cy.get('#close-contact-with-tb-detail').type("Grandmother diagnosed with TB");
+
+     //Indicate if applicant is pregnant
+     cy.get('input[name="pregnant"]').check("dont-know");
+
+     //Select if applicant have menstrual periods
+     cy.get('input[name="menstrualPeriods"]').check("na");
+
+     //Enter applicant physical examination notes
+     cy.get('#physical-exam-notes').type("Applicant appears fit");
+
+
+    // Click the submit button
+    cy.get('button[type="submit"]').click();
+
+    // Validate the summary box appears at the top contains the correct error messages
+    cy.get(".govuk-error-summary").should("be.visible");
+    errorMessages.forEach((error) => {
+      cy.get(".govuk-error-summary").should("contain.text", error);
+
+      // Validate that user is navigated to correct error when clicking message in summary
+    cy.get('.govuk-error-summary a').each((link, index) => {
+        cy.wrap(link).click();
+        cy.url().should('include', urlFragment[index]);
+    });
+    });
+  });
+});
