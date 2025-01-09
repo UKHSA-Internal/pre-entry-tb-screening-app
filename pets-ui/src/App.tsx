dd3538809a1509cd53677c3ef1bd2ef5c3c21dd5
Will Tipper <will.tipper@hotmail.co.uk>
2025-01-09 17:28:55 +0000

    Create travel confirmation page

--- pets-ui/src/App.tsx
+++ pets-ui/src/App.tsx
@@ -5,6 +5,7 @@ import ContactDetailsPage from './pages/contact-details';
 import ApplicantSearchPage from './pages/applicant-search';
 import ApplicantConfirmation from './pages/applicant-confirmation';
 import TravelDetailsPage from './pages/travel-details';
+import TravelConfirmation from './pages/travel-confirmation';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/applicant-search" element={<ApplicantSearchPage />} />
         <Route path="/applicant-confirmation" element={<ApplicantConfirmation />} />
         <Route path="/travel-details" element={<TravelDetailsPage />} />
+        <Route path="/travel-confirmation" element={<TravelConfirmation />} />
       </Routes>
     </Router>
   )
