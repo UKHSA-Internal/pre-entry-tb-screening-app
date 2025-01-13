e9f215a75a5b400ea008a8814a8f627ab64158c3
Will Tipper <will.tipper@hotmail.co.uk>
2025-01-13 16:42:31 +0000

    Create medical screening summary page

--- pets-ui/src/App.tsx
+++ pets-ui/src/App.tsx
@@ -4,6 +4,7 @@ import HomePage from './pages/home-page';
 import ContactDetailsPage from './pages/contact-details';
 import ApplicantSearchPage from './pages/applicant-search';
 import MedicalScreeningPage from './pages/medical-screening';
+import MedicalSummaryPage from './pages/medical-screening-summary';
 import ApplicantConfirmation from './pages/applicant-confirmation';
 import TravelDetailsPage from './pages/travel-details';
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/applicant-search" element={<ApplicantSearchPage />} />
         <Route path="/applicant-confirmation" element={<ApplicantConfirmation />} />
         <Route path="/medical-screening" element={<MedicalScreeningPage />} />
+        <Route path="/medical-summary" element={<MedicalSummaryPage />} />
         <Route path="/travel-details" element={<TravelDetailsPage />} />
       </Routes>
     </Router>
---
+++ pets-ui/src/sections/medical-screening-summary.tsx
@@ -0,0 +1,213 @@
+import Button from '@/components/button/button';
+import { ButtonType } from '@/utils/enums';
+import { useAppSelector } from '@/redux/hooks';
+import { selectApplicant } from '@/redux/applicantSlice';
+import { selectMedicalScreening } from '@/redux/medicalScreeningSlice';
+import { useNavigate } from 'react-router-dom';
+
+const MedicalScreeningReview = () => {
+    const applicantData = useAppSelector(selectApplicant);
+    const medicalData = useAppSelector(selectMedicalScreening);
+    const navigate = useNavigate()
+
+    const handleSubmit = () => {
+        console.log(medicalData)
+        // TO DO: post medicalData
+        navigate("/medical-confirmation")
+    }
+
+    return(
+        <div>
+            <dl className="govuk-summary-list">
+                <div className="govuk-summary-list__row">
+                    <dt className="govuk-summary-list__key">
+                        Name
+                    </dt>
+                    <dd className="govuk-summary-list__value">
+                        {applicantData.fullName}
+                    </dd>
+                    <dd className="govuk-summary-list__actions">
+                    </dd>
+                </div>
+                <div className="govuk-summary-list__row">
+                    <dt className="govuk-summary-list__key">
+                        Age
+                    </dt>
+                    <dd className="govuk-summary-list__value">
+                        {medicalData.age}
+                    </dd>
+                    <dd className="govuk-summary-list__actions">
+                        <a className="govuk-link" style={{color: "#1d70b8"}} onClick={() => navigate("/medical-screening#age")}>
+                            Change<span className="govuk-visually-hidden"> age</span>
+                        </a>
+                    </dd>
+                </div>
+                <div className="govuk-summary-list__row">
+                    <dt className="govuk-summary-list__key">
+                        Does the applicant have TB symptoms?
+                    </dt>
+                    <dd className="govuk-summary-list__value">
+                        {medicalData.tbSymptoms}
+                    </dd>
+                    <dd className="govuk-summary-list__actions">
+                        <a className="govuk-link" style={{color: "#1d70b8"}} onClick={() => navigate("/medical-screening#tb-symptoms")}>
+                            Change<span className="govuk-visually-hidden"> TB symptoms</span>
+                        </a>
+                    </dd>
+                </div>
+                <div className="govuk-summary-list__row">
+                    <dt className="govuk-summary-list__key">
+                        TB symptoms
+                    </dt>
+                    <dd className="govuk-summary-list__value">
+                        {medicalData.tbSymptomsList.map((symptom: string) => symptom.replaceAll("-", " ")).join(", ")}
+                    </dd>
+                    <dd className="govuk-summary-list__actions">
+                        <a className="govuk-link" style={{color: "#1d70b8"}} onClick={() => navigate("/medical-screening#tb-symptoms-list")}>
+                            Change<span className="govuk-visually-hidden"> TB symptoms</span>
+                        </a>
+                    </dd>
+                </div>
+                <div className="govuk-summary-list__row">
+                    <dt className="govuk-summary-list__key">
+                        Other symptoms
+                    </dt>
+                    <dd className="govuk-summary-list__value">
+                        {medicalData.otherSymptomsDetail}
+                    </dd>
+                    <dd className="govuk-summary-list__actions">
+                        <a className="govuk-link" style={{color: "#1d70b8"}} onClick={() => navigate("/medical-screening#other-symptoms-detail")}>
+                            Change<span className="govuk-visually-hidden"> other symptoms</span>
+                        </a>
+                    </dd>
+                </div>
+                <div className="govuk-summary-list__row">
+                    <dt className="govuk-summary-list__key">
+                        Applicant history if under 11
+                    </dt>
+                    <dd className="govuk-summary-list__value">
+                        {medicalData.underElevenConditions.map((symptom: string) => symptom.replaceAll("-", " ")).join(", ")}
+                    </dd>
+                    <dd className="govuk-summary-list__actions">
+                        <a className="govuk-link" style={{color: "#1d70b8"}} onClick={() => navigate("/medical-screening#under-eleven-conditions")}>
+                            Change<span className="govuk-visually-hidden"> applicant history if under 11</span>
+                        </a>
+                    </dd>
+                </div>
+                <div className="govuk-summary-list__row">
+                    <dt className="govuk-summary-list__key">
+                        Additional details of applicant history if under 11
+                    </dt>
+                    <dd className="govuk-summary-list__value">
+                        {medicalData.underElevenConditionsDetail}
+                    </dd>
+                    <dd className="govuk-summary-list__actions">
+                        <a className="govuk-link" style={{color: "#1d70b8"}} onClick={() => navigate("/medical-screening#under-eleven-conditions-detail")}>
+                            Change<span className="govuk-visually-hidden"> additional details of applicant history if under 11</span>
+                        </a>
+                    </dd>
+                </div>
+                <div className="govuk-summary-list__row">
+                    <dt className="govuk-summary-list__key">
+                        Has the applicant ever had tuberculosis?
+                    </dt>
+                    <dd className="govuk-summary-list__value">
+                        {medicalData.previousTb}
+                    </dd>
+                    <dd className="govuk-summary-list__actions">
+                        <a className="govuk-link" style={{color: "#1d70b8"}} onClick={() => navigate("/medical-screening#previous-tb")}>
+                            Change<span className="govuk-visually-hidden"> applicant&apos;s previous TB</span>
+                        </a>
+                    </dd>
+                </div>
+                <div className="govuk-summary-list__row">
+                    <dt className="govuk-summary-list__key">
+                        Detail of applicant&apos;s previous TB
+                    </dt>
+                    <dd className="govuk-summary-list__value">
+                        {medicalData.previousTbDetail}
+                    </dd>
+                    <dd className="govuk-summary-list__actions">
+                        <a className="govuk-link" style={{color: "#1d70b8"}} onClick={() => navigate("/medical-screening#previous-tb-detail")}>
+                            Change<span className="govuk-visually-hidden"> applicant&apos;s previous TB</span>
+                        </a>
+                    </dd>
+                </div>
+                <div className="govuk-summary-list__row">
+                    <dt className="govuk-summary-list__key">
+                        Has the applicant had close contact with any person with active pulmonary tuberculosis within the past year?
+                    </dt>
+                    <dd className="govuk-summary-list__value">
+                        {medicalData.closeContactWithTb}
+                    </dd>
+                    <dd className="govuk-summary-list__actions">
+                        <a className="govuk-link" style={{color: "#1d70b8"}} onClick={() => navigate("/medical-screening#close-contact-with-tb")}>
+                            Change<span className="govuk-visually-hidden"> applicant&apos;s close contact with TB in the past year</span>
+                        </a>
+                    </dd>
+                </div>
+                <div className="govuk-summary-list__row">
+                    <dt className="govuk-summary-list__key">
+                        Details of applicant&apos;s close contact with any person with active pulmonary tuberculosis
+                    </dt>
+                    <dd className="govuk-summary-list__value">
+                        {medicalData.closeContactWithTbDetail}
+                    </dd>
+                    <dd className="govuk-summary-list__actions">
+                        <a className="govuk-link" style={{color: "#1d70b8"}} onClick={() => navigate("/medical-screening#close-contact-with-tb-detail")}>
+                            Change<span className="govuk-visually-hidden"> details of applicant&apos;s close contact with TB in the past year</span>
+                        </a>
+                    </dd>
+                </div>
+                <div className="govuk-summary-list__row">
+                    <dt className="govuk-summary-list__key">
+                        Is the applicant pregnant?
+                    </dt>
+                    <dd className="govuk-summary-list__value">
+                        {medicalData.pregnant}
+                    </dd>
+                    <dd className="govuk-summary-list__actions">
+                        <a className="govuk-link" style={{color: "#1d70b8"}} onClick={() => navigate("/medical-screening#pregnant")}>
+                            Change<span className="govuk-visually-hidden"> pregnancy</span>
+                        </a>
+                    </dd>
+                </div>
+                <div className="govuk-summary-list__row">
+                    <dt className="govuk-summary-list__key">
+                        Does the applicant have menstrual periods?
+                    </dt>
+                    <dd className="govuk-summary-list__value">
+                        {medicalData.menstrualPeriods}
+                    </dd>
+                    <dd className="govuk-summary-list__actions">
+                        <a className="govuk-link" style={{color: "#1d70b8"}} onClick={() => navigate("/medical-screening#menstrual-periods")}>
+                            Change<span className="govuk-visually-hidden"> menstrual periods</span>
+                        </a>
+                    </dd>
+                </div>
+                <div className="govuk-summary-list__row">
+                    <dt className="govuk-summary-list__key">
+                        Physical examination notes
+                    </dt>
+                    <dd className="govuk-summary-list__value">
+                        {medicalData.physicalExamNotes}
+                    </dd>
+                    <dd className="govuk-summary-list__actions">
+                        <a className="govuk-link" style={{color: "#1d70b8"}} onClick={() => navigate("/medical-screening#physical-exam-notes")}>
+                            Change<span className="govuk-visually-hidden"> physical examination notes</span>
+                        </a>
+                    </dd>
+                </div>
+
+            </dl>
+            <Button
+            id="confirm"
+            type={ButtonType.DEFAULT}
+            text="Confirm"
+            href="/medical-confirmation"
+            handleClick={handleSubmit}/>
+        </div>
+    )
+}
+
+export default MedicalScreeningReview;
