ce4d98b7d15b340dd728d02043e6c8793af32b35
Will Tipper <will.tipper@hotmail.co.uk>
2025-01-13 17:25:47 +0000

    Update int test

--- pets-ui/test/medical-screening-form.intTest.tsx
+++ pets-ui/test/medical-screening-form.intTest.tsx
@@ -4,7 +4,8 @@ import userEvent from '@testing-library/user-event'
 import { Mock } from 'vitest'
 import { renderWithProviders } from '@/utils/test-utils'
 import MedicalScreeningForm from '@/sections/medical-screening-form'
-import MedicalScreeningReview from '@/sections/medical-screening-confirmation'
+import MedicalScreeningReview from '@/sections/medical-screening-summary'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const useNavigateMock: Mock = vi.fn();
 vi.mock(`react-router-dom`, async (): Promise<unknown> => {
@@ -30,10 +31,10 @@ afterAll(() => server.close())
 
 test('state is updated from MedicalScreeningForm and then read by MedicalScreeningReview', async () => {
   renderWithProviders(
-    <div>
+    <Router>
       <MedicalScreeningForm /> 
       <MedicalScreeningReview /> 
-    </div>
+    </Router>
   )
   
   const user = userEvent.setup()
@@ -82,9 +83,36 @@ test('state is updated from MedicalScreeningForm and then read by MedicalScreeni
   expect(screen.getAllByTestId('menstrual-periods')[2]).not.toBeChecked()
   expect(screen.getByTestId('physical-exam-notes')).toHaveValue('Details of physical examination.')
 
-  await user.click(screen.getByRole('button'))
+  await user.click(screen.getAllByRole('button')[0])
+  
+  expect(useNavigateMock).toBeCalled()
 
-  expect(screen.getAllByRole('paragraph')[0]).toHaveTextContent('Age: 99')
-  expect(screen.getAllByRole('paragraph')[1]).toHaveTextContent('Previous TB: yes')
-  expect(screen.getAllByRole('paragraph')[2]).toHaveTextContent('Exam Notes: Details of physical examination.')
+  expect(screen.getAllByRole('term')[3]).toHaveTextContent('Name')
+  expect(screen.getAllByRole('definition')[4]).toHaveTextContent('')
+  expect(screen.getAllByRole('term')[4]).toHaveTextContent('Age')
+  expect(screen.getAllByRole('definition')[5]).toHaveTextContent('99')
+  expect(screen.getAllByRole('term')[5]).toHaveTextContent('Does the applicant have TB symptoms?')
+  expect(screen.getAllByRole('definition')[7]).toHaveTextContent('yes')
+  expect(screen.getAllByRole('term')[6]).toHaveTextContent('TB symptoms')
+  expect(screen.getAllByRole('definition')[9]).toHaveTextContent('cough, night sweats')
+  expect(screen.getAllByRole('term')[7]).toHaveTextContent('Other symptoms')
+  expect(screen.getAllByRole('definition')[11]).toHaveTextContent('')
+  expect(screen.getAllByRole('term')[8]).toHaveTextContent('Applicant history if under 11')
+  expect(screen.getAllByRole('definition')[13]).toHaveTextContent('not applicable applicant is aged 11 or over')
+  expect(screen.getAllByRole('term')[9]).toHaveTextContent('Additional details of applicant history if under 11')
+  expect(screen.getAllByRole('definition')[15]).toHaveTextContent('')
+  expect(screen.getAllByRole('term')[10]).toHaveTextContent('Has the applicant ever had tuberculosis?')
+  expect(screen.getAllByRole('definition')[17]).toHaveTextContent('yes')
+  expect(screen.getAllByRole('term')[11]).toHaveTextContent("Detail of applicant's previous TB")
+  expect(screen.getAllByRole('definition')[19]).toHaveTextContent('Details of previous TB.')
+  expect(screen.getAllByRole('term')[12]).toHaveTextContent('Has the applicant had close contact with any person with active pulmonary tuberculosis within the past year?')
+  expect(screen.getAllByRole('definition')[21]).toHaveTextContent('no')
+  expect(screen.getAllByRole('term')[13]).toHaveTextContent("Details of applicant's close contact with any person with active pulmonary tuberculosis")
+  expect(screen.getAllByRole('definition')[23]).toHaveTextContent('')
+  expect(screen.getAllByRole('term')[14]).toHaveTextContent('Is the applicant pregnant?')
+  expect(screen.getAllByRole('definition')[25]).toHaveTextContent('dont-know')
+  expect(screen.getAllByRole('term')[15]).toHaveTextContent('Does the applicant have menstrual periods?')
+  expect(screen.getAllByRole('definition')[27]).toHaveTextContent('no')
+  expect(screen.getAllByRole('term')[16]).toHaveTextContent('Physical examination notes')
+  expect(screen.getAllByRole('definition')[29]).toHaveTextContent('Details of physical examination.')
 })
