fede5b4446693aa13334ef122b99a09b63397dda
Will <100358610+WJTipper@users.noreply.github.com>
2025-01-16 13:43:52 +0000

    TBBETA-156: Travel Summary Page (#169)
    * Add route for travel details form to app.tsx
    
    * Add type for travel info to applicant.d.ts
    
    * Create travel-details page
    
    * Add regexes for numbers only and email address
    
    * Create travel info slice in redux
    
    * Fix travel details type
    
    * Remove validateFullName from helpers
    
    * Add visaOptions & fix numbersOnly regex in helpers
    
    * Add travel info attributes to attribute-component map
    
    * Fix double quotes in applicant-form intTest
    
    * Create travel info form page
    
    * Create intTest for travel info form page
    
    * Create travel summary page and route
    
    * Remove temporary travel info summary
    
    * Fix travel summary page title
    
    * Fix travel summary href in travel form submit fn
    
    * Create applicant travel summary table
    
    * Update travel int test to use new summary table
    
    * Add default value prop to freeText & dropdown components
    
    * Update travel summary change links:
    - remove href attribute (this causes state to be cleared if a different page is specified)
    - replace with useNavigate hook
    - update styling (when href is absent text defaults to black)
    
    * Update travel form page:
    - add data in travel slice as default values of form fields
    - add useLocation & useEffect to scroll to the correct element when clicking change links on summary screen (by default react router does not scroll to the correct element)
    
    * Add placeholder breadcrumbs
    
    * Cypress Tests TBBETA-156
    
    Cypress Tests to cover TBBETA-156
    
    * Updates to Tests
    
    Adding tests to TBBETA-156
    
    * Update validateTravelSummaryPage.cy.ts
    
    * Pin sonarcloud scan action to v4.1.0
    
    * Update int test
    
    * Remove console logs
    
    * Replace getElementById with useRef in travel form
    
    * Fix town link in travel summary
    
    ---------
    
    Co-authored-by: edem.atoklo@ukhsa.gov.uk <Edem.Atoklo@ukhsa.gov.uk>

--- pets-ui/src/App.tsx
+++ pets-ui/src/App.tsx
@@ -5,6 +5,7 @@ import ContactDetailsPage from './pages/contact-details';
 import ApplicantSearchPage from './pages/applicant-search';
 import ApplicantConfirmation from './pages/applicant-confirmation';
 import TravelDetailsPage from './pages/travel-details';
+import TravelSummaryPage from './pages/travel-summary';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/applicant-search" element={<ApplicantSearchPage />} />
         <Route path="/applicant-confirmation" element={<ApplicantConfirmation />} />
         <Route path="/travel-details" element={<TravelDetailsPage />} />
+        <Route path="/travel-summary" element={<TravelSummaryPage />} />
       </Routes>
     </Router>
   )
