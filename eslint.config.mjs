291552f8ea469c6f6fca8fd67d3b2a087377b06a
darasimi-ajewole <Darasimi.Ajewole@ukhsa.gov.uk>
2025-01-26 15:07:40 +0000

    TBBETA-296: Added linting for cypress and UI

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,11 @@ import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
-import importPlugin from "eslint-plugin-import";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import pluginCypress from "eslint-plugin-cypress/flat";
 
 export default tseslint.config(
   {
@@ -10,7 +14,6 @@ export default tseslint.config(
     extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
     plugins: {
       "simple-import-sort": simpleImportSort,
-      "plugin:import": importPlugin,
     },
     languageOptions: {
       ecmaVersion: 2022,
@@ -39,6 +42,41 @@ export default tseslint.config(
     },
   },
   eslintPluginPrettier,
+  {
+    files: ["pets-ui/**/*.{ts,tsx}"],
+    ignores: ["dist", "pets-ui/cypress/**"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      react,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: false,
+        },
+      ],
+    },
+    settings: {
+      react: { version: "18.3" },
+    },
+  },
+  {
+    files: ["pets-ui/cypress/**/*.cy.ts"],
+    plugins: {
+      cypress: pluginCypress,
+    },
+    ...pluginCypress.configs.recommended,
+  },
   {
     files: ["**/*.js"],
     extends: [tseslint.configs.disableTypeChecked],
