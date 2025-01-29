77782f3c62a7c326fd664c5380c07290a159b816
darasimi-ajewole <Darasimi.Ajewole@ukhsa.gov.uk>
2025-01-29 13:08:45 +0000

    TBBETA-296: Updating stage name

--- pets-core-services/src/swagger-generator/generator.ts
+++ pets-core-services/src/swagger-generator/generator.ts
@@ -102,7 +102,11 @@ export const writeApiDocumentation = (configs: SwaggerConfig[]) => {
       version: "1.0.0",
       title: `aw-pets-euw-${getEnvironmentVariable("ENVIRONMENT")}-apigateway-coreservices`,
     },
-    servers: [{ url: `${getEnvironmentVariable("APP_DOMAIN")}/api` }],
+    servers: [
+      {
+        url: `${getEnvironmentVariable("APP_DOMAIN")}/${getEnvironmentVariable("API_GATEWAY_STAGE")}`,
+      },
+    ],
   });
   // eslint-disable-next-line no-console
   console.log(JSON.stringify(docs, null, 2));
