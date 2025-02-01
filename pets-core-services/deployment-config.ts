6f6f08158c8d2866726c613c52d1ee1ae0c15ed9
darasimi-ajewole <Darasimi.Ajewole@ukhsa.gov.uk>
2025-02-01 09:33:29 +0000

    TBBETA-296: Work on Typings

--- pets-core-services/deployment-config.ts
+++ pets-core-services/deployment-config.ts
@@ -1,18 +1,25 @@
 import { join } from "path";
 
-const config = [
+import { getEnvironmentVariable } from "./src/shared/config";
+
+type DeploymentConfig = {
+  lambdaName: string;
+  s3Bucket: string;
+  path: string;
+};
+
+const config: DeploymentConfig[] = [
   {
-    lambdaName: process.env.CLINIC_SERVICE_LAMBDA_NAME,
-    s3Bucket: process.env.CLINIC_SERVICE_LAMBDA_BUCKET,
+    lambdaName: getEnvironmentVariable("CLINIC_SERVICE_LAMBDA_NAME"),
+    s3Bucket: getEnvironmentVariable("CLINIC_SERVICE_LAMBDA_BUCKET"),
     path: join(__dirname, "./src/clinic-service/lambdas/clinics.ts"),
   },
   {
-    lambdaName: process.env.APPLICANT_SERVICE_LAMBDA_NAME,
-    s3Bucket: process.env.APPLICANT_SERVICE_LAMBDA_BUCKET,
+    lambdaName: getEnvironmentVariable("APPLICANT_SERVICE_LAMBDA_NAME"),
+    s3Bucket: getEnvironmentVariable("APPLICANT_SERVICE_LAMBDA_BUCKET"),
     path: join(__dirname, "./src/applicant-service/lambdas/applicants.ts"),
   },
 ];
 
-// TODO: Add typings for config after you upload to bucket successfully
 // eslint-disable-next-line no-console
 console.log(JSON.stringify(config, null, 2)); // exporting to stdout
