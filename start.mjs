87f36800d11a7e9a71d6da64f7e17e5066ccc8cc
darasimi-ajewole <Darasimi.Ajewole@ukhsa.gov.uk>
2025-02-02 19:56:40 +0000

    TBBETA-296: Added async command for running background processes

--- start.mjs
+++ start.mjs
@@ -1,5 +1,4 @@
-import { execSync } from "child_process";
-import concurrently from "concurrently";
+import { execSync, spawn } from "child_process";
 import * as dotenv from "dotenv";
 import { resolve } from "path";
 
@@ -11,6 +10,22 @@ function runCommand(cmd) {
   }
 }
 
+const controller = new AbortController();
+const { signal } = controller;
+function runAsyncCommand(command) {
+  const [cmd, ...args] = command.split(" ");
+  const process = spawn(cmd, args, { stdio: "inherit", signal });
+
+  process.on("error", () => {
+    console.error(`Error in process`);
+    controller.abort();
+  });
+  process.on("SIGINT", () => {
+    console.log("Exiting all waiting processes...");
+    controller.abort();
+  });
+}
+
 dotenv.config({
   path: resolve(process.cwd(), "configs/.env"),
 });
@@ -18,11 +33,12 @@ dotenv.config({
 runCommand("pnpm rimraf pets-core-services/openapi-docs.json");
 runCommand("pnpm rimraf pets-local-infra/cdk.out");
 runCommand("docker compose down");
-runCommand("pnpm --filter 'pets-core-services' generate:swagger-doc"); // Generate swagger file for backend
+runCommand("pnpm --filter pets-core-services generate:swagger-doc"); // Generate swagger file for backend
 runCommand("docker compose up -d --wait");
-runCommand("pnpm --filter 'pets-local-infra' bootstrap");
-runCommand("pnpm --filter 'pets-local-infra' deploy:local"); // Deploy to localstack
-runCommand("pnpm --filter 'pets-ui' dev"); // Starts Vite Server
+runAsyncCommand("pnpm --filter pets-local-infra build:core-services");
+runCommand("pnpm --filter pets-local-infra bootstrap");
+runCommand("pnpm --filter pets-local-infra deploy:local"); // Deploy to localstack
+runAsyncCommand("pnpm --filter pets-ui dev"); // Starts Vite Server
 
 // TODO: Remove the List below
 // Generate swagger file
