2304b6e542c4621b6011d68b537d0d768bd5ee41
darasimi-ajewole <Darasimi.Ajewole@ukhsa.gov.uk>
2025-02-03 12:23:00 +0000

    TBBETA-296: FIxing another deployment trouble

--- pets-ui/src/pages/api-docs.tsx
+++ pets-ui/src/pages/api-docs.tsx
@@ -6,13 +6,27 @@ import { Helmet } from "react-helmet-async";
 import swaggerSpec from "../../../pets-core-services/openapi-docs.json";
 
 const SwaggerUI = React.lazy(() => import("swagger-ui-react"));
-const ApiDocs = () => (
-  <>
-    <Helmet>
-      <title>Api Documentation</title>
-    </Helmet>
-    <SwaggerUI spec={swaggerSpec} />
-  </>
-);
+const ApiDocs = () => {
+  const serverUrl = import.meta.env.DEV
+    ? `http://${location.host}/api`
+    : `https://${location.host}/api`;
+
+  const spec = {
+    ...swaggerSpec,
+    servers: [
+      {
+        url: serverUrl,
+      },
+    ],
+  };
+  return (
+    <>
+      <Helmet>
+        <title>Api Documentation</title>
+      </Helmet>
+      <SwaggerUI spec={spec} />
+    </>
+  );
+};
 
 export default ApiDocs;
