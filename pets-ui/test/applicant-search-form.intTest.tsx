80ff11e1dcaad126c00b2dd481886329d9b9c356
Will Tipper <will.tipper@hotmail.co.uk>
2025-02-05 11:54:53 +0000

    Revise int tests: - remove no results section from search form int test - create new test for no results section, populate redux store before render

--- pets-ui/test/applicant-search-form.intTest.tsx
+++ pets-ui/test/applicant-search-form.intTest.tsx
@@ -1,16 +1,16 @@
-import { http, HttpResponse } from 'msw'
-import { setupServer } from 'msw/node'
-import { fireEvent, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import { Mock } from 'vitest'
-import { renderWithProviders } from '@/utils/test-utils'
-import ApplicantSearchForm from '@/sections/applicant-search-form'
-import ApplicantEmptyResult from '@/sections/applicant-no-results'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
+import { fireEvent, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Mock } from "vitest";
+import { renderWithProviders } from "@/utils/test-utils";
+import ApplicantSearchForm from "@/sections/applicant-search-form";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const useNavigateMock: Mock = vi.fn();
 vi.mock(`react-router-dom`, async (): Promise<unknown> => {
-  const actual: Record<string, unknown> = await vi.importActual(`react-router-dom`);
+  const actual: Record<string, unknown> =
+    await vi.importActual(`react-router-dom`);
   return {
     ...actual,
     useNavigate: (): Mock => useNavigateMock,
@@ -18,49 +18,50 @@ vi.mock(`react-router-dom`, async (): Promise<unknown> => {
 });
 
 export const handlers = [
-  http.get('http://localhost:3005/dev/applicant-details', async ({ request }) => {
-    const newPost = await request.json()
-    console.log(newPost);
-    const url = new URL(request.url)
-    const passportNumber = url.searchParams.get('passportNumber')
-    const countryOfIssue = url.searchParams.get('countryOfIssue')
-    if (passportNumber != '12345' || countryOfIssue != 'AUS') {
-      throw new Error('Unexpected query parameters')
-    }
-    return HttpResponse.json({}, { status: 200 })
-  }),
-]
+  http.get(
+    "http://localhost:3005/dev/applicant-details",
+    async ({ request }) => {
+      const newPost = await request.json();
+      console.log(newPost);
+      const url = new URL(request.url);
+      const passportNumber = url.searchParams.get("passportNumber");
+      const countryOfIssue = url.searchParams.get("countryOfIssue");
+      if (passportNumber != "12345" || countryOfIssue != "AUS") {
+        throw new Error("Unexpected query parameters");
+      }
+      return HttpResponse.json({}, { status: 200 });
+    },
+  ),
+];
 
-const server = setupServer(...handlers)
+const server = setupServer(...handlers);
 
 // Enable API mocking before tests.
-beforeAll(() => server.listen())
+beforeAll(() => server.listen());
 
 // Reset any runtime request handlers we may add during the tests.
-afterEach(() => server.resetHandlers())
+afterEach(() => server.resetHandlers());
 
 // Disable API mocking after the tests are done.
-afterAll(() => server.close())
+afterAll(() => server.close());
 
-test('/applicant-details endpoint is invoked when form is filled out and button is clicked', async () => {
+test("/applicant-details endpoint is invoked when form is filled out and button is clicked", async () => {
   renderWithProviders(
     <Router>
-      <ApplicantSearchForm/>
-      <ApplicantEmptyResult/>
-    </Router>
-  )
-  
-  const user = userEvent.setup()
+      <ApplicantSearchForm />
+    </Router>,
+  );
 
-  await user.type(screen.getByTestId('passport-number'), '12345')
-  fireEvent.change(screen.getAllByRole('combobox')[0], {target: { value: 'AUS' }})
+  const user = userEvent.setup();
 
-  expect(screen.getByTestId('passport-number')).toHaveValue('12345')
-  expect(screen.getAllByRole('combobox')[0]).toHaveValue('AUS')
+  await user.type(screen.getByTestId("passport-number"), "12345");
+  fireEvent.change(screen.getAllByRole("combobox")[0], {
+    target: { value: "AUS" },
+  });
 
-  await user.click(screen.getAllByRole('button')[0])
-  expect(useNavigateMock).toBeCalled()
+  expect(screen.getByTestId("passport-number")).toHaveValue("12345");
+  expect(screen.getAllByRole("combobox")[0]).toHaveValue("AUS");
 
-  expect(screen.getByText('No matching record found')).toBeInTheDocument()
-  expect(screen.getByText('No matches for passport number 12345')).toBeInTheDocument()
-})
+  await user.click(screen.getAllByRole("button")[0]);
+  expect(useNavigateMock).toBeCalled();
+});
