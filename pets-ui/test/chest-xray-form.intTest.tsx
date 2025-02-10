8831c1ac54ee95c9dcf864d44fcef346ebeace21
Terry <terry.phung@ukhsa.gov.uk>
2025-02-10 14:29:26 +0000

    added tests for missing AL/DL Files and throwing error

--- pets-ui/test/chest-xray-form.intTest.tsx
+++ pets-ui/test/chest-xray-form.intTest.tsx
@@ -1,4 +1,5 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { BrowserRouter as Router } from "react-router-dom";
 import { describe, expect, it } from "vitest";
 
@@ -17,7 +18,31 @@ describe("ChestXrayForm Component", () => {
     expect(screen.getByText("Was a lateral decubitus X-ray required ?")).toBeInTheDocument();
   });
 
-  it("uploads a postero-anterior X-ray file", () => {
+  it("uploads a postero-anterior X-ray file", async () => {
+    renderWithProviders(
+      <Router>
+        <ChestXrayForm nextpage="/next" />
+      </Router>,
+    );
+    const input: HTMLInputElement = screen.getByTestId("posteroAnteriorFile");
+    const file = new File(["dummy content"], "example.jpg", { type: "image/jpeg" });
+    fireEvent.change(input, { target: { files: [file] } });
+
+    fireEvent.click(screen.getAllByTestId("apicalLordoticXray")[1]);
+    fireEvent.click(screen.getAllByTestId("lateralDecubitus")[1]);
+
+    // expect file to be in the box
+    expect(input.files?.length).toBe(1);
+
+    const submitButton = screen.getByRole("button", { name: /continue/i });
+    const user = userEvent.setup();
+    await user.click(submitButton);
+
+    // has the file (fize count = 1)
+    expect(input.files?.length).toBe(1);
+  });
+
+  it("Missing upload for AL and LD", async () => {
     renderWithProviders(
       <Router>
         <ChestXrayForm nextpage="/next" />
@@ -27,19 +52,14 @@ describe("ChestXrayForm Component", () => {
     const file = new File(["dummy content"], "example.jpg", { type: "image/jpeg" });
     fireEvent.change(input, { target: { files: [file] } });
 
-    fireEvent.click(screen.getAllByTestId("apicalLordoticXray")[1]);
-    fireEvent.click(screen.getAllByTestId("lateralDecubitus")[1]);
+    fireEvent.click(screen.getAllByTestId("apicalLordoticXray")[0]);
+    fireEvent.click(screen.getAllByTestId("lateralDecubitus")[0]);
 
-    // expect file to be in the box
-    expect(input.files.length).toBe(1);
+    const submit = screen.getByRole("button", { name: /continue/i });
+    const user = userEvent.setup();
+    await user.click(submit);
 
-    const submitButton = screen.getByRole("button", { name: /continue/i });
-    fireEvent.click(submitButton);
-
-    // has the file (fize count = 1)
-    expect(input.files.length).toBe(1);
-
-    // No errors
-    expect(screen.queryByText(/Please Upload/i)).not.toBeInTheDocument();
+    // errors
+    expect(screen.getAllByText(/Please Upload/i)).not.toBeNull();
   });
 });
