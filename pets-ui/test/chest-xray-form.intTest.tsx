bd013fa1f9b5e7033355da029a08a1d29adaa195
Terry <terry.phung@ukhsa.gov.uk>
2025-02-11 17:10:42 +0000

    added test for chest-xray page

--- pets-ui/test/chest-xray-form.intTest.tsx
+++ pets-ui/test/chest-xray-form.intTest.tsx
@@ -1,11 +1,35 @@
 import { fireEvent, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter as Router } from "react-router-dom";
 import { describe, expect, it } from "vitest";
 
+import ChestXrayUploadPage from "@/pages/chest-xray-upload";
 import ChestXrayForm from "@/sections/chest-xray-form";
 import { renderWithProviders } from "@/utils/test-utils";
 
+describe("ChestXrayUploadPage", () => {
+  it("Shows the breadcrums", () => {
+    renderWithProviders(
+      <Router>
+        <HelmetProvider>
+          <ChestXrayUploadPage />
+        </HelmetProvider>
+      </Router>,
+    );
+    const breadcrumbItems = [
+      { text: "Home", href: "/" },
+      { text: "Application Search", href: "/applicant-search" },
+      { text: "Medical Screening", href: "#" },
+    ];
+
+    breadcrumbItems.forEach((item) => {
+      const breadcrumbElement = screen.getByText(item.text);
+      expect(breadcrumbElement).toBeInTheDocument();
+      expect(breadcrumbElement.closest("a")).toHaveAttribute("href", item.href);
+    });
+  });
+});
 describe("ChestXrayForm Component", () => {
   it("renders the component and loads from redux", () => {
     renderWithProviders(
