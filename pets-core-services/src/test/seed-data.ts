ece866d59cb30e42a98e1c977d48538f5a1423b8
darasimi-ajewole <Darasimi.Ajewole@ukhsa.gov.uk>
2025-02-16 23:09:39 +0000

    TBBETA-623: Seeding db for tests

--- pets-core-services/src/test/seed-data.ts
+++ pets-core-services/src/test/seed-data.ts
@@ -1,11 +1,28 @@
 import { seededApplicants } from "../applicant-service/fixtures/applicants";
 import { Applicant } from "../applicant-service/models/applicant";
+import { seededMedicalScreening } from "../application-service/fixtures/medical-screening";
+import { seededTravelInformation } from "../application-service/fixtures/travel-information";
+import { MedicalScreening } from "../application-service/models/medical-screening";
+import { TravelInformation } from "../application-service/models/travel-information";
+import { seededApplications } from "../shared/fixtures/application";
+import { Application } from "../shared/models/application";
 
 export const seedDatabase = async () => {
+  for (const app of seededApplications) {
+    await Application.createNewApplication(app);
+  }
   for (const applicantDetails of seededApplicants) {
     await Applicant.createNewApplicant(applicantDetails);
   }
+
+  for (const travelInformation of seededTravelInformation) {
+    await TravelInformation.createTravelInformation(travelInformation);
+  }
+  for (const medicalScreening of seededMedicalScreening) {
+    await MedicalScreening.createMedicalScreening(medicalScreening);
+  }
 };
+
 export default seedDatabase;
 
 if (process.argv.includes("seed")) {
