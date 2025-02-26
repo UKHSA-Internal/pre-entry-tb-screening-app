95f6ac01a633f49e639c9ef5a14e8595faaa6071
edatoklo <167878066+edatoklo@users.noreply.github.com>
2025-02-26 15:00:52 +0000

    created env variables for TBBETA-63 and minor changes to scripts

--- pets-ui/cypress/e2e/TBBETA-63 - redirectB2C.cy.ts
+++ pets-ui/cypress/e2e/TBBETA-63 - redirectB2C.cy.ts
@@ -1,3 +1,4 @@
+import { testCredentials } from "../support/test-data";
 /**
  * Scenario:
  * Given I am a signed up user:
@@ -15,37 +16,33 @@ describe("Validate that user is navigated to the Applicant Search page when succ
   });
 
   it("should allow login and navigate to Applicant Search page", () => {
-    // Click the sign-in button on your local app
-    cy.get("#sign-in").click();
+    expect(testCredentials.length).to.be.greaterThan(0);
+    // Click the sign-in button
+    cy.get("button#sign-in").click({ force: true });
 
-    // Get credentials from the fixture
-    cy.fixture("login-data.json").then(
-      (credentials: Array<{ email: string; password: string }>) => {
-        // Select a random credential
-        const randomIndex = Math.floor(Math.random() * credentials.length);
-        const { email, password } = credentials[randomIndex];
+    // Select a random credential
+    const randomIndex = Math.floor(Math.random() * testCredentials.length);
+    const { email, password } = testCredentials[randomIndex];
 
-        // Input login details on the B2C login page
-        cy.origin(
-          "https://petsb2cdev.ciamlogin.com",
-          { args: { email, password } },
-          ({ email, password }) => {
-            cy.get('input[type="email"]').type(email);
-            cy.get("#idSIButton9").click();
-            cy.get('input[type="password"]').type(password);
-            cy.get("#idSIButton9").click();
+    // Input login details on the B2C login page
+    cy.origin(
+      "https://petsb2cdev.ciamlogin.com",
+      { args: { email, password } },
+      ({ email, password }) => {
+        cy.get("#i0116").type(email);
+        cy.get("#idSIButton9").click();
+        cy.get('input[type="password"]').type(password);
+        cy.get("#idSIButton9").click();
 
-            // Verify text "Stay signed in?" to be visible
-            cy.contains("Stay signed in?").should("be.visible");
+        // Verify text "Stay signed in?" to be visible
+        cy.contains("Stay signed in?").should("be.visible");
 
-            // Click on the "Yes" button
-            cy.contains("Yes").click();
-          },
-        );
-
-        // Validate that user is redirected to the Application Search page
-        cy.url().should("include", "http://localhost:3000/applicant-search");
+        // Click on the "Yes" button
+        cy.contains("Yes").click();
       },
     );
+
+    // Validate that user is redirected to the Application Search page
+    cy.url().should("include", "http://localhost:3000/applicant-search");
   });
 });
--- pets-ui/cypress/support/test-data.ts
+++ pets-ui/cypress/support/test-data.ts
@@ -16,3 +16,30 @@ export const testData = {
     postcode: "S4R 0M6",
   },
 };
+
+export const testCredentials: Array<{ email: string; password: string }> = [
+  {
+    email: String(Cypress.env("USER_1_EMAIL") || "pets.tester@hotmail.com"),
+    password: String(Cypress.env("USER_1_PASSWORD") || "qa!T3ster1"),
+  },
+  {
+    email: String(Cypress.env("USER_2_EMAIL") || "pets.tester1@hotmail.com"),
+    password: String(Cypress.env("USER_2_PASSWORD") || "qa!T3ster1"),
+  },
+  {
+    email: String(Cypress.env("USER_3_EMAIL") || "pets.tester2@hotmail.com"),
+    password: String(Cypress.env("USER_3_PASSWORD") || "qa!T3ster1"),
+  },
+  {
+    email: String(Cypress.env("USER_4_EMAIL") || "pets.tester3@hotmail.com"),
+    password: String(Cypress.env("USER_4_PASSWORD") || "qa!T3ster1"),
+  },
+  {
+    email: String(Cypress.env("USER_5_EMAIL") || "pets.tester4@hotmail.com"),
+    password: String(Cypress.env("USER_5_PASSWORD") || "qa!T3ster1"),
+  },
+  {
+    email: String(Cypress.env("USER_6_EMAIL") || "pets.tester5@hotmail.com"),
+    password: String(Cypress.env("USER_6_PASSWORD") || "qa!T3ster1"),
+  },
+];
