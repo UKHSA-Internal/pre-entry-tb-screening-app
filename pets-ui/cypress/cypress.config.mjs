3376fbeee467e1e46bc1270537e25f5d6b274932
edatoklo <167878066+edatoklo@users.noreply.github.com>
2025-02-26 15:38:33 +0000

    updates to TBBETA-63

--- pets-ui/cypress/cypress.config.mjs
+++ pets-ui/cypress/cypress.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-    supportFile: "support/e2e.ts",
-    specPattern: "e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/e2e.ts",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     experimentalStudio: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- pets-ui/cypress/support/e2e.ts
+++ pets-ui/cypress/support/e2e.ts
@@ -1,17 +1,1 @@
 import "cypress-mochawesome-reporter/register";
-
-// Handle the crypto object not being available in Firefox during tests
-beforeEach(() => {
-  // Ignore authentication errors related to crypto
-  cy.on("uncaught:exception", (err) => {
-    // Return false to prevent Cypress from failing the test
-    if (
-      err.message.includes("crypto_nonexistent") ||
-      err.message.includes("The crypto object or function is not available")
-    ) {
-      return false;
-    }
-    // We still want to fail on other exceptions
-    return true;
-  });
-});
