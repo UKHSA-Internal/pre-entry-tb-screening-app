0ca66f02942ba2c8dfa8d3c3cc9baa74add214cd
darasimi-ajewole <Darasimi.Ajewole@ukhsa.gov.uk>
2025-02-26 11:08:02 +0000

    TBBETA-63: Resolving another CI issue

--- pets-ui/cypress/cypress.config.mjs
+++ pets-ui/cypress/cypress.config.mjs
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
     supportFile: "support/e2e.ts",
-    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    specPattern: "e2e/**/*.cy.{js,jsx,ts,tsx}",
     experimentalStudio: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
