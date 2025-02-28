a8f46f04d4f456342a11bf19fe7869292011a6aa
darasimi-ajewole <Darasimi.Ajewole@ukhsa.gov.uk>
2025-02-28 07:37:00 +0000

    TBBETA-312: Added integration tests for Authoriser

--- pets-core-services/src/authoriser/constants.ts
+++ pets-core-services/src/authoriser/constants.ts
@@ -1,3 +1,5 @@
+import { assertEnvExists } from "../shared/config";
+
 export enum Roles {
   ApplicationWrite = "application.write",
   ApplicationRead = "application.read",
@@ -6,33 +8,35 @@ export enum Roles {
   ClinicsRead = "clinics.read",
   ImagingWrite = "imaging.write",
 }
+const AWS_ACCOUNT_ID = assertEnvExists(process.env.AWS_ACCOUNT_ID);
+const API_GATEWAY_ID = assertEnvExists(process.env.API_GATEWAY_ID);
 
 export const policyMapping: Record<Roles, string[]> = {
   [Roles.ApplicantsRead]: [
-    `arn:aws:execute-api:eu-west-2:${process.env.AWS_ACCOUNT_ID}:${process.env.API_GATEWAY_ID}/*/GET/applicant`,
-    `arn:aws:execute-api:eu-west-2:${process.env.AWS_ACCOUNT_ID}:${process.env.API_GATEWAY_ID}/*/GET/applicant/*`,
+    `arn:aws:execute-api:eu-west-2:${AWS_ACCOUNT_ID}:${API_GATEWAY_ID}/*/GET/applicant`,
+    `arn:aws:execute-api:eu-west-2:${AWS_ACCOUNT_ID}:${API_GATEWAY_ID}/*/GET/applicant/*`,
   ],
   [Roles.ApplicantsWrite]: [
-    `arn:aws:execute-api:eu-west-2:${process.env.AWS_ACCOUNT_ID}:${process.env.API_GATEWAY_ID}/*/POST/applicant`,
-    `arn:aws:execute-api:eu-west-2:${process.env.AWS_ACCOUNT_ID}:${process.env.API_GATEWAY_ID}/*/POST/applicant/*`,
-    `arn:aws:execute-api:eu-west-2:${process.env.AWS_ACCOUNT_ID}:${process.env.API_GATEWAY_ID}/*/PUT/applicant`,
-    `arn:aws:execute-api:eu-west-2:${process.env.AWS_ACCOUNT_ID}:${process.env.API_GATEWAY_ID}/*/PUT/applicant/*`,
+    `arn:aws:execute-api:eu-west-2:${AWS_ACCOUNT_ID}:${API_GATEWAY_ID}/*/POST/applicant`,
+    `arn:aws:execute-api:eu-west-2:${AWS_ACCOUNT_ID}:${API_GATEWAY_ID}/*/POST/applicant/*`,
+    `arn:aws:execute-api:eu-west-2:${AWS_ACCOUNT_ID}:${API_GATEWAY_ID}/*/PUT/applicant`,
+    `arn:aws:execute-api:eu-west-2:${AWS_ACCOUNT_ID}:${API_GATEWAY_ID}/*/PUT/applicant/*`,
   ],
   [Roles.ApplicationRead]: [
-    `arn:aws:execute-api:eu-west-2:${process.env.AWS_ACCOUNT_ID}:${process.env.API_GATEWAY_ID}/*/GET/application`,
-    `arn:aws:execute-api:eu-west-2:${process.env.AWS_ACCOUNT_ID}:${process.env.API_GATEWAY_ID}/*/GET/application/*`,
+    `arn:aws:execute-api:eu-west-2:${AWS_ACCOUNT_ID}:${API_GATEWAY_ID}/*/GET/application`,
+    `arn:aws:execute-api:eu-west-2:${AWS_ACCOUNT_ID}:${API_GATEWAY_ID}/*/GET/application/*`,
   ],
   [Roles.ApplicationWrite]: [
-    `arn:aws:execute-api:eu-west-2:${process.env.AWS_ACCOUNT_ID}:${process.env.API_GATEWAY_ID}/*/POST/application`,
-    `arn:aws:execute-api:eu-west-2:${process.env.AWS_ACCOUNT_ID}:${process.env.API_GATEWAY_ID}/*/POST/application/*`,
-    `arn:aws:execute-api:eu-west-2:${process.env.AWS_ACCOUNT_ID}:${process.env.API_GATEWAY_ID}/*/PUT/application`,
-    `arn:aws:execute-api:eu-west-2:${process.env.AWS_ACCOUNT_ID}:${process.env.API_GATEWAY_ID}/*/PUT/application/*`,
+    `arn:aws:execute-api:eu-west-2:${AWS_ACCOUNT_ID}:${API_GATEWAY_ID}/*/POST/application`,
+    `arn:aws:execute-api:eu-west-2:${AWS_ACCOUNT_ID}:${API_GATEWAY_ID}/*/POST/application/*`,
+    `arn:aws:execute-api:eu-west-2:${AWS_ACCOUNT_ID}:${API_GATEWAY_ID}/*/PUT/application`,
+    `arn:aws:execute-api:eu-west-2:${AWS_ACCOUNT_ID}:${API_GATEWAY_ID}/*/PUT/application/*`,
   ],
   [Roles.ImagingWrite]: [
-    `arn:aws:execute-api:eu-west-2:${process.env.AWS_ACCOUNT_ID}:${process.env.API_GATEWAY_ID}/*/POST/application/*`, // TODO: Change this when 506 is picked
+    `arn:aws:execute-api:eu-west-2:${AWS_ACCOUNT_ID}:${API_GATEWAY_ID}/*/POST/application/*`, // TODO: Change this when 506 is picked
   ],
   [Roles.ClinicsRead]: [
-    `arn:aws:execute-api:eu-west-2:${process.env.AWS_ACCOUNT_ID}:${process.env.API_GATEWAY_ID}/*/GET/clinics`,
-    `arn:aws:execute-api:eu-west-2:${process.env.AWS_ACCOUNT_ID}:${process.env.API_GATEWAY_ID}/*/GET/clinics/*`,
+    `arn:aws:execute-api:eu-west-2:${AWS_ACCOUNT_ID}:${API_GATEWAY_ID}/*/GET/clinics`,
+    `arn:aws:execute-api:eu-west-2:${AWS_ACCOUNT_ID}:${API_GATEWAY_ID}/*/GET/clinics/*`,
   ],
 };
