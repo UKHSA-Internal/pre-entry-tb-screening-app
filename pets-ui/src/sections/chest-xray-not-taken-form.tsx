202372a202ef62ebee2c95f7508ab628df0d7d5c
Mikee Chong <mikee.chong@accenture.com>
2025-03-03 13:43:19 +0000

    remove linting issue

--- pets-ui/src/sections/chest-xray-not-taken-form.tsx
+++ pets-ui/src/sections/chest-xray-not-taken-form.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { useRef } from "react";
 import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
