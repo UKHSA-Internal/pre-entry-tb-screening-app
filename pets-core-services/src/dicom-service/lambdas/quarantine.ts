8c53f56247e5be8e317dd7b114e25657c5ee381d
Jaroslaw Michalski <jaroslaw.m.michalski@phe.gov.uk>
2025-03-06 12:47:13 +0000

    add code to test copying files (WIP)

--- pets-core-services/src/dicom-service/lambdas/quarantine.ts
+++ pets-core-services/src/dicom-service/lambdas/quarantine.ts
@@ -1,18 +1,65 @@
+import { CopyObjectCommand, CopyObjectCommandInput } from "@aws-sdk/client-s3";
 import { EventBridgeEvent } from "aws-lambda";
 
 import awsClients from "../../shared/clients/aws";
 import { assertEnvExists } from "../../shared/config";
 import { logger } from "../../shared/logger";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const QUARANTINE_BUCKET = assertEnvExists(process.env.QUARANTINE_BUCKET);
 
 export const handler = (event: EventBridgeEvent<string, object>) => {
-  logger.info({ event }, "Received Quarantine event");
+  const { detail } = event;
+  let bucketName: string | undefined;
+  let fileName: string | undefined;
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  if (detail) {
+    // @ts-expect-error Property 'scanResultDetails' does not exist on type 'object'
+    if (detail?.s3ObjectDetails) {
+      // @ts-expect-error Property 'scanResultDetails' does not exist on type 'object'
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const s3ObjDetails = detail.s3ObjectDetails;
+
+      if (s3ObjDetails) {
+        logger.info("s3ObjectDetails:", s3ObjDetails);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+        bucketName = s3ObjDetails?.bucketName;
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+        fileName = s3ObjDetails?.objectKey;
+
+        if (typeof bucketName !== "string" || typeof fileName !== "string") {
+          logger.info("EventBridge event doesn't contain correct bucketName or objectKey");
+
+          return;
+        }
+      }
+    }
+  } else {
+    logger.info("EventBridge event object doesn't contain 'detail' property");
+
+    return;
+  }
+
   const { s3Client } = awsClients;
-  /**
-   * TODO: check and remove malicious images
-   */
+
+  // @ts-expect-error Property 'scanResultDetails' does not exist on type 'object'.
+  if (detail?.scanResultDetails !== "NO_THREATS_FOUND") {
+    logger.info("EventBridge rule is not properly set");
+  } else {
+    logger.info({ event }, "Received Quarantine event");
+
+    const params: CopyObjectCommandInput = {
+      Bucket: QUARANTINE_BUCKET,
+      CopySource: `${bucketName}/${fileName}`,
+      Key: fileName,
+    };
+
+    const copyCommand = new CopyObjectCommand(params);
+    const promise = s3Client.send(copyCommand);
+    const response = promise.then((result) => result).catch((error) => logger.error(error));
+
+    logger.info("Result of copying a file:", response);
+    logger.info("response type:", typeof response);
+
+    return;
+  }
 };
