fc3564b8bf29cd6ab3e6fc504a73e740aa7ad2f3
Will Tipper <will.tipper@hotmail.co.uk>
2025-03-06 14:38:47 +0000

    Create tb cert schemas

--- pets-core-services/src/application-service/types/zod-schema.ts
+++ pets-core-services/src/application-service/types/zod-schema.ts
@@ -109,10 +109,38 @@ export const MedicalScreeningResponseSchema = MedicalScreeningRequestSchema.exte
   }),
 });
 
+export const TbCerificationRequestSchema = z.object({
+  certificateIssued: z.nativeEnum(YesOrNo).openapi({
+    description: "Whether a clearance certificate has been issued",
+  }),
+  certificateComments: z.string().openapi({
+    description: "Physican's comments",
+  }),
+  certificateIssueDate: z.string().date().openapi({
+    description: "Date of certificate issue in ISO format",
+  }),
+  certificateNumber: z.string().openapi({
+    description: "Clearance certificate number",
+  }),
+});
+
+export const TbCertificationResponseSchema = TbCerificationRequestSchema.extend({
+  applicationId: z.string().openapi({
+    description: "ID of application",
+  }),
+  dateCreated: z.string().date().openapi({
+    description: "Creation Date in UTC timezone",
+  }),
+  status: z.nativeEnum(TaskStatus).openapi({
+    description: "Status of Task",
+  }),
+});
+
 export const ApplicationSchema = z.object({
   applicationId: z.string().openapi({
     description: "application id",
   }),
   travelInformation: TravelInformationResponseSchema,
   medicalScreening: MedicalScreeningResponseSchema,
+  tbCertification: TbCertificationResponseSchema,
 });
