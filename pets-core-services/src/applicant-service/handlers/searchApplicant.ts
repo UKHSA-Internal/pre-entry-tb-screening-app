f3e5e6b9801a2aef849943596d92b6378efe8fce
darasimi-ajewole <Darasimi.Ajewole@ukhsa.gov.uk>
2025-03-20 14:20:59 +0000

    TBBETA-506: Change API notation

--- pets-core-services/src/applicant-service/handlers/searchApplicant.ts
+++ pets-core-services/src/applicant-service/handlers/searchApplicant.ts
@@ -38,7 +38,7 @@ export const searchApplicantHandler = async (event: SearchApplicantEvent) => {
       parsedHeaders.passportnumber,
     );
 
-    if (!applicants.length) return createHttpResponse(404, { message: "Applicant does not exist" });
+    if (!applicants.length) return createHttpResponse(204, []);
 
     // Note: This check would need to be modified Post-MVP, For MVP, only a single applicant should exist for passport and country combination
     if (applicants.length > 1) {
