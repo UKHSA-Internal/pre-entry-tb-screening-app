ebfece8bf24e2bec1299349cb6467b079b947a38
LBotcherbyUKHSA <lewis.botcherby@ukhsa.gov.uk>
2025-03-21 12:32:19 +0000

    Feature/tbbeta 658 - "Skip to main content" link (#388)
    * created initial skipLink component
    
    * reworked skipLink within container, added css and testing
    
    * fixed linting
    
    * added tabindex to main and expanded testing
    
    * fixed linting
    
    * added css to remove outline for tabindex -1
    
    * Remove braces in skipLink, update href to main-content
    
    * Fix test
    
    * Direct focus to page start on load so that keyboard users can always access skip link
    
    ---------
    
    Co-authored-by: Lewis Botcherby <lewis.botcherby@accenture.com>
    Co-authored-by: Will <100358610+WJTipper@users.noreply.github.com>
    Co-authored-by: Will Tipper <will.tipper@hotmail.co.uk>

---
+++ pets-ui/src/components/skipLink/skipLink.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+
+import SkipLink from "./skipLink";
+
+describe("SkipLink component", () => {
+  it("renders correctly", () => {
+    render(<SkipLink />);
+    expect(screen.getByText("Skip to main content")).toBeInTheDocument();
+    expect(screen.getByText("Skip to main content")).toHaveAttribute("href", "#main-content");
+  });
+});
