5fa09a70ff587f10f608991a70c7dac23818875e
Will Tipper <will.tipper@hotmail.co.uk>
2025-03-25 16:26:45 +0000

    Add test for tb cert not issued to lambda tests

--- pets-core-services/src/application-service/lambdas/application.spec.ts
+++ pets-core-services/src/application-service/lambdas/application.spec.ts
@@ -270,50 +270,72 @@ describe("Test for Application Lambda", () => {
     });
   });
 
-  test("Validating creating TB certificate successfully", async () => {
-    // Arrange
-    const event: PetsAPIGatewayProxyEvent = {
-      ...mockAPIGwEvent,
-      resource: "/application/{applicationId}/tb-certificate",
-      path: `/application/${seededApplications[0].applicationId}/tb-certificate`,
-      httpMethod: "POST",
-    };
+  describe("TB certificate", () => {
+    test("Validating creating TB certificate successfully", async () => {
+      // Arrange
+      const event: PetsAPIGatewayProxyEvent = {
+        ...mockAPIGwEvent,
+        resource: "/application/{applicationId}/tb-certificate",
+        path: `/application/${seededApplications[0].applicationId}/tb-certificate`,
+        httpMethod: "POST",
+      };
 
-    // Act
-    const response: APIGatewayProxyResult = await handler(event, context);
+      // Act
+      const response: APIGatewayProxyResult = await handler(event, context);
 
-    // Assert
-    expect(response.statusCode).toBe(400);
-    expect(JSON.parse(response.body)).toMatchObject({
-      message: "Request Body failed validation",
-      validationError: {
-        certificateComments: ["Required"],
-        certificateIssueDate: ["Required"],
-        certificateIssued: ["Required"],
-        certificateNumber: ["Required"],
-      },
+      // Assert
+      expect(response.statusCode).toBe(400);
+      expect(JSON.parse(response.body)).toMatchObject({
+        message: "Request Body failed validation",
+        validationError: {
+          certificateComments: ["Required"],
+          certificateIssueDate: ["Required"],
+          certificateIssued: ["Required"],
+          certificateNumber: ["Required"],
+        },
+      });
     });
-  });
 
-  test("Saving TB certificate successfully", async () => {
-    // Arrange;
-    const event: PetsAPIGatewayProxyEvent = {
-      ...mockAPIGwEvent,
-      resource: "/application/{applicationId}/tb-certificate",
-      path: `/application/${seededApplications[0].applicationId}/tb-certificate`,
-      httpMethod: "POST",
-      body: JSON.stringify({
-        certificateComments: "comments",
-        certificateIssueDate: "2025-01-01",
-        certificateIssued: YesOrNo.Yes,
-        certificateNumber: "123456",
-      }),
-    };
+    test("Saving TB certificate successfully when issued", async () => {
+      // Arrange;
+      const event: PetsAPIGatewayProxyEvent = {
+        ...mockAPIGwEvent,
+        resource: "/application/{applicationId}/tb-certificate",
+        path: `/application/${seededApplications[0].applicationId}/tb-certificate`,
+        httpMethod: "POST",
+        body: JSON.stringify({
+          certificateComments: "comments",
+          certificateIssueDate: "2025-01-01",
+          certificateIssued: YesOrNo.Yes,
+          certificateNumber: "123456",
+        }),
+      };
 
-    // Act
-    const response: APIGatewayProxyResult = await handler(event, context);
+      // Act
+      const response: APIGatewayProxyResult = await handler(event, context);
 
-    // Assert
-    expect(response.statusCode).toBe(200);
+      // Assert
+      expect(response.statusCode).toBe(200);
+    });
+
+    test("Saving TB certificate successfully when not issued", async () => {
+      // Arrange;
+      const event: PetsAPIGatewayProxyEvent = {
+        ...mockAPIGwEvent,
+        resource: "/application/{applicationId}/tb-certificate",
+        path: `/application/${seededApplications[0].applicationId}/tb-certificate`,
+        httpMethod: "POST",
+        body: JSON.stringify({
+          certificateComments: "comments",
+          certificateIssued: YesOrNo.No,
+        }),
+      };
+
+      // Act
+      const response: APIGatewayProxyResult = await handler(event, context);
+
+      // Assert
+      expect(response.statusCode).toBe(200);
+    });
   });
 });
