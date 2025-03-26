dceb37cdce1c641f11067a26cdd895701113689c
Will Tipper <will.tipper@hotmail.co.uk>
2025-03-26 13:23:59 +0000

    Update progress tracker: - refactor tasks into component - use applicant data header component - add tasks for travel, medical screening, chest x-ray and tb cert

--- pets-ui/src/sections/progress-tracker.tsx
+++ pets-ui/src/sections/progress-tracker.tsx
@@ -1,62 +1,95 @@
 import { Link, useNavigate } from "react-router-dom";
 
+import ApplicantDataHeader from "@/components/applicantDataHeader/applicantDataHeader";
 import Button from "@/components/button/button";
 import { selectApplicant } from "@/redux/applicantSlice";
+import { selectChestXray } from "@/redux/chestXraySlice";
 import { useAppSelector } from "@/redux/hooks";
+import { selectMedicalScreening } from "@/redux/medicalScreeningSlice";
+import { selectTbCertificate } from "@/redux/tbCertificateSlice";
+import { selectTravel } from "@/redux/travelSlice";
 import { ApplicationStatus, ButtonType } from "@/utils/enums";
 
 const ProgressTracker = () => {
   const navigate = useNavigate();
 
   const applicantData = useAppSelector(selectApplicant);
+  const travelData = useAppSelector(selectTravel);
+  const medicalScreeningData = useAppSelector(selectMedicalScreening);
+  const chestXrayData = useAppSelector(selectChestXray);
+  const tbCertificateData = useAppSelector(selectTbCertificate);
+
+  interface TaskProps {
+    description: string;
+    status: ApplicationStatus;
+    linkWhenIncomplete: string;
+    linkWhenComplete: string;
+  }
+
+  const Task = (props: TaskProps) => {
+    return (
+      <li className="govuk-task-list__item govuk-task-list__item--with-link">
+        <div className="govuk-task-list__name-and-hint">
+          {props.status == ApplicationStatus.INCOMPLETE && (
+            <Link className="govuk-link govuk-task-list__link" to={props.linkWhenIncomplete}>
+              {props.description}
+            </Link>
+          )}
+          {props.status == ApplicationStatus.COMPLETE && (
+            <Link className="govuk-link govuk-task-list__link" to={props.linkWhenComplete}>
+              {props.description}
+            </Link>
+          )}
+        </div>
+        {props.status == ApplicationStatus.INCOMPLETE && (
+          <div className="govuk-task-list__status">
+            <strong className="govuk-tag govuk-tag--blue">Incomplete</strong>
+          </div>
+        )}
+        {props.status == ApplicationStatus.COMPLETE && (
+          <div className="govuk-task-list__status">Completed</div>
+        )}
+      </li>
+    );
+  };
 
   return (
     <div>
-      <div>
-        <dl className="govuk-summary-list">
-          <div className="govuk-summary-list__row">
-            <dt className="govuk-summary-list__key">Name</dt>
-            <dd className="govuk-summary-list__value">{applicantData.fullName}</dd>
-          </div>
-          <div className="govuk-summary-list__row">
-            <dt className="govuk-summary-list__key">Date of birth</dt>
-            <dd className="govuk-summary-list__value">
-              {applicantData.dateOfBirth.day}/{applicantData.dateOfBirth.month}/
-              {applicantData.dateOfBirth.year}
-            </dd>
-          </div>
-          <div className="govuk-summary-list__row">
-            <dt className="govuk-summary-list__key">Passport number</dt>
-            <dd className="govuk-summary-list__value">{applicantData.passportNumber}</dd>
-          </div>
-        </dl>
-      </div>
+      <ApplicantDataHeader applicantData={applicantData} />
 
       <p className="govuk-body">Complete all sections.</p>
 
       <ul className="govuk-task-list">
-        <li className="govuk-task-list__item govuk-task-list__item--with-link">
-          <div className="govuk-task-list__name-and-hint">
-            {applicantData.status == ApplicationStatus.INCOMPLETE && (
-              <Link className="govuk-link govuk-task-list__link" to="/contact">
-                Visa applicant details
-              </Link>
-            )}
-            {applicantData.status == ApplicationStatus.COMPLETE && (
-              <Link className="govuk-link govuk-task-list__link" to="/applicant-summary">
-                Visa applicant details
-              </Link>
-            )}
-          </div>
-          {applicantData.status == ApplicationStatus.INCOMPLETE && (
-            <div className="govuk-task-list__status">
-              <strong className="govuk-tag govuk-tag--blue">Incomplete</strong>
-            </div>
-          )}
-          {applicantData.status == ApplicationStatus.COMPLETE && (
-            <div className="govuk-task-list__status">Completed</div>
-          )}
-        </li>
+        <Task
+          description="Visa applicant details"
+          status={applicantData.status}
+          linkWhenIncomplete="/contact"
+          linkWhenComplete="/applicant-summary"
+        />
+        <Task
+          description="Travel information"
+          status={travelData.status}
+          linkWhenIncomplete="/travel-details"
+          linkWhenComplete="/travel-summary"
+        />
+        <Task
+          description="Medical history and TB symptoms"
+          status={medicalScreeningData.status}
+          linkWhenIncomplete="/medical-screening"
+          linkWhenComplete="/medical-summary"
+        />
+        <Task
+          description="Radiological outcome"
+          status={chestXrayData.status}
+          linkWhenIncomplete="/chest-xray-question"
+          linkWhenComplete="/chest-xray-summary"
+        />
+        <Task
+          description="TB certificate declaration"
+          status={tbCertificateData.status}
+          linkWhenIncomplete="/tb-certificate-declaration"
+          linkWhenComplete="/tb-certificate-summary"
+        />
       </ul>
 
       <p className="govuk-body">You cannot currently log sputum test information in this system.</p>
