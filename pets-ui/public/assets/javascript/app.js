1f46ede5993594956d6857b3882ee9826a428030
mikeech <mikee.chong@ukhsa.gov.uk>
2025-03-28 09:27:39 +0000

    remove unnecessary imports

--- pets-ui/public/assets/javascript/app.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-app.use(
-  "/assets",
-  express.static(path.join(__dirname, "/node_modules/govuk-frontend/dist/govuk/assets")),
-);
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
