e26698b4a283bf45eb54cfa6431ad487d0af30eb
Will Tipper <will.tipper@ukhsa.gov.uk>
2025-04-01 14:26:19 +0100

    Update error page content

--- pets-ui/src/pages/error-page.tsx
+++ pets-ui/src/pages/error-page.tsx
@@ -6,7 +6,15 @@ import Heading from "@/components/heading/heading";
 export default function ErrorPage() {
   return (
     <Container title={"Error"}>
-      <Heading level={1} size="l" title="An error occurred" />
+      <Heading
+        level={1}
+        size="l"
+        title="Sorry, there is a problem with the Complete UK pre-entry health screening service"
+      />
+      <p className="govuk-body">Try again now or later.</p>
+      <p className="govuk-body">
+        Contact uktbscreeningsupport@ukhsa.gov.uk if the problem continues.
+      </p>
     </Container>
   );
 }
