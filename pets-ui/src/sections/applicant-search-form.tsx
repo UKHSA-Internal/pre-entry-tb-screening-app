b538e08577b93dcbe68b11486c8dc5fc945b4ed6
Will Tipper <will.tipper@ukhsa.gov.uk>
2025-04-02 11:34:59 +0100

    Fix isLoading initial state

--- pets-ui/src/sections/applicant-search-form.tsx
+++ pets-ui/src/sections/applicant-search-form.tsx
@@ -37,7 +37,7 @@ const ApplicantSearchForm = () => {
   const methods = useForm<ApplicantSearchFormType>({ reValidateMode: "onSubmit" });
   const dispatch = useAppDispatch();
 
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     dispatch(clearApplicantDetails());
