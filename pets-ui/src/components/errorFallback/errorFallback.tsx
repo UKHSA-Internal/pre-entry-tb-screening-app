b77d5154f683f2ced8cc463c5664f890178986a6
Will Tipper <will.tipper@ukhsa.gov.uk>
2025-04-07 11:57:30 +0100

    Remove navigate from errorFallback component

--- pets-ui/src/components/errorFallback/errorFallback.tsx
+++ pets-ui/src/components/errorFallback/errorFallback.tsx
@@ -1,14 +1,5 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router";
-
 import ErrorPage from "@/pages/error-page";
 
-export default function ErrorFallback() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("/error");
-  }, [navigate]);
-
+export const ErrorFallback = () => {
   return <ErrorPage />;
-}
+};
