cae85adb2adeb8aab82a0ff32479be4270553c7a
mikeech <mikee.chong@ukhsa.gov.uk>
2025-04-22 16:43:23 +0100

    added fieldWrapper component to reduce code repetition

--- pets-ui/src/components/checkbox/checkbox.tsx
+++ pets-ui/src/components/checkbox/checkbox.tsx
@@ -1,9 +1,9 @@
 import "./checkbox.scss";
 
-import { useEffect, useState } from "react";
 import { useFormContext } from "react-hook-form";
 
-import Heading, { HeadingSize } from "../heading/heading";
+import FieldWrapper from "../fieldWrapper/fieldWrapper";
+import { HeadingSize } from "../heading/heading";
 import { checkboxHeadingStyles } from "./checkbox.styles";
 
 export interface CheckboxProps {
@@ -23,113 +23,79 @@ export interface CheckboxProps {
   headingStyle?: React.CSSProperties;
 }
 
-export default function Checkbox({
-  headingLevel = 2,
-  headingSize = "m",
-  ...props
-}: Readonly<CheckboxProps>) {
-  const answerOptions: string[] = props.answerOptions;
+export default function Checkbox(props: Readonly<CheckboxProps>) {
+  const { register } = useFormContext();
+
+  const answerOptions: string[] = [...props.answerOptions];
+  const exclusiveOptions = [...(props.exclusiveAnswerOptions ?? [])];
+
   if (props.sortAnswersAlphabetically) {
     answerOptions.sort((a, b) => a.localeCompare(b));
+    exclusiveOptions.sort((a, b) => a.localeCompare(b));
   }
-  const exclusiveAnswerOptions: string[] = props.exclusiveAnswerOptions ?? [];
-  if (props.sortAnswersAlphabetically) {
-    exclusiveAnswerOptions.sort((a, b) => a.localeCompare(b));
-  }
-
-  const { register } = useFormContext();
-  const [errorText, setErrorText] = useState("");
-  const [wrapperClass, setWrapperClass] = useState("govuk-form-group");
-
-  useEffect(() => {
-    setErrorText(props.errorMessage);
-    setWrapperClass("govuk-form-group" + (props.errorMessage ? " govuk-form-group--error" : ""));
-  }, [props.errorMessage]);
 
   return (
-    <div id={props.id} className={wrapperClass}>
-      <fieldset
-        className="govuk-fieldset"
-        aria-describedby={props.heading && props.label && `${props.id}-label`}
-      >
-        <legend className="govuk-fieldset__legend">
-          {props.heading ? (
-            <Heading
-              title={props.heading}
-              level={headingLevel}
-              size={headingSize}
-              style={{
-                ...(props.heading && checkboxHeadingStyles),
-              }}
-            />
-          ) : (
-            props.label
-          )}
-        </legend>
-
-        {props.heading && props.label && (
-          <div className="govuk-label" id={`${props.id}-label`}>
-            {props.label}
-          </div>
-        )}
-        {props.hint && <div className="govuk-hint">{props.hint}</div>}
-        {errorText && (
-          <p className="govuk-error-message">
-            <span className="govuk-visually-hidden">Error:</span> {errorText}
-          </p>
-        )}
-        <div className="govuk-checkboxes" data-module="govuk-checkboxes">
-          {answerOptions.map((answerOption: string, index: number) => {
-            const optionId = `${props.id}-option-${index}`;
-            return (
-              <div className="govuk-checkboxes__item" key={optionId}>
+    <FieldWrapper
+      id={props.id}
+      heading={props.heading}
+      label={props.label}
+      hint={props.hint}
+      errorMessage={props.errorMessage}
+      headingLevel={props.headingLevel}
+      headingSize={props.headingSize}
+      headingStyle={{
+        ...(props.heading && checkboxHeadingStyles),
+        ...props.headingStyle,
+      }}
+    >
+      <div className="govuk-checkboxes" data-module="govuk-checkboxes">
+        {answerOptions.map((option, index) => {
+          const optionId = `${props.id}-option-${index}`;
+          return (
+            <div className="govuk-checkboxes__item" key={optionId}>
+              <input
+                id={optionId}
+                className="govuk-checkboxes__input"
+                type="checkbox"
+                data-testid={props.id}
+                value={option}
+                {...register(props.formValue, {
+                  required: props.required,
+                })}
+                defaultChecked={props.defaultValue?.includes(option)}
+              />
+              <label className="govuk-label govuk-checkboxes__label" htmlFor={optionId}>
+                {option}
+              </label>
+            </div>
+          );
+        })}
+        {exclusiveOptions.map((option, index) => {
+          const optionId = `${props.id}-exclusive-${index}`;
+          return (
+            <div key={optionId}>
+              <div className="govuk-checkboxes__divider">or</div>
+              <div className="govuk-checkboxes__item">
                 <input
                   id={optionId}
                   className="govuk-checkboxes__input"
                   type="checkbox"
                   data-testid={props.id}
-                  value={answerOption}
+                  value={option}
                   {...register(props.formValue, {
                     required: props.required,
                   })}
-                  defaultChecked={props.defaultValue?.includes(answerOption)}
+                  data-behaviour="exclusive"
+                  defaultChecked={props.defaultValue?.includes(option)}
                 />
                 <label className="govuk-label govuk-checkboxes__label" htmlFor={optionId}>
-                  {answerOption}
+                  {option}
                 </label>
               </div>
-            );
-          })}
-          {exclusiveAnswerOptions.map((exclusiveAnswerOption: string, index: number) => {
-            const exclusiveOptionId = `${props.id}-exclusive-${index}`;
-            return (
-              <div key={exclusiveOptionId}>
-                <div className="govuk-checkboxes__divider">or</div>
-                <div className="govuk-checkboxes__item">
-                  <input
-                    id={exclusiveOptionId}
-                    className="govuk-checkboxes__input"
-                    type="checkbox"
-                    data-testid={props.id}
-                    value={exclusiveAnswerOption}
-                    {...register(props.formValue, {
-                      required: props.required,
-                    })}
-                    data-behaviour="exclusive"
-                    defaultChecked={props.defaultValue?.includes(exclusiveAnswerOption)}
-                  />
-                  <label
-                    className="govuk-label govuk-checkboxes__label"
-                    htmlFor={exclusiveOptionId}
-                  >
-                    {exclusiveAnswerOption}
-                  </label>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </fieldset>
-    </div>
+            </div>
+          );
+        })}
+      </div>
+    </FieldWrapper>
   );
 }
---
+++ pets-ui/src/components/fieldWrapper/fieldWrapper.tsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+
+import Heading, { HeadingSize } from "../heading/heading";
+
+interface FieldWrapperProps {
+  id: string;
+  heading?: string;
+  label?: string;
+  hint?: string;
+  headingLevel?: 1 | 2 | 3 | 4;
+  headingSize?: HeadingSize;
+  headingStyle?: React.CSSProperties;
+  children: React.ReactNode;
+  labelStyle?: React.CSSProperties;
+  useFieldset?: boolean;
+  errorMessage: string;
+}
+
+export default function FieldWrapper({
+  headingLevel = 2,
+  headingSize = "m",
+  useFieldset = true,
+  ...props
+}: FieldWrapperProps) {
+  const [errorText, setErrorText] = useState("");
+  const [wrapperClass, setWrapperClass] = useState("govuk-form-group");
+
+  useEffect(() => {
+    setErrorText(props.errorMessage);
+    setWrapperClass(wrapperClass + (props.errorMessage ? " govuk-form-group--error" : ""));
+  }, [props.errorMessage, wrapperClass]);
+
+  return (
+    <div id={props.id} className={wrapperClass}>
+      {/* conditional fieldset as textarea, tables/non-input fields will not require fieldset... unless are excluding them from this as they are not technically 'field elements' */}
+      {useFieldset ? (
+        <fieldset
+          className="govuk-fieldset"
+          aria-describedby={
+            props.hint
+              ? `${props.id}-hint`
+              : props.heading && props.label
+                ? `${props.id}-label`
+                : undefined
+          }
+        >
+          <legend className="govuk-fieldset__legend">
+            {props.heading ? (
+              <Heading
+                title={props.heading}
+                level={headingLevel}
+                size={headingSize}
+                style={props.headingStyle}
+              />
+            ) : (
+              props.label
+            )}
+          </legend>
+          {props.heading && props.label && (
+            <div className="govuk-label" id={`${props.id}-label`} style={props.labelStyle}>
+              {props.label}
+            </div>
+          )}
+          {props.hint && (
+            <div className="govuk-hint" id={`${props.id}-hint`}>
+              {props.hint}
+            </div>
+          )}
+          {errorText && (
+            <p className="govuk-error-message">
+              <span className="govuk-visually-hidden">Error:</span> {errorText}
+            </p>
+          )}
+          {props.children}
+        </fieldset>
+      ) : (
+        <>
+          {errorText && (
+            <p className="govuk-error-message">
+              <span className="govuk-visually-hidden">Error:</span> {props.errorMessage}
+            </p>
+          )}
+          {props.children}
+        </>
+      )}
+    </div>
+  );
+}
