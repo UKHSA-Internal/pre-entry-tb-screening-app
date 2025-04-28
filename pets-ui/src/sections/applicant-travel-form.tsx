0bb32186fef055e011a6c41274e9a2145ab9311c
Will <100358610+WJTipper@users.noreply.github.com>
2025-04-28 12:32:53 +0100

    TBBETA-876: make optional fields clear to users (#441)
    * wip
    
    * Add 'optional' suffixes to form fields & update wording to match designs
    
    * Fix test
    
    * Fix email required message
    
    * Fix test
    
    * Revert temp changes
    
    * Add missing emptyValueText to cxr notes
    
    * Fix certificate further details title
    
    * Fix test
    
    ---------
    
    Co-authored-by: mikeech <mikee.chong@ukhsa.gov.uk>

--- pets-ui/src/sections/applicant-travel-form.tsx
+++ pets-ui/src/sections/applicant-travel-form.tsx
@@ -81,7 +81,7 @@ const ApplicantTravelForm = () => {
           <FreeText
             id="address-1"
             label="Address line 1"
-            heading="Applicant's UK address"
+            heading="UK address (optional)"
             headingStyle={{ marginBottom: 20 }}
             errorMessage={errors?.applicantUkAddress1?.message ?? ""}
             formValue="applicantUkAddress1"
@@ -95,7 +95,7 @@ const ApplicantTravelForm = () => {
         <div ref={addressLine2Ref}>
           <FreeText
             id="address-2"
-            label="Address line 2 (optional)"
+            label="Address line 2"
             errorMessage={errors?.applicantUkAddress2?.message ?? ""}
             formValue="applicantUkAddress2"
             required={false}
@@ -135,7 +135,7 @@ const ApplicantTravelForm = () => {
           <FreeText
             id="mobile-number"
             errorMessage={errors?.ukMobileNumber?.message ?? ""}
-            heading="Applicant's UK phone number"
+            heading="Phone number (optional)"
             formValue="ukMobileNumber"
             required="Enter UK mobile number"
             patternValue={formRegex.numbersOnly}
@@ -148,9 +148,9 @@ const ApplicantTravelForm = () => {
           <FreeText
             id="email"
             errorMessage={errors?.ukEmail?.message ?? ""}
-            heading="Applicant's UK email"
+            heading="Email address (optional)"
             formValue="ukEmail"
-            required="Enter UK email address"
+            required="Enter email address"
             patternValue={formRegex.emailAddress}
             patternError="Email must be in correct format"
             defaultValue={travelData.ukEmail}
