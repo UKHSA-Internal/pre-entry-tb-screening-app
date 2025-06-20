b6ed516800e32b203496ca12c189ea5bb5fdb5ee
jasnave-ukhsa <jasnave.pinto@ukhsa.gov.uk>
2025-06-20 15:30:28 +0100

    TBBETA-897: sputum backend (#459)
    * TBBETA-897: sputum models and handler
    
    * TBETA-897: fix  lint issues
    
    * TBBETA-897: cleanup and schema updates for sputum
    
    * TBBETA-897: unit tests
    
    * add lambda tests (TBBETA-981)
    
    * fix test, add 1 more (models, TBBETA-897)
    
    * TBBETA-897: Fix unit tests
    
    * TBEETA-897: cleanup
    
    * TBBETA-897: fix unit tests
    
    * modify test (application sputum handler)
    
    * TBETA-897: code refactor
    
    * TBBETA-897: Fix dateCreated and version bug
    
    * fix minor mistake and add test for it
    
    * clean mergedSamples data
    
    * TBBETA-897: Fix createdBy and unit tests
    
    * add Inconclusive to smear/culture result options
    
    * revert pnpm-lock.yaml changes
    
    * update pnpm-lock.yaml
    
    ---------
    
    Co-authored-by: Jaroslaw Michalski <jaroslaw.m.michalski@ukhsa.gov.uk>
    Co-authored-by: Jarosław Michalski <22432929+jaro-m@users.noreply.github.com>

---
+++ pets-core-services/src/application-service/handlers/save-sputum-details.ts
@@ -0,0 +1,61 @@
+import { z } from "zod";
+
+import { createHttpResponse } from "../../shared/http";
+import { logger } from "../../shared/logger";
+import { PetsAPIGatewayProxyEvent } from "../../shared/types";
+import { SputumDetails, SputumDetailsDbOps } from "../models/sputum-details";
+import { SputumRequestSchema } from "../types/zod-schema";
+
+export type SputumRequestSchema = z.infer<typeof SputumRequestSchema>;
+
+export type SaveSputumDetailsEvent = PetsAPIGatewayProxyEvent & {
+  parsedBody?: SputumRequestSchema;
+};
+
+export const saveSputumDetailsHandler = async (event: SaveSputumDetailsEvent) => {
+  try {
+    const applicationId = decodeURIComponent(event.pathParameters?.["applicationId"] ?? "").trim();
+
+    logger.info({ applicationId }, "Save Sputum Details handler triggered");
+
+    const { parsedBody } = event;
+
+    if (!parsedBody) {
+      logger.error("Event missing parsed body");
+      return createHttpResponse(500, {
+        message: "Internal Server Error: Sputum Details Request missing",
+      });
+    }
+
+    //  Validate Sputum  Details Request
+    const parsed = SputumRequestSchema.safeParse(parsedBody);
+    if (!parsed.success) {
+      logger.error("Validation failed", parsed.error.flatten());
+      return createHttpResponse(400, {
+        message: "Sputum Details Request validation failed",
+      });
+    }
+
+    const { createdBy } = event.requestContext.authorizer;
+    let sputumDetails: SputumDetails;
+    try {
+      sputumDetails = await SputumDetailsDbOps.createOrUpdateSputumDetails(applicationId, {
+        ...parsed.data,
+        createdBy,
+        applicationId,
+      });
+    } catch (error: unknown) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      if ((error as any).name === "ConditionalCheckFailedException")
+        return createHttpResponse(400, { message: "Sputum Details already saved" });
+      throw error;
+    }
+
+    return createHttpResponse(200, {
+      ...sputumDetails.toJson(),
+    });
+  } catch (err) {
+    logger.error(err, "Error saving Sputum Details");
+    return createHttpResponse(500, { message: "Something went wrong" });
+  }
+};
