cd764b03affb4bbe459a52d7825db086bba1d10c
Jarosław Michalski <22432929+jaro-m@users.noreply.github.com>
2025-06-30 16:17:04 +0100

    Feature/TBBETA-100 Clinic service update (#404)
    * add /clinics/active endpoint
    
    Modified/extended schema, added Clinic model,
    added new 'path' to 'routes' and added seed data.
    
    * add isActiveClinic endpoint
    
    This endpoint returns boolean value according to TBBETA-100 task
    
    * add tests and improve Clinic class methods
    
    isActiveClinic method uses QueryCommand instead of ScanCommand
    which in theory should be faster
    
    * add more tests for clinic-service
    
    Also some improvements added to the code
    
    * add reading json file
    
    Clinics are read from a file, added validation.
    
    * add tests for JSON->DB conversion code (clinic)
    
    * remove .skip in 3 clinic lambda tests
    
    * add more test for clinic-service
    
    Also done some improvements to the service code
    
    * fix test
    
    Added one more test as well
    
    * add more tests (clinic) and improve existing ones
    
    * add workflow to autopopulate db
    
    Added json file with clinics data
    
    * move workflow changes to the right file
    
    * modify workflow (clinics), add dynamodb json file
    
    Workflow modification (1), added dynamodb json file with clinics data.
    
    * modify workflow (clinics data)
    
    Workflow improvements (2)
    
    * remove unneeded workflow entry (clinics data)
    
    Workflow modification (3)
    
    * update clinics table name in clinics data file
    
    * remove incorrect data from clinics data file
    
    * add pk & sk to clinics data
    
    * add conversion/validation python script
    
    The script will convert Clinics data in plain JSON to DynamoDB JSON format.
    Also it will validate the data and ignore all incorrect clinic objects.
    
    * fix JSONCLINICSDATA value in workflow script
    
    * change working-directory value in workflow
    
    * modify aws batch-write-item command in workflow
    
    * add checking new file data
    
    * minor improvements
    
    Output of the newly created file is limited to 20 lines,
    that shoul be enough to check if it's created OK.
    Removed file that's dynamically created during workflow script run.
    Just in cave any file in python environment (ve) are ignored
    (added entry to .gitignore file)
    
    * fix file path issue
    
    * fix issues with env vars/constants
    
    Improvements to the workflow script (clinics data)
    
    * improve/fix variable use in script
    
    Minor improvements to the python script (json conversion/validation)
    
    * add more clinic data to clinicsData.json
    
    * modify schema and create test clinic table in db
    
    endDate, apart from being string, can also be null.
    Added code to create a new clinic table in the test db.
    
    * address sonarcloud issues
    
    * address PR comments
    
    * fix CLINIC_SERVICE_DATABASE_NAME value in all envs
    
    * clinicId in query params, minor fixes
    
    The request to check whether the clinic is active or not needs to have
    clinicId in query params now and now in path params as before.
    That will allow clinicId string to contain slash character: '/'
    Fixed/added tests and added improvements to fetchActiveClinicsHandler.
    
    * add/improve tests, minor fixes
    
    Added missing curly brackets in 'if' statement in fetchActiveClinicsHandler.
    
    * rename endpoint and remove unneeded code
    
    The 'clinicID' (query param) is now required.
    The endpoint is simplyfied,
    checks whether the clinic is active or not and nothing else.
    
    * modify clinic IDs
    
    * fix test
    
    The error message changed.
    
    ---------
    
    Co-authored-by: Jaroslaw Michalski <jaroslaw.m.michalski@phe.gov.uk>

--- pets-core-services/src/clinic-service/lambdas/clinics.spec.ts
+++ pets-core-services/src/clinic-service/lambdas/clinics.spec.ts
@@ -1,33 +1,58 @@
+import { ScanCommand } from "@aws-sdk/lib-dynamodb";
 import { APIGatewayProxyResult } from "aws-lambda";
-import { describe, expect, test } from "vitest";
+import { mockClient } from "aws-sdk-client-mock";
+import { describe, expect, test, vi } from "vitest";
 
+import awsClients from "../../shared/clients/aws";
+import { CountryCode } from "../../shared/country";
+import { logger } from "../../shared/logger";
 import { PetsAPIGatewayProxyEvent } from "../../shared/types";
 import { context, mockAPIGwEvent } from "../../test/mocks/events";
+import { NewClinic } from "../models/clinics";
 import { handler } from "./clinics";
 
-describe("Test for Applicant Lambda", () => {
+const clinicDetails: NewClinic[] = [
+  {
+    clinicId: "Test-Clinic-Id",
+    name: "Test Clinic",
+    city: "Test Town",
+    country: CountryCode.ALA,
+    startDate: "2025-03-03",
+    endDate: null,
+    createdBy: "test-clinic-creator@epost.this",
+  },
+];
+
+describe("Test for Clinic Lambda", () => {
+  const ddbMock = mockClient(awsClients.dynamoDBDocClient);
+
   test("Creating a Clinic", async () => {
     // Arrange
     const event: PetsAPIGatewayProxyEvent = {
       ...mockAPIGwEvent,
       resource: "/clinics/{clinicId}",
-      path: "/clinics/12345",
+      path: `/clinics/${clinicDetails[0].clinicId}`,
       pathParameters: {
-        clinicId: "12345",
+        clinicId: clinicDetails[0].clinicId,
       },
       httpMethod: "POST",
+      // requestSchema: ClinicSchema,
       body: JSON.stringify({
-        clinicId: "Test-Clinic-Id",
-        clinicName: "Test Clinic",
-        iom: true,
+        ...clinicDetails[0],
       }),
     };
 
     // Act
     const response: APIGatewayProxyResult = await handler(event, context);
+    const parsedResponse = JSON.parse(response.body) as { body: string };
+    const parsedBody = JSON.parse(parsedResponse.body) as NewClinic[];
 
     // Assert
     expect(response.statusCode).toBe(200);
+
+    expect(parsedBody).toMatchObject({
+      ...clinicDetails[0],
+    });
   });
 
   test("Fetching a Clinic", async () => {
@@ -51,10 +76,50 @@ describe("Test for Applicant Lambda", () => {
 
   test("Fetching all Clinics", async () => {
     // Arrange;
+    const loggerMock = vi.spyOn(logger, "info").mockImplementation(() => null);
     const event: PetsAPIGatewayProxyEvent = {
       ...mockAPIGwEvent,
       resource: "/clinics",
       path: "/clinics",
+      httpMethod: "GET",
+    };
+    ddbMock.on(ScanCommand).resolves({
+      Items: [
+        {
+          ...clinicDetails[0],
+          pk: `CLINIC#${clinicDetails[0].clinicId}`,
+          sk: "CLINIC#ROOT",
+        },
+      ],
+    });
+
+    // Act
+    const response: APIGatewayProxyResult = await handler(event, context);
+
+    // Assert
+    expect(response.statusCode).toBe(200);
+    expect(JSON.parse(response.body)).toMatchObject([
+      {
+        ...clinicDetails[0],
+        startDate: new Date(clinicDetails[0].startDate).toISOString(),
+      },
+    ]);
+    expect(loggerMock).toHaveBeenCalled();
+    expect(loggerMock).toHaveBeenLastCalledWith(
+      { resultCount: 1 },
+      "Clinics data fetched successfully",
+    );
+  });
+
+  test("Checking an active clinic", async () => {
+    // Arrange;
+    const event: PetsAPIGatewayProxyEvent = {
+      ...mockAPIGwEvent,
+      resource: "/clinics/active",
+      path: "/clinics/active",
+      queryStringParameters: {
+        clinicId: "1",
+      },
       httpMethod: "GET",
     };
 
@@ -63,5 +128,37 @@ describe("Test for Applicant Lambda", () => {
 
     // Assert
     expect(response.statusCode).toBe(200);
+  });
+
+  test("Checking an active Clinic", async () => {
+    // Arrange;
+    const loggerMock = vi.spyOn(logger, "info").mockImplementation(() => null);
+    const event: PetsAPIGatewayProxyEvent = {
+      ...mockAPIGwEvent,
+      resource: "/clinics/isactive",
+      path: "/clinics/isactive",
+      httpMethod: "GET",
+      queryStringParameters: { clinicId: `${clinicDetails[0].clinicId}` },
+    };
+    ddbMock.on(ScanCommand).resolves({
+      Items: [
+        {
+          ...clinicDetails[0],
+          pk: `CLINIC#${clinicDetails[0].clinicId}`,
+          sk: "CLINIC#ROOT",
+        },
+      ],
+    });
+
+    // Act
+    const response: APIGatewayProxyResult = await handler(event, context);
+
+    // Assert
+    expect(response.statusCode).toBe(200);
+    expect(JSON.parse(response.body)).toMatchObject({ isActive: false });
+    expect(loggerMock).toHaveBeenCalled();
+    expect(loggerMock).toHaveBeenLastCalledWith(
+      `Fetching the clinic (${clinicDetails[0].clinicId})`,
+    );
   });
 });
