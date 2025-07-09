da42df0504bb7aa2e3f96a5d73b9e2bcd09c322e
will.tipper <will.tipper@ukhsa.gov.uk>
2025-07-09 14:18:48 +0100

    Add back links to tb certificate section

--- pets-ui/src/pages/tb-summary.tsx
+++ pets-ui/src/pages/tb-summary.tsx
@@ -1,17 +1,10 @@
-import { IBreadcrumbItem } from "@/components/breadcrumb/breadcrumb";
 import Container from "@/components/container/container";
 import Heading from "@/components/heading/heading";
 import TbSummary from "@/sections/tb-summary";
 
 export default function TbSummaryPage() {
-  const breadcrumbItems: IBreadcrumbItem[] = [
-    {
-      text: "Application progress tracker",
-      href: "/tracker",
-    },
-  ];
   return (
-    <Container title="TB Summary" breadcrumbItems={breadcrumbItems}>
+    <Container title="TB Summary" backLinkTo="/tb-certificate-declaration">
       <Heading level={1} size="l" title="Check TB clearance certificate declaration" />
       <TbSummary />
     </Container>
