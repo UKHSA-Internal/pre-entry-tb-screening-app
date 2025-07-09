555745abb6402a1ec3a043002554d5c5e3415bdf
will.tipper <will.tipper@ukhsa.gov.uk>
2025-07-09 14:18:20 +0100

    Add back links to medical screening section

--- pets-ui/src/pages/medical-screening-confirmation.tsx
+++ pets-ui/src/pages/medical-screening-confirmation.tsx
@@ -1,19 +1,11 @@
-import { IBreadcrumbItem } from "@/components/breadcrumb/breadcrumb";
 import Confirmation from "@/components/confirmation/confirmation";
 import Container from "@/components/container/container";
 
 export default function MedicalConfirmation() {
-  const breadcrumbItems: IBreadcrumbItem[] = [
-    {
-      text: "Application progress tracker",
-      href: "/tracker",
-    },
-  ];
-
   const furtherInfo = ["The applicant is now ready to conduct their chest x-ray or sputum test."];
 
   return (
-    <Container title="Medical screening confirmation" breadcrumbItems={breadcrumbItems}>
+    <Container title="Medical screening confirmation" backLinkTo="/medical-summary">
       <Confirmation
         confirmationText={"Medical screening record created"}
         furtherInfo={furtherInfo}
--- pets-ui/src/pages/medical-screening-summary.tsx
+++ pets-ui/src/pages/medical-screening-summary.tsx
@@ -1,18 +1,10 @@
-import { IBreadcrumbItem } from "@/components/breadcrumb/breadcrumb";
 import Container from "@/components/container/container";
 import Heading from "@/components/heading/heading";
 import MedicalScreeningReview from "@/sections/medical-screening-summary";
 
 export default function MedicalSummaryPage() {
-  const breadcrumbItems: IBreadcrumbItem[] = [
-    {
-      text: "Application progress tracker",
-      href: "/tracker",
-    },
-  ];
-
   return (
-    <Container title="Medical screening summary" breadcrumbItems={breadcrumbItems}>
+    <Container title="Medical screening summary" backLinkTo="/medical-screening">
       <Heading level={1} size="l" title="Check medical screening" />
       <MedicalScreeningReview />
     </Container>
