15aa7be15eec059a50e53ce27850326358e9819e
will.tipper <will.tipper@ukhsa.gov.uk>
2025-07-09 14:17:45 +0100

    Add back links to travel details section

--- pets-ui/src/pages/travel-summary.tsx
+++ pets-ui/src/pages/travel-summary.tsx
@@ -1,18 +1,10 @@
-import { IBreadcrumbItem } from "@/components/breadcrumb/breadcrumb";
 import Container from "@/components/container/container";
 import Heading from "@/components/heading/heading";
 import TravelReview from "@/sections/applicant-travel-summary";
 
 export default function TravelSummaryPage() {
-  const breadcrumbItems: IBreadcrumbItem[] = [
-    {
-      text: "Application progress tracker",
-      href: "/tracker",
-    },
-  ];
-
   return (
-    <Container title="Applicant travel information summary" breadcrumbItems={breadcrumbItems}>
+    <Container title="Applicant travel information summary" backLinkTo="/travel-details">
       <Heading level={1} size="l" title="Check travel information" />
       <TravelReview />
     </Container>
