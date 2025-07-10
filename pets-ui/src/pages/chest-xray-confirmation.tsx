8cc0584b09a3f04e0f05643cf82de77716eb526b
will.tipper <will.tipper@ukhsa.gov.uk>
2025-07-10 12:44:01 +0100

    Add back links to radiological section

--- pets-ui/src/pages/chest-xray-confirmation.tsx
+++ pets-ui/src/pages/chest-xray-confirmation.tsx
@@ -1,19 +1,11 @@
-import { IBreadcrumbItem } from "@/components/breadcrumb/breadcrumb";
 import Confirmation from "@/components/confirmation/confirmation";
 import Container from "@/components/container/container";
 
 export default function ChestXrayConfirmation() {
-  const breadcrumbItems: IBreadcrumbItem[] = [
-    {
-      text: "Application progress tracker",
-      href: "/tracker",
-    },
-  ];
-
   const furtherInfo = ["You can now return to the progress tracker."];
 
   return (
-    <Container title="Radiological outcome confirmed" breadcrumbItems={breadcrumbItems}>
+    <Container title="Radiological outcome confirmed" backLinkTo="/chest-xray-summary">
       <Confirmation
         confirmationText={"Radiological outcome confirmed"}
         furtherInfo={furtherInfo}
--- pets-ui/src/pages/chest-xray-summary.tsx
+++ pets-ui/src/pages/chest-xray-summary.tsx
@@ -1,18 +1,10 @@
-import { IBreadcrumbItem } from "@/components/breadcrumb/breadcrumb";
 import Container from "@/components/container/container";
 import Heading from "@/components/heading/heading";
 import ChestXraySummary from "@/sections/chest-xray-summary";
 
 export default function ChestXraySummaryPage() {
-  const breadcrumbItems: IBreadcrumbItem[] = [
-    {
-      text: "Application progress tracker",
-      href: "/tracker",
-    },
-  ];
-
   return (
-    <Container title="Check chest X-ray information" breadcrumbItems={breadcrumbItems}>
+    <Container title="Check chest X-ray information" backLinkTo="/sputum-question">
       <Heading level={1} size="l" title="Check chest X-ray information" />
       <ChestXraySummary />
     </Container>
