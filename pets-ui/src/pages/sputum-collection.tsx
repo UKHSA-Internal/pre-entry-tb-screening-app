7e704bb48965f19f10cc8dc3430c0863fd474804
will.tipper <will.tipper@ukhsa.gov.uk>
2025-07-10 17:13:44 +0100

    Add back links to sputum section

--- pets-ui/src/pages/sputum-collection.tsx
+++ pets-ui/src/pages/sputum-collection.tsx
@@ -1,17 +1,9 @@
-import { IBreadcrumbItem } from "@/components/breadcrumb/breadcrumb";
 import Container from "@/components/container/container";
 import SputumCollectionForm from "@/sections/sputum-collection-form";
 
 export default function SputumCollectionPage() {
-  const breadcrumbItems: IBreadcrumbItem[] = [
-    {
-      text: "Application progress tracker",
-      href: "/tracker",
-    },
-  ];
-
   return (
-    <Container title="Enter sputum sample collection information" breadcrumbItems={breadcrumbItems}>
+    <Container title="Enter sputum sample collection information" backLinkTo="/tracker">
       <SputumCollectionForm />
     </Container>
   );
