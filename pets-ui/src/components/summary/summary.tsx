f68ea1129f9fb616a4e00e6dda247fc1879394a1
Petko Dimitrov <petko.dimitrov@ukhsa.gov.uk>
2025-07-31 15:27:13 +0100

    Fix summary page

--- pets-ui/src/components/summary/summary.tsx
+++ pets-ui/src/components/summary/summary.tsx
@@ -16,50 +16,30 @@ interface SummaryProps {
 }
 
 function summaryValue(status: ApplicationStatus, summaryElement: SummaryElement) {
-  if (Array.isArray(summaryElement.value)) {
-    return (
-      <div className="govuk-summary-value-column">
-        {summaryElement.value.length > 0
-          ? summaryElement.value.map((value) => {
-              return (
-                <dd className="govuk-summary-list__value" key={value}>
-                  {value}
-                </dd>
-              );
-            })
-          : status != ApplicationStatus.COMPLETE &&
-            summaryElement.link && (
-              <LinkLabel
-                to={summaryElement.link}
-                title={summaryElement.emptyValueText ?? ""}
-                hiddenLabel=""
-                externalLink={false}
-              />
-            )}
-      </div>
-    );
-  } else if (summaryElement.value) {
-    return <dd className="govuk-summary-list__value">{summaryElement.value}</dd>;
-  } else if (
-    summaryElement.emptyValueText &&
-    summaryElement.link &&
-    status !== ApplicationStatus.COMPLETE
-  ) {
-    return (
-      <dd className="govuk-summary-list__value">
-        <LinkLabel
-          to={summaryElement.link}
-          title={summaryElement.emptyValueText}
-          hiddenLabel=""
-          externalLink={false}
-        />
-      </dd>
-    );
-  } else {
-    const displayValue =
-      status === ApplicationStatus.COMPLETE ? "" : summaryElement.emptyValueText || "Not provided";
-    return <dd className="govuk-summary-list__value">{displayValue}</dd>;
+  const hasValue = Array.isArray(summaryElement.value)
+    ? summaryElement.value.length > 0
+    : !!summaryElement.value;
+
+  if (hasValue) {
+    if (Array.isArray(summaryElement.value)) {
+      return (
+        <div className="govuk-summary-value-column">
+          {summaryElement.value.map((value) => {
+            return (
+              <dd className="govuk-summary-list__value" key={value}>
+                {value}
+              </dd>
+            );
+          })}
+        </div>
+      );
+    } else {
+      return <dd className="govuk-summary-list__value">{summaryElement.value}</dd>;
+    }
   }
+
+  const displayValue = status === ApplicationStatus.COMPLETE ? "" : "Not provided";
+  return <dd className="govuk-summary-list__value">{displayValue}</dd>;
 }
 
 export default function Summary(props: Readonly<SummaryProps>) {
@@ -70,27 +50,18 @@ export default function Summary(props: Readonly<SummaryProps>) {
           <div className="govuk-summary-list__row" key={summaryElement.key}>
             <dt className="govuk-summary-list__key">{summaryElement.key}</dt>
             {summaryValue(props.status, summaryElement)}
-            {(() => {
-              const hasValue = Array.isArray(summaryElement.value)
-                ? summaryElement.value.length > 0
-                : !!summaryElement.value;
-
-              return (
-                summaryElement.link &&
-                summaryElement.link.length > 0 &&
-                hasValue &&
-                props.status !== ApplicationStatus.COMPLETE && (
-                  <dd className="govuk-summary-list__actions">
-                    <LinkLabel
-                      to={summaryElement.link}
-                      title="Change"
-                      hiddenLabel={" " + summaryElement.hiddenLabel}
-                      externalLink={false}
-                    />
-                  </dd>
-                )
-              );
-            })()}
+            {summaryElement.link &&
+              summaryElement.link.length > 0 &&
+              props.status !== ApplicationStatus.COMPLETE && (
+                <dd className="govuk-summary-list__actions">
+                  <LinkLabel
+                    to={summaryElement.link}
+                    title="Change"
+                    hiddenLabel={" " + summaryElement.hiddenLabel}
+                    externalLink={false}
+                  />
+                </dd>
+              )}
           </div>
         );
       })}
