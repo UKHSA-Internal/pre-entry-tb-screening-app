25cea3790293aed45ed9bd5d1186547a5a711949
jasnave-ukhsa <jasnave.pinto@ukhsa.gov.uk>
2025-09-02 14:48:33 +0100

    fix the idclaims bug (#528)

--- pets-ui/src/utils/clinic.ts
+++ pets-ui/src/utils/clinic.ts
@@ -3,18 +3,33 @@ import { msalInstance } from "@/auth/auth";
 import { AppDispatch } from "@/redux/store";
 import { setClinic } from "@/redux/tbCertificateSlice";
 
-export const getClinicId = (): string | null => {
+export const getClinicId = async (): Promise<string | null> => {
   if (import.meta.env.VITE_AZURE_SKIP_TOKEN_ACQUISITION === "true") {
     return null;
   }
 
-  const account = msalInstance.getActiveAccount()!;
-  const clinicId = (account.idTokenClaims as { ClinicID: string }).ClinicID;
-  return clinicId || null;
+  let account = msalInstance.getActiveAccount() ?? undefined;
+
+  if (!account) {
+    const accounts = msalInstance.getAllAccounts();
+    if (accounts.length > 0) {
+      msalInstance.setActiveAccount(accounts[0]);
+      account = accounts[0];
+    }
+  }
+
+  const response = await msalInstance.acquireTokenSilent({
+    account,
+    scopes: [], // add API scopes if needed
+  });
+
+  const claims = response.idTokenClaims as { ClinicID?: string };
+  const clinicId = claims?.ClinicID ?? null;
+  return clinicId;
 };
 
-export const fetchClinic = (dispatch: AppDispatch): void => {
-  const clinicId = getClinicId();
+export const fetchClinic = async (dispatch: AppDispatch): Promise<void> => {
+  const clinicId = await getClinicId();
   if (!clinicId) return;
 
   void getClinicById(clinicId).then(({ data }) => {
