346f673ffffd81effb8e3405f93b6d48708f4e8a
Jaroslaw Michalski <jaroslaw.m.michalski@ukhsa.gov.uk>
2025-09-08 15:04:46 +0100

    TBBETA-1232 fix schemas, add test

--- pets-core-services/src/application-service/handlers/save-sputum-decision.spec.ts
+++ pets-core-services/src/application-service/handlers/save-sputum-decision.spec.ts
@@ -5,13 +5,12 @@ import { logger } from "../../shared/logger";
 import { TaskStatus } from "../../shared/types/enum";
 import { mockAPIGwEvent } from "../../test/mocks/events";
 import { seededSputumDecision } from "../fixtures/sputum-decision";
+import { SputumDecision } from "../models/sputum-decision";
 import { YesOrNo } from "../types/enums";
 import { SaveSputumDecisionEvent, saveSputumDecisionHandler } from "./save-sputum-decision";
 
 const newSputumDecisionDetails: SaveSputumDecisionEvent["parsedBody"] = {
   sputumRequired: YesOrNo.No,
-  status: TaskStatus.completed,
-  dateCreated: "2025-05-05",
 };
 
 describe("Test for Sputum Decision into DB", () => {
@@ -120,5 +119,29 @@ describe("Test for Sputum Decision into DB", () => {
     expect(JSON.parse(response.body)).toMatchObject({
       message: "Something went wrong",
     });
+  });
+
+  test("Handling error while saving sputum decision", async () => {
+    // Arrange;
+    const errorLoggerMock = vi.spyOn(logger, "error").mockImplementation(() => null);
+    const errorMessage = "Couldn't save it";
+    vi.spyOn(SputumDecision, "createSputumDecision").mockImplementation(() => {
+      throw new Error(errorMessage);
+    });
+    const event: SaveSputumDecisionEvent = {
+      ...mockAPIGwEvent,
+      parsedBody: { ...seededSputumDecision[1] },
+    };
+
+    // Act
+    // await expect(saveSputumDecisionHandler(event)).resolves.toBe("noooo");
+    const response = await saveSputumDecisionHandler(event);
+
+    // Assert
+    expect(response.statusCode).toEqual(500);
+    expect(errorLoggerMock).toHaveBeenCalledWith(
+      Error(errorMessage),
+      "Error saving Sputum Decision",
+    );
   });
 });
