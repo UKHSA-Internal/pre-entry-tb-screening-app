088abca548c833f0178acffa1bbfbff6adf40641
will.tipper <will.tipper@ukhsa.gov.uk>
2025-09-09 18:48:57 +0100

    Update cxr confirmation page title & content

--- pets-ui/src/pages/chest-xray-confirmation.tsx
+++ pets-ui/src/pages/chest-xray-confirmation.tsx
@@ -3,10 +3,13 @@ import Container from "@/components/container/container";
 
 export default function ChestXrayConfirmation() {
   return (
-    <Container title="Radiological outcome confirmed" backLinkTo="/chest-xray-summary">
+    <Container title="Chest X-ray images confirmed - Complete UK pre-entry health screening - GOV.UK">
       <Confirmation
-        confirmationText={"Radiological outcome confirmed"}
-        furtherInfo={["You can now return to the progress tracker."]}
+        confirmationText={"Chest X-ray images confirmed"}
+        furtherInfo={[
+          "We have sent the chest X-ray images to UKHSA.",
+          "You can now view a summary for this visa applicant.",
+        ]}
         buttonText={"Continue"}
         buttonLink={"/tracker"}
         whatHappensNext
