0cb7ff3f4eeb6009d5632c3e150d5b4a8254f30d
Jaroslaw Michalski <jaroslaw.m.michalski@ukhsa.gov.uk>
2025-09-12 21:00:58 +0100

    TBBETA-784 export secrets as env vars
    Selected secrets from config/.env.local.secrets file
    will be exported as env vars.
    dotenv.populate() is used instead of dotenv.config()

--- scripts/start.mjs
+++ scripts/start.mjs
@@ -1,7 +1,12 @@
-import * as dotenv from "dotenv";
-import { resolve } from "path";
+import dotenv from "dotenv";
 import concurrently from "concurrently";
 import { execSync } from "child_process";
+import { parseEnv } from "util";
+import { readFileSync } from "fs";
+import { resolve } from "path";
+
+// Add here whatever should be exported as env vars from .env.local.secrets file
+const REQUIRED_SECRETS = ["VITE_MSAL_CLIENT_ID", "VITE_MSAL_TENANT_ID"];
 
 const isPowerShell =
   !!process.env.PSModulePath || process.env.ComSpec?.toLowerCase().includes("powershell.exe");
@@ -18,9 +23,36 @@ function runCommand(cmd) {
   }
 }
 
-dotenv.config({
-  path: resolve(process.cwd(), "configs/.env"),
-});
+async function readAndExportEnvVars(filePath) {
+  console.info(filePath);
+  try {
+    const secretsStr = readFileSync(filePath, "utf-8").toString();
+    const secrets = parseEnv(secretsStr);
+    const toExport = {};
+
+    for (let key in secrets) {
+      // const key = k.trim();
+
+      if (
+        !filePath.endsWith(".local.secrets") ||
+        (filePath.endsWith(".local.secrets") && REQUIRED_SECRETS.includes(key))
+      ) {
+        toExport[key] = secrets[key];
+      }
+    }
+
+    console.info("✅ Selected secrets exported as env vars");
+    dotenv.populate(process.env, toExport);
+  } catch (error) {
+    console.error("❌ Exporting secrets failed:", error);
+    process.exit(1);
+  }
+}
+
+// Export vars from .env file
+readAndExportEnvVars(resolve(process.cwd(), "configs/.env"));
+// Export vars from .env.local.secret file (only from REQUIRED_SECRETS)
+readAndExportEnvVars(resolve(process.cwd(), "configs/.env.local.secrets"));
 
 runCommand("git update-index --assume-unchanged pets-core-services/openapi-docs.json");
 runCommand("pnpm rimraf pets-local-infra/cdk.out");
