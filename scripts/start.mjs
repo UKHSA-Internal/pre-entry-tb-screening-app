67fe24df35e41c132cd3a7406dbde9043b6b0793
Jaroslaw Michalski <jaroslaw.m.michalski@ukhsa.gov.uk>
2025-09-23 17:28:29 +0100

    TBBETA-784 simplify start.mjs changes

--- scripts/start.mjs
+++ scripts/start.mjs
@@ -1,8 +1,6 @@
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
 import concurrently from "concurrently";
 import { execSync } from "child_process";
-import { parseEnv } from "util";
-import { readFileSync } from "fs";
 import { resolve } from "path";
 
 // Add here whatever should be exported as env vars from .env.local.secrets file
@@ -23,43 +21,21 @@ function runCommand(cmd) {
   }
 }
 
-function readAndExportEnvVars(filePath, requireq = []) {
-  console.info(filePath);
-  try {
-    const secretsStr = readFileSync(filePath, "utf-8").toString();
-    const secrets = parseEnv(secretsStr);
-    const toExport = {};
-
-    if (Object.keys(secrets).length === 0) {
-      throw new Error(`Could not export vars from the file: ${filePath}`);
-    }
-
-    for (let key in secrets) {
-      if (requireq.length === 0 || requireq.includes(key)) {
-        toExport[key] = secrets[key];
-      }
-    }
-
-    console.info("✅ Selected secrets exported as env vars");
-    dotenv.populate(process.env, toExport);
-  } catch (error) {
-    console.error(`❌ Exporting secrets failed for env: ${process.env.ENVIRONMENT}`, error);
-    // This should fail CI step/job
-    process.exit(1);
-  }
-}
-
 // Export vars from .env file
-readAndExportEnvVars(resolve(process.cwd(), "configs/.env"));
-// If it's not CI process, but on a dev's machine, then export some secrets, otherwise ignore it
+dotenv.config({
+  path: resolve(process.cwd(), "configs/.env"),
+});
+
+// If it's not CI process, but on a dev's machine, then export secrets
 if (
   !process.env.CI &&
   !process.env.GITHUB_ACTIONS &&
   process.env.ENVIRONMENT.toLocaleLowerCase() === "local"
 ) {
   // Export vars from .env.local.secret file
-  // (only the ones that are included in REQUIRED_SECRETS)
-  readAndExportEnvVars(resolve(process.cwd(), "configs/.env.local.secrets"), REQUIRED_SECRETS);
+  dotenv.config({
+    path: resolve(process.cwd(), "configs/.env.local.secrets"),
+  });
 }
 
 runCommand("git update-index --assume-unchanged pets-core-services/openapi-docs.json");
