30f9eaf9081f53c5e89f72aa28477ca7999e0a14
will.tipper <will.tipper@ukhsa.gov.uk>
2025-10-08 14:18:43 +0100

    Fix sc issue

--- pets-ui/src/components/scrollToTop/scrollToTop.tsx
+++ pets-ui/src/components/scrollToTop/scrollToTop.tsx
@@ -5,8 +5,8 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    if (!window.location.hash) {
-      window.scrollTo(0, 0);
+    if (!globalThis.location.hash) {
+      globalThis.scrollTo(0, 0);
     }
   }, [pathname]);
 
