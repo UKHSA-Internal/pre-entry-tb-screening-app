256323a9c90d26807815dcd16e2a6548a32e9670
Will <100358610+WJTipper@users.noreply.github.com>
2025-10-08 10:21:28 +0100

    Update beta banner to latest design (#554)

--- pets-ui/src/components/phaseBanner/phaseBanner.tsx
+++ pets-ui/src/components/phaseBanner/phaseBanner.tsx
@@ -6,13 +6,14 @@ export default function PhaseBanner() {
       <p className="govuk-phase-banner__content">
         <strong className="govuk-tag govuk-phase-banner__content__tag">BETA</strong>
         <span className="govuk-phase-banner__text">
-          This is a new service – your{" "}
+          This is a new service. Help us improve it and{" "}
           <LinkLabel
             to="https://forms.office.com/pages/responsepage.aspx?id=mRRO7jVKLkutR188-d6GZtaAaJfrhApCue13O2-oStFUNlIyRkRMWVBNQkszSTJISDJGU1pJTTkxNy4u&route=shorturl"
-            title="feedback"
+            title="give your feedback (opens in new tab)"
             externalLink={true}
-          />{" "}
-          will help us to improve it.
+            openInNewTab
+          />
+          .
         </span>
       </p>
     </div>
