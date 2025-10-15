924499a13b2382c398b80e1b540b94ff45f5ef67
will.tipper <will.tipper@ukhsa.gov.uk>
2025-10-15 14:30:16 +0100

    Update travel address & contact details page

--- pets-ui/src/pages/travel-uk-address.tsx
+++ pets-ui/src/pages/travel-uk-address.tsx
@@ -1,11 +1,11 @@
 import Container from "@/components/container/container";
 import ApplicantTravelAddressAndContactDetails from "@/sections/applicant-travel-uk-address";
 
-export default function TravelDetailsPage() {
+export default function TravelAddressAndContactDetailsPage() {
   return (
     <Container
-      title="Travel information - Complete UK pre-entry health screening - GOV.UK"
-      backLinkTo="/tracker"
+      title="Visa applicant's proposed UK address - Complete UK pre-entry health screening - GOV.UK"
+      backLinkTo="/proposed-visa-category"
     >
       <ApplicantTravelAddressAndContactDetails />
     </Container>
