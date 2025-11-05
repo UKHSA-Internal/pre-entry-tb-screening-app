c954c5cb309e6ab1fb6f5e9d45525b5796f43bfc
Jaroslaw Michalski <jaroslaw.m.michalski@ukhsa.gov.uk>
2025-11-05 15:32:55 +0000

    TBBETA-1421 modify pk and sk
    pk & sk were causing ConditionalCheckFailedException,
    the change should fix that

--- pets-core-services/src/audit-service/models/audit-db-ops.ts
+++ pets-core-services/src/audit-service/models/audit-db-ops.ts
@@ -44,12 +44,12 @@ export class AuditDbOps {
   static readonly sk = "AUDIT#DETAILS";
   static readonly getTableName = () => process.env.AUDIT_SERVICE_DATABASE_NAME;
 
-  static todbItem(audit: AuditBase) {
+  static todbItem(audit: AuditBase, pk: string, sk: string) {
     const dbItem = {
       ...audit,
       dateUpdated: audit.dateUpdated.toISOString(),
-      pk: this.getPk(audit.applicationId),
-      sk: this.sk,
+      pk: pk,
+      sk: sk,
     };
     return dbItem;
   }
@@ -58,73 +58,78 @@ export class AuditDbOps {
     try {
       logger.info("Started saving new Audit to DB");
 
-      if (record?.dynamodb?.NewImage && record?.eventSourceARN) {
-        const newImage = record.dynamodb.NewImage;
-        const changeDetails = unmarshall(
-          newImage as AttributeValue | Record<string, AttributeValue>,
-        );
-        logger.info({ changeDetails }, "unmarshalled 'newImage'");
-
-        const myRe = /:table\/(\w+-\w+)\//g;
-        const table = myRe.exec(record.eventSourceARN);
-        logger.info({ table }, "table RegExMatchArray");
-
-        if (!table?.length || table.length < 2) {
-          return;
-        }
-
-        const tableName = table[1];
-
-        const source = undefined; // record?.source;
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const email = changeDetails?.updatedBy ? changeDetails.updatedBy : changeDetails?.createdBy;
-
-        logger.info(`email = ${email as string}`);
-
-        // TODO: Where to get those values from and are they mandatory fields?
-        if (!table || !email) {
-          logger.info("Missing data (table, source, email)");
-
-          return;
-        }
-
-        const updatedDetails: Audit = {
-          applicationId: newImage?.applicationId?.S as string,
-          // User Email or AWS user for console updates
-          updatedBy: email as string,
-          eventType: record.eventName,
-          // Application (App/API) - Application, API (for IOM) or Console
-          source: source ? (source as SourceType) : SourceType.app,
-          // applicant-details / application-details
-          sourceTable: tableName,
-          // TODO: should there be unmarshalled version of newImage value?
-          changeDetails: newImage ? JSON.stringify(newImage) : "",
-          dateUpdated: new Date(),
-        };
-        logger.info("Creating <Audit> object.");
-        const newAudit = new Audit(updatedDetails);
-        logger.info({ updatedDetails }, "<Audit> object created");
-        const dbItem = this.todbItem(newAudit);
-        logger.info({ dbItem }, "Creted dbItem");
-
-        const params: PutCommandInput = {
-          TableName: this.getTableName(),
-          Item: { ...dbItem },
-          // Check to ensure the pk is truly a unique value
-          ConditionExpression: "attribute_not_exists(pk) AND attribute_not_exists(sk)",
-        };
-        const command = new PutCommand(params);
-        await docClient.send(command);
-        logger.info("dbItem saved in DB");
-
-        logger.info("New audit created successfully");
-
-        return newAudit as AuditBase;
-      } else {
+      if (!record?.dynamodb?.NewImage || !record?.eventSourceARN) {
         logger.info({ record }, "There was no newImage object in the record");
 
         return;
       }
+
+      const newImage = record.dynamodb.NewImage;
+      const changeDetails = unmarshall(newImage as AttributeValue | Record<string, AttributeValue>);
+      logger.info({ changeDetails }, "unmarshalled 'newImage'");
+
+      const myRe = /:table\/(\w+-\w+)\//g;
+      const table = myRe.exec(record.eventSourceARN);
+      logger.info({ table }, "table RegExMatchArray");
+
+      if (!table?.length || table.length < 2) {
+        logger.error(`Could not get table name from regex: ${JSON.stringify(table)}`);
+
+        return;
+      }
+      const tableName = table[1];
+
+      // TODO: where to get it from (record.source ???);
+      const source = undefined;
+
+      const email = changeDetails?.updatedBy
+        ? (changeDetails.updatedBy as string)
+        : (changeDetails?.createdBy as string);
+      logger.info(`email = ${email}`);
+
+      // TODO: Where to get those values from and are they mandatory fields?
+      if (!table || !email) {
+        logger.error("Missing table name or updatedBy/createdBy email");
+
+        return;
+      }
+
+      const updatedDetails: Audit = {
+        applicationId: changeDetails?.applicationId as string,
+        // User Email or AWS user for console updates
+        updatedBy: email,
+        eventType: record.eventName,
+        // Application (App/API) - Application, API (for IOM) or Console
+        source: source ? (source as SourceType) : SourceType.app,
+        // applicant-details / application-details
+        sourceTable: tableName,
+        // TODO: should unmarshalled version of newImage be used here?
+        changeDetails: newImage ? JSON.stringify(newImage) : "",
+        dateUpdated: new Date(),
+      };
+      logger.info("Creating <Audit> object.");
+      const newAudit = new Audit(updatedDetails);
+      logger.info({ updatedDetails }, "<Audit> object created");
+      const dbItem = this.todbItem(
+        newAudit,
+        `AUDIT#${changeDetails.pk}`,
+        `AUDIT#${changeDetails.sk}}`,
+      );
+      logger.info({ dbItem }, "Creted dbItem");
+
+      const params: PutCommandInput = {
+        TableName: this.getTableName(),
+        Item: { ...dbItem },
+        // Check to ensure the pk is truly a unique value
+        ConditionExpression: "attribute_not_exists(pk) AND attribute_not_exists(sk)",
+      };
+      const command = new PutCommand(params);
+      await docClient.send(command);
+      logger.info("dbItem saved in DB");
+
+      logger.info("New audit created successfully");
+
+      return newAudit as AuditBase;
     } catch (error) {
       logger.error({ error }, "Error creating audit");
       throw error;
