ecc9a27e589c1cb9749b63ae562e81abf02cff89
Will Tipper <will.tipper@ukhsa.gov.uk>
2025-11-11 11:52:34 +0000

    Add unit tests

--- pets-ui/src/components/button/button.test.tsx
+++ pets-ui/src/components/button/button.test.tsx
@@ -1,14 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 
-import { ButtonClass } from "@/utils/enums";
+import { ButtonClass, ButtonType } from "@/utils/enums";
 
 import Button from "./button";
 
 const handleClick = () => {};
 
 describe("Button component", () => {
-  it("renders correctly when button type is DEFAULT", () => {
+  it("renders correctly when button class is DEFAULT and no type provided", () => {
     render(
       <Button
         id="test-id"
@@ -18,6 +18,8 @@ describe("Button component", () => {
       />,
     );
     expect(screen.getByText("test-text-default")).toBeTruthy();
+    const button = screen.getByRole("button", { name: "test-text-default" });
+    expect(button).toHaveAttribute("type", "submit");
 
     const { container } = render(
       <Button
@@ -32,22 +34,26 @@ describe("Button component", () => {
     expect(container.getElementsByClassName("govuk-button--warning")).toHaveLength(0);
   });
 
-  it("renders correctly when button type is SECONDARY", () => {
+  it("renders correctly when button class is SECONDARY and type is submit", () => {
     render(
       <Button
         id="test-id"
         text="test-text-secondary"
         class={ButtonClass.SECONDARY}
+        type={ButtonType.SUBMIT}
         handleClick={handleClick}
       />,
     );
     expect(screen.getByText("test-text-secondary")).toBeTruthy();
+    const button = screen.getByRole("button", { name: "test-text-secondary" });
+    expect(button).toHaveAttribute("type", "submit");
 
     const { container } = render(
       <Button
         id="test-id"
         text="test-text-secondary"
         class={ButtonClass.SECONDARY}
+        type={ButtonType.SUBMIT}
         handleClick={handleClick}
       />,
     );
@@ -56,22 +62,26 @@ describe("Button component", () => {
     expect(container.getElementsByClassName("govuk-button--warning")).toHaveLength(0);
   });
 
-  it("renders correctly when button type is WARNING", () => {
+  it("renders correctly when button type is WARNING and type is button", () => {
     render(
       <Button
         id="test-id"
         text="test-text-warning"
         class={ButtonClass.WARNING}
+        type={ButtonType.BUTTON}
         handleClick={handleClick}
       />,
     );
     expect(screen.getByText("test-text-warning")).toBeTruthy();
+    const button = screen.getByRole("button", { name: "test-text-warning" });
+    expect(button).toHaveAttribute("type", "button");
 
     const { container } = render(
       <Button
         id="test-id"
         text="test-text-warning"
         class={ButtonClass.WARNING}
+        type={ButtonType.BUTTON}
         handleClick={handleClick}
       />,
     );
