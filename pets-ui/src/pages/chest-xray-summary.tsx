9554455bcf5387e14f6f776538f7bbac427e8f3a
Will Tipper <will.tipper@ukhsa.gov.uk>
2025-11-12 15:24:42 +0000

    Create ga cxr upload journey

--- pets-ui/src/pages/chest-xray-summary.tsx
+++ pets-ui/src/pages/chest-xray-summary.tsx
@@ -1,12 +1,25 @@
+import { useEffect } from "react";
+
 import Container from "@/components/container/container";
 import Heading from "@/components/heading/heading";
 import { useAppSelector } from "@/redux/hooks";
-import { selectChestXray } from "@/redux/store";
+import { selectApplication, selectChestXray } from "@/redux/store";
 import ChestXraySummary from "@/sections/chest-xray-summary";
 import { ApplicationStatus } from "@/utils/enums";
+import { sendGoogleAnalyticsJourneyEvent } from "@/utils/helpers";
 
 export default function ChestXraySummaryPage() {
+  const applicationData = useAppSelector(selectApplication);
   const chestXrayData = useAppSelector(selectChestXray);
+
+  useEffect(() => {
+    sendGoogleAnalyticsJourneyEvent(
+      "Check chest X-ray images",
+      applicationData.applicationId,
+      "Upload chest X-ray images",
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Container
