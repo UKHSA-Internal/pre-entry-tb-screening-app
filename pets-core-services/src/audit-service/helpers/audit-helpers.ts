3052b05b31313b0d3b26f22ef243db2f5c1e197a
Jaroslaw Michalski <jaroslaw.m.michalski@ukhsa.gov.uk>
2025-11-20 11:39:02 +0000

    TBBETA-1421 use PutEventSelectorCommand

--- pets-core-services/src/audit-service/helpers/audit-helpers.ts
+++ pets-core-services/src/audit-service/helpers/audit-helpers.ts
@@ -3,6 +3,9 @@ import {
   LookupEventsCommand,
   LookupEventsCommandInput,
   LookupEventsCommandOutput,
+  PutEventSelectorsCommand,
+  PutEventSelectorsCommandInput,
+  PutEventSelectorsCommandOutput,
   ThrottlingException,
 } from "@aws-sdk/client-cloudtrail";
 import { DynamoDBRecord } from "aws-lambda";
@@ -27,104 +30,46 @@ export const getConsoleEvent = async (record: DynamoDBRecord) => {
     return;
   }
 
-  // Look up CloudTrail events around that time
-  // const startTime = new Date(approxTime * 1000 - 60 * 1000); // 1 min before
-  // const endTime = new Date(approxTime * 1000 + 20 * 1000); // 1 min after
-  // const startTime = new Date(Date.now() - 3 * 60 * 1000);
-  // const endTime = new Date();
-  const startTime = new Date(Date.now() - 6 * 60 * 60 * 1000); // 1h before
-  // const endTime = new Date();
-  // const ITEM_EVENTS = ["PutItem", "DeleteItem"];
-  const events: Event[] = [];
-  let nextToken: string | undefined = undefined;
-  let queryNumber = 0;
-  const eventNames: (string | undefined)[] = [];
-  const eventSources: (string | undefined)[] = [];
-  // const id = record.eventID;
-
-  const params = {
-    LookupAttributes: [
-      // {
-      //   AttributeKey: "EventSource",
-      //   AttributeValue: "dynamodb.amazonaws.com",
-      // },
+  const params: PutEventSelectorsCommandInput = {
+    TrailName: "pets-euw-cloudtrail-managementevents",
+    EventSelectors: [
       {
-        AttributeKey: "EventName",
-        AttributeValue: "PutItem",
+        ReadWriteType: "All",
+        IncludeManagementEvents: true,
+        DataResources: [
+          {
+            Type: "AWS::DynamoDB::Table",
+            Values: [
+              "arn:aws:dynamodb:eu-west-2:108782068086:table/applicant-details",
+              "arn:aws:dynamodb:eu-west-2:108782068086:table/application-details",
+            ],
+          },
+        ],
+        // ExcludeManagementEventSources: ["STRING_VALUE"],
       },
     ],
-    StartTime: startTime,
-    // EndTime: endTime,
-    // MaxResults: 10,
-    NextToken: nextToken,
+    AdvancedEventSelectors: [
+      {
+        Name: "Log all PutItam db events",
+        FieldSelectors: [
+          { Field: "eventCategory", Equals: ["Data"] },
+          { Field: "eventName", Equals: ["PutItem", "DeleteItem"] },
+          { Field: "resources.type", Equals: ["AWS::DynamoDB::Table"] },
+          {
+            Field: "resources.ARN",
+            StartsWith: [
+              "arn:aws:dynamodb:eu-west-2:108782068086:table/applicant-details",
+              "arn:aws:dynamodb:eu-west-2:108782068086:table/application-details",
+            ],
+          },
+        ],
+      },
+    ],
   };
+  const command = new PutEventSelectorsCommand(params);
+  const results: PutEventSelectorsCommandOutput = await client.send(command);
 
-  logger.info("Sending LookupEventCommand");
-  do {
-    try {
-      const result: LookupEventsCommandOutput = await client.send(
-        new LookupEventsCommand(params as LookupEventsCommandInput),
-      );
-      queryNumber += 1;
-      // const filtered = [];
-      // result.Events?.filter((e) => e.EventName && ITEM_EVENTS.includes(e.EventName)) || [];
-      if (!result.Events || result.Events?.length < 1) {
-        logger.info({ result }, "No 'Events'");
-        // return;
-      } else {
-        for (const e of result.Events) {
-          if (!eventNames.includes(e.EventName)) eventNames.push(e.EventName);
-          if (!eventSources.includes(e.EventSource)) eventSources.push(e.EventSource);
-          // if (e.EventName && ITEM_EVENTS.includes(e.EventName)) events.push(e);
-          // const CTEvent = e.CloudTrailEvent ? JSON.parse(e.CloudTrailEvent) : undefined;
-          events.push(e);
-        }
-      }
-      // // don't go over 6 requests for now
-      // if (queryNumber >= 6) {
-      //   nextToken = undefined;
-      // } else {
-      //   nextToken = result.NextToken;
-      // }
-    } catch (err) {
-      logger.error({ err }, "CloudTrail lookup failed");
-      if (err instanceof ThrottlingException) {
-        logger.error(`ERR / Queried ${queryNumber} times, received ${events.length}`);
-      }
-      nextToken = undefined;
-      // return;
-    }
-  } while (nextToken);
+  logger.info({ results }, "Results:");
 
-  logger.info(`Queried ${queryNumber} times, received ${events.length}`);
-  logger.info({ ...eventNames }, "EventNames");
-  logger.info({ ...eventSources }, "EventSources");
-  logger.info({ events }, "CloudTrail lookup result");
-
-  const consoleEvents = events.filter((evt: Event) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const details = evt?.CloudTrailEvent ? JSON.parse(evt.CloudTrailEvent) : undefined;
-
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    if (!details?.userAgent || !details?.requestParameters?.table) return;
-
-    logger.info(details, "CloudTrail parsing event");
-
-    return (
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      details.userAgent === "console.amazonaws.com" &&
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      details.requestParameters?.tableName === tableName
-    );
-  });
-
-  if (consoleEvents && consoleEvents.length > 0) {
-    // @ts-expect-error it can't be undefined
-    logger.info(`Update to ${tableName} came from AWS Console:`, consoleEvents[0]);
-    return SourceType.console;
-  } else {
-    // @ts-expect-error it can't be undefined
-    logger.info(`Update to ${tableName} likely came from SDK/API.`, consoleEvents[0]);
-    return SourceType.app;
-  }
+  return SourceType.app;
 };
