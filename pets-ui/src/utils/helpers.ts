d22489d882fbf6b4213e40cae9258c7daf354d0c
Will Tipper <will.tipper@ukhsa.gov.uk>
2025-11-21 17:06:47 +0000

    Move ga utils to separate file

--- pets-ui/src/utils/helpers.ts
+++ pets-ui/src/utils/helpers.ts
@@ -241,45 +241,6 @@ const isChildUnder11 = (medicalScreeningData: ReduxMedicalScreeningType) => {
   return parsedAge < 11 ? "Yes" : "No";
 };
 
-const updateGoogleAnalyticsConsent = (consentGranted: boolean) => {
-  if (typeof globalThis.gtag === "function") {
-    globalThis.gtag("consent", "update", {
-      ad_storage: consentGranted ? "granted" : "denied",
-      analytics_storage: consentGranted ? "granted" : "denied",
-      functionality_storage: consentGranted ? "granted" : "denied",
-      personalization_storage: consentGranted ? "granted" : "denied",
-    });
-  }
-};
-
-const sendGoogleAnalyticsJourneyEvent = (
-  pageTitle: string,
-  journeyId: string,
-  journeyType: string,
-) => {
-  if (typeof globalThis.gtag === "function") {
-    globalThis.gtag("event", pageTitle, {
-      journey_id: journeyId,
-      journey_type: journeyType,
-    });
-  }
-};
-
-const sendGoogleAnalyticsFormErrorEvent = (pageTitle: string, errorList: string[]) => {
-  if (typeof globalThis.gtag === "function") {
-    globalThis.gtag("form_validation_error", {
-      page: pageTitle,
-      validation_failures: errorList,
-    });
-  }
-};
-
-const setGoogleAnalyticsParams = (paramName: string, params?: unknown) => {
-  if (typeof globalThis.gtag === "function") {
-    globalThis.gtag("set", paramName, params);
-  }
-};
-
 export {
   calculateCertificateExpiryDate,
   calculateCertificateIssueDate,
@@ -294,12 +255,8 @@ export {
   isValidDate,
   logError,
   missingFieldsMessage,
-  sendGoogleAnalyticsFormErrorEvent,
-  sendGoogleAnalyticsJourneyEvent,
-  setGoogleAnalyticsParams,
   spreadArrayIfNotEmpty,
   standardiseDayOrMonth,
   toArray,
-  updateGoogleAnalyticsConsent,
   validateDate,
 };
