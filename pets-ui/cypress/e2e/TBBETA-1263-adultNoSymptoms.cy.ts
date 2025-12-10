9fb5fd78b955768c9655863104676386e558ba3b
Will Tipper <will.tipper@ukhsa.gov.uk>
2025-12-10 17:56:52 +0000

    Fix applicant details summary page url in cypress after merge

--- pets-ui/cypress/e2e/TBBETA-1263-adultNoSymptoms.cy.ts
+++ pets-ui/cypress/e2e/TBBETA-1263-adultNoSymptoms.cy.ts
@@ -157,7 +157,7 @@ describe("Adult with TB History, X-ray Normal, Certificate Issued (6 months)", (
     });
 
     // Verify redirection to the Applicant Summary page
-    cy.url().should("include", "/check-applicant-details");
+    cy.url().should("include", "/check-visa-applicant-details");
     applicantSummaryPage.verifyPageLoaded();
 
     // Verify some of the submitted data appears correctly in the summary
