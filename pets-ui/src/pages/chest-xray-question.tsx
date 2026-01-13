909761473ecc2a64a39b4d80e366b963526f0700
Will Tipper <will.tipper@ukhsa.gov.uk>
2026-01-13 16:53:52 +0000

    Update cxr question page back link

--- pets-ui/src/pages/chest-xray-question.tsx
+++ pets-ui/src/pages/chest-xray-question.tsx
@@ -2,12 +2,22 @@ import { useEffect } from "react";
 
 import Container from "@/components/container/container";
 import { useAppSelector } from "@/redux/hooks";
-import { selectApplication } from "@/redux/store";
+import { selectApplicant, selectApplication } from "@/redux/store";
 import ChestXrayQuestionForm from "@/sections/chest-xray-question-form";
 import { sendGoogleAnalyticsJourneyEvent } from "@/utils/google-analytics-utils";
+import { calculateApplicantAge } from "@/utils/helpers";
 
 export default function ChestXrayQuestionPage() {
   const applicationData = useAppSelector(selectApplication);
+  const applicantData = useAppSelector(selectApplicant);
+  const applicantAge = calculateApplicantAge(applicantData.dateOfBirth);
+
+  let backLinkUrl = "/record-medical-history-tb-symptoms";
+  if (applicantData.sex == "Female") {
+    backLinkUrl = "/medical-history-female";
+  } else if (typeof applicantAge.ageInYears == "number" && applicantAge.ageInYears < 11) {
+    backLinkUrl = "/medical-history-under-11-years-old";
+  }
 
   useEffect(() => {
     sendGoogleAnalyticsJourneyEvent(
@@ -21,7 +31,7 @@ export default function ChestXrayQuestionPage() {
   return (
     <Container
       title="Is an X-ray required? - Complete UK pre-entry health screening - GOV.UK"
-      backLinkTo="/record-medical-history-tb-symptoms"
+      backLinkTo={backLinkUrl}
     >
       <ChestXrayQuestionForm />
     </Container>
