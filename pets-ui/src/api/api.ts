61e5d31ea893b11103973fd50a441aab4700b001
Jaroslaw Michalski <jaroslaw.m.michalski@ukhsa.gov.uk>
2026-01-26 12:15:46 +0000

    TBBETA-1914 modify loggin line

--- pets-ui/src/api/api.ts
+++ pets-ui/src/api/api.ts
@@ -60,7 +60,7 @@ petsApi.interceptors.response.use(
     }
 
     if (error.response?.status === 404) {
-      console.error({ error }, "API error");
+      console.error(error);
       globalThis.location.href = "/page-not-found";
     } else if (error.response?.status && error.response.status >= 400) {
       globalThis.location.href = "/sorry-there-is-problem-with-service";
