8c4ccdbd903787e9e0bfbb3e7f87b04c76f29f7a
jasnave-ukhsa <jasnave.pinto@ukhsa.gov.uk>
2026-02-04 15:33:02 +0000

    fix unit tests

--- pets-ui/src/api/api.ts
+++ pets-ui/src/api/api.ts
@@ -146,7 +146,10 @@ export const postChestXrayDetails = async (
   applicationId: string,
   chestXrayDetails: PostedChestXrayUnionType,
 ) => {
-  const result = await petsApi.post(`/application/${applicationId}/chest-xray?requireValidation=Yes`, chestXrayDetails);
+  const result = await petsApi.post(
+    `/application/${applicationId}/chest-xray?requireValidation=Yes`,
+    chestXrayDetails,
+  );
   return { status: result.status, statusText: result.statusText };
 };
 
