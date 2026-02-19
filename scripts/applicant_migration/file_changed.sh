52a9311533c73cfaa61fa088829d4780361175fa
Jaroslaw Michalski <jaroslaw.m.michalski@ukhsa.gov.uk>
2026-02-19 16:50:55 +0000

    TBBETA-1974 add gh action to trigger tests

---
+++ scripts/applicant_migration/file_changed.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+all_changed_files=$(git diff --name-only HEAD^ HEAD)
+
+for changed_file in $all_changed_files;
+do
+    if [ $changed_file == "scripts/applicant_migration/src/migration_script.py" ]; then
+        echo true
+        exit 0
+    fi
+done
