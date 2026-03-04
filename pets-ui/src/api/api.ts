80eeaf63bafbb822f928be0535868a019cb139cf
Will Tipper <will.tipper@ukhsa.gov.uk>
2026-03-04 17:14:35 +0000

    Remove 404 redirect in axios interceptor: - not needed when user navigates to url that doesn't exist, behaviour is already defined in App.tsx - needed for upcoming backend change where applicant search will return 404 for new applicant rather than 204

--- pets-ui/src/api/api.ts
+++ pets-ui/src/api/api.ts
@@ -61,9 +61,7 @@ petsApi.interceptors.response.use(
       sendGoogleAnalyticsHttpError(0, url);
     }
 
-    if (error.response?.status === 404) {
-      globalThis.location.href = "/page-not-found";
-    } else if (error.response?.status && error.response.status >= 400) {
+    if (error.response?.status && error.response?.status !== 404 && error.response.status >= 400) {
       globalThis.location.href = "/sorry-there-is-problem-with-service";
     }
 
