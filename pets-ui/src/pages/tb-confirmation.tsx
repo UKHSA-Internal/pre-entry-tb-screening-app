0b12086b3f877cd5bf23534ceff2742bbefa2365
Will Tipper <will.tipper@ukhsa.gov.uk>
2026-03-11 16:36:55 +0000

    Open survey links in new tab

--- pets-ui/src/pages/tb-confirmation.tsx
+++ pets-ui/src/pages/tb-confirmation.tsx
@@ -43,6 +43,7 @@ export default function TbConfirmationPage() {
         to="https://forms.office.com/pages/responsepage.aspx?id=mRRO7jVKLkutR188-d6GZtaAaJfrhApCue13O2-oStFUNlIyRkRMWVBNQkszSTJISDJGU1pJTTkxNy4u&route=shorturl"
         title="What did you think of this service?"
         externalLink={true}
+        openInNewTab
       />{" "}
       (takes 10 minutes)
     </React.Fragment>,
