4052e705e43d31d952854f3057ffa99d6ba1087d
Will Tipper <will.tipper@ukhsa.gov.uk>
2026-03-30 13:55:05 +0100

    Move logic to set user role & clinic id from applicant search page to task choice page

--- pets-ui/src/pages/task-choice.tsx
+++ pets-ui/src/pages/task-choice.tsx
@@ -1,8 +1,29 @@
+import { useEffect } from "react";
+
 import Container from "@/components/container/container";
 import Heading from "@/components/heading/heading";
 import LinkLabel from "@/components/linkLabel/LinkLabel";
+import { setUserClinicId } from "@/redux/clinicSlice";
+import { useAppDispatch } from "@/redux/hooks";
+import { setGoogleAnalyticsParams } from "@/utils/google-analytics-utils";
+import { getUserProperties } from "@/utils/userProperties";
 
 export default function TaskChoicePage() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const setUserProperties = async () => {
+      const userProperties = await getUserProperties();
+      setGoogleAnalyticsParams("user_properties", {
+        user_role: userProperties.jobTitle,
+        clinic_id: userProperties.clinicId,
+      });
+      dispatch(setUserClinicId(userProperties.clinicId ?? ""));
+    };
+    void setUserProperties();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Container
       title="No visa applicant found - Complete UK pre-entry health screening - GOV.UK"
