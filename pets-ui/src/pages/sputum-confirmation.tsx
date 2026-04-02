2a49ce76504a24b943105e7783ed58b0376d9120
Will Tipper <will.tipper@ukhsa.gov.uk>
2026-04-02 15:35:09 +0100

    TBBETA-1902: update partial sputum confirmation page

--- pets-ui/src/pages/sputum-confirmation.tsx
+++ pets-ui/src/pages/sputum-confirmation.tsx
@@ -1,25 +1,21 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
+import Button from "@/components/button/button";
 import Confirmation from "@/components/confirmation/confirmation";
 import Container from "@/components/container/container";
+import Heading from "@/components/heading/heading";
 import { useAppSelector } from "@/redux/hooks";
 import { selectApplication, selectSputum } from "@/redux/store";
-import { PositiveOrNegative, TaskStatus } from "@/utils/enums";
+import { ButtonClass, PositiveOrNegative, TaskStatus } from "@/utils/enums";
 import { sendGoogleAnalyticsJourneyEvent } from "@/utils/google-analytics-utils";
 
 export default function SputumConfirmation() {
   const applicationData = useAppSelector(selectApplication);
   const sputumData = useAppSelector(selectSputum);
+  const navigate = useNavigate();
 
   const samples = [sputumData.sample1, sputumData.sample2, sputumData.sample3];
-
-  const allDatesComplete = samples.every(
-    (sample) =>
-      sample.collection.dateOfSample.day &&
-      sample.collection.dateOfSample.month &&
-      sample.collection.dateOfSample.year &&
-      sample.collection.collectionMethod,
-  );
 
   const allResultsComplete = samples.every((sample) => {
     const hasCollectionData =
@@ -33,28 +29,6 @@ export default function SputumConfirmation() {
     return hasCollectionData && hasSmearResult && hasCultureResult;
   });
 
-  const hasAnyResults = samples.some((sample) => {
-    const hasSmearResult = sample.smearResults.smearResult !== PositiveOrNegative.NOT_YET_ENTERED;
-    const hasCultureResult =
-      sample.cultureResults.cultureResult !== PositiveOrNegative.NOT_YET_ENTERED;
-    return hasSmearResult || hasCultureResult;
-  });
-
-  const confirmationText =
-    sputumData.status === TaskStatus.COMPLETE || allResultsComplete
-      ? "Sputum collection and results confirmed"
-      : "Partial sputum sample information confirmed";
-
-  const furtherInfo = (() => {
-    if (allDatesComplete && hasAnyResults && !allResultsComplete) {
-      return ["The panel physician should wait to confirm the remaining sputum sample results"];
-    }
-    return [
-      "We have sent sputum collection details and results to UKHSA.",
-      "You can now view a summary for this visa applicant.",
-    ];
-  })();
-
   useEffect(() => {
     sendGoogleAnalyticsJourneyEvent(
       "sputum_sample_information_confirmed",
@@ -64,15 +38,46 @@ export default function SputumConfirmation() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return (
-    <Container title={confirmationText + " - Complete UK pre-entry health screening - GOV.UK"}>
-      <Confirmation
-        confirmationText={confirmationText}
-        furtherInfo={furtherInfo}
-        buttonText={"Continue"}
-        buttonLink={"/tracker"}
-        whatHappensNext
-      />
-    </Container>
-  );
+  if (sputumData.status === TaskStatus.COMPLETE || allResultsComplete) {
+    return (
+      <Container
+        title={
+          "Sputum collection and results confirmed - Complete UK pre-entry health screening - GOV.UK"
+        }
+      >
+        <Confirmation
+          confirmationText={"Sputum collection and results confirmed"}
+          furtherInfo={[
+            "We have sent sputum collection details and results to UKHSA.",
+            "You can now view a summary for this visa applicant.",
+          ]}
+          buttonText={"Continue"}
+          buttonLink={"/tracker"}
+          whatHappensNext
+        />
+      </Container>
+    );
+  } else {
+    return (
+      <Container
+        title={"Your progress has been saved - Complete UK pre-entry health screening - GOV.UK"}
+      >
+        <Heading level={1} size="l" title="Your progress has been saved" />
+        <p className="govuk-body">The information you entered has been saved.</p>
+        <Heading level={2} size="m" title="What happens next" />
+        <p className="govuk-body">
+          You need to enter results for all 3 sputum samples before you can complete the TB health
+          screening.
+        </p>
+        <Button
+          id="continue"
+          class={ButtonClass.DEFAULT}
+          text={"Continue"}
+          handleClick={() => {
+            navigate("/tracker");
+          }}
+        />
+      </Container>
+    );
+  }
 }
