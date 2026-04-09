d3030656cc6aec2a245ef91ac5f007ba5c3ac2b2
Will Tipper <will.tipper@ukhsa.gov.uk>
2026-04-09 17:17:25 +0100

    Add overrides to screening history & tracker for applications past their expiry date

--- pets-ui/src/sections/progress-tracker.tsx
+++ pets-ui/src/sections/progress-tracker.tsx
@@ -33,7 +33,7 @@ import {
   TaskStatus,
   YesOrNo,
 } from "@/utils/enums";
-import { formatDateForDisplay, upsertAppIntoAppList } from "@/utils/helpers";
+import { formatDateForDisplay, isDateInThePast, upsertAppIntoAppList } from "@/utils/helpers";
 
 interface TaskProps {
   description: string;
@@ -168,7 +168,19 @@ const ProgressTracker = () => {
 
   let tbCertificateStatusOverride = undefined;
   if (tbCertificateData.status === TaskStatus.COMPLETE) {
-    if (tbCertificateData.isIssued === YesOrNo.YES) {
+    if (
+      tbCertificateData.isIssued === YesOrNo.YES &&
+      applicationData.expiryDate &&
+      isDateInThePast(
+        applicationData.expiryDate.day,
+        applicationData.expiryDate.month,
+        applicationData.expiryDate.year,
+      )
+    ) {
+      tbCertificateStatusOverride = (
+        <strong className="govuk-tag govuk-tag--grey">Certificate expired</strong>
+      );
+    } else if (tbCertificateData.isIssued === YesOrNo.YES) {
       tbCertificateStatusOverride = (
         <strong className="govuk-tag govuk-tag--green">Certificate issued</strong>
       );
@@ -233,6 +245,7 @@ const ProgressTracker = () => {
             applicantData={applicantData}
             applicationStatus={applicationData.applicationStatus}
             showCountryOfIssue={false}
+            certificateExpiryDate={applicationData.expiryDate}
           />
         </div>
         {applicantPhotoContext?.applicantPhotoDataUrl ? (
